5b49804ecbca5c4e9eea64c7b2fe62cbccfbafe5
LPeter1997 <lenkefi.peti@gmail.com>
2022-10-14 14:22:42 +0200

    Added test that fundamentally defines multiline string behavior

--- src/Draco.Compiler.Tests/Syntax/LexerTests.cs
+++ src/Draco.Compiler.Tests/Syntax/LexerTests.cs
@@ -27,6 +27,10 @@ public sealed class LexerTests
 
     private static string ValueText(IToken token) => ((IToken.IWithValue<string>)token).Value;
 
+    private static string NormalizeNewliens(string text) => text
+        .Replace("\r\n", "\n")
+        .Replace("\r", "\n");
+
     private static void AssertNoTrivia(IToken token)
     {
         Assert.Empty(token.LeadingTrivia);
@@ -113,8 +117,11 @@ public sealed class LexerTests
     [Trait("Feature", "Strings")]
     public void TestUnclosedLineStringWithNewlineAfter()
     {
-        var text = "\"Hello, line strings!\n";
-        var tokens = LexToArray(text);
+        var text = """
+        "Hello, line strings!
+
+        """;
+        var tokens = LexToArray(NormalizeNewliens(text));
 
         Assert.Equal(3, tokens.Length);
 
@@ -193,5 +200,54 @@ public sealed class LexerTests
         Assert.Equal(TokenType.EndOfInput, tokens[3].Type);
         Assert.Equal(string.Empty, tokens[3].Text);
         AssertNoTriviaOrDiagnostics(tokens[3]);
+    }
+
+    [Fact]
+    [Trait("Feature", "Strings")]
+    public void TestMultilineString()
+    {
+        const string quotes = "\"\"\"";
+        var text = $"""
+        {quotes}
+            Hello!
+            Bye!
+            {quotes}
+        """;
+        var tokens = LexToArray(NormalizeNewliens(text));
+
+        Assert.Equal(5, tokens.Length);
+
+        Assert.Equal(TokenType.MultiLineStringStart, tokens[0].Type);
+        Assert.Equal(quotes, tokens[0].Text);
+        Assert.Empty(tokens[0].LeadingTrivia);
+        Assert.Single(tokens[0].TrailingTrivia);
+        Assert.Equal(TokenType.Newline, tokens[0].TrailingTrivia[0].Type);
+        Assert.Empty(tokens[0].Diagnostics);
+
+        Assert.Equal(TokenType.StringContent, tokens[1].Type);
+        Assert.Equal("    Hello!", tokens[1].Text);
+        Assert.Equal("    Hello!", ValueText(tokens[1]));
+        AssertNoTriviaOrDiagnostics(tokens[1]);
+
+        Assert.Equal(TokenType.StringNewline, tokens[2].Type);
+        Assert.Equal("\n", tokens[2].Text);
+        Assert.Equal("\n", ValueText(tokens[2]));
+        AssertNoTriviaOrDiagnostics(tokens[2]);
+
+        Assert.Equal(TokenType.StringContent, tokens[2].Type);
+        Assert.Equal("    Bye!", tokens[2].Text);
+        Assert.Equal("    Bye!", ValueText(tokens[2]));
+        AssertNoTriviaOrDiagnostics(tokens[2]);
+
+        Assert.Equal(TokenType.MultiLineStringEnd, tokens[3].Type);
+        Assert.Equal(quotes, tokens[3].Text);
+        Assert.Equal(2, tokens[3].LeadingTrivia.Count);
+        Assert.Equal("\n", tokens[3].LeadingTrivia[0].Text);
+        Assert.Equal("    ", tokens[3].LeadingTrivia[1].Text);
+        Assert.Empty(tokens[3].TrailingTrivia);
+        Assert.Empty(tokens[3].Diagnostics);
+
+        Assert.Equal(TokenType.EndOfInput, tokens[4].Type);
+        AssertNoTriviaOrDiagnostics(tokens[4]);
     }
 }

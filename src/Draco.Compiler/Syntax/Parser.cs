287ddbbc6f59f72da04f3227ba52f3d8fd49d59d
LPeter1997 <lenkefi.peti@gmail.com>
2022-10-17 11:02:44 +0200

    Update Parser.cs

--- src/Draco.Compiler/Syntax/Parser.cs
+++ src/Draco.Compiler/Syntax/Parser.cs
@@ -221,8 +221,7 @@ internal sealed class Parser
         {
             var input = this.Synchronize(t => t.Type switch
             {
-                TokenType.CurlyClose
-                or TokenType.KeywordFunc or TokenType.KeywordVar or TokenType.KeywordVal => false,
+                TokenType.KeywordFunc or TokenType.KeywordVar or TokenType.KeywordVal => false,
                 TokenType.Identifier when this.Peek(1).Type == TokenType.Colon => false,
                 _ => true,
             });

5769e6209e89f79a5ebb9ff54cb3f6970ef8e867
LPeter1997 <lenkefi.peti@gmail.com>
2022-10-20 17:03:43 +0200

    Added visitor interface generation

--- src/Draco.RedGreenTree.Cli/Program.cs
+++ src/Draco.RedGreenTree.Cli/Program.cs
@@ -30,6 +30,16 @@ internal sealed class RedTreeOptions
     public string TreeRoot { get; set; } = string.Empty;
 }
 
+[Verb("visitor", HelpText = "Generate visitor for the internal tree.")]
+internal sealed class VisitorOptions
+{
+    [Option('p', "project", Required = true, HelpText = "The project to read types from.")]
+    public string Project { get; set; } = string.Empty;
+
+    [Option('t', "tree-root", Required = true, HelpText = "The fully qualified name of the green tree root type.")]
+    public string TreeRoot { get; set; } = string.Empty;
+}
+
 internal class Program
 {
     private static async Task<INamedTypeSymbol?> LoadTypeFromProject(string projectPath, string typePath)
@@ -41,7 +51,11 @@ internal class Program
         return compilation?.GetTypeByMetadataName(typePath);
     }
 
-    internal static async Task Main(string[] args) => await Parser.Default.ParseArguments<GreenTreeOptions, RedTreeOptions>(args)
+    internal static async Task Main(string[] args) => await Parser.Default
+        .ParseArguments<
+            GreenTreeOptions,
+            RedTreeOptions,
+            VisitorOptions>(args)
         .MapResult(
             async (GreenTreeOptions opts) =>
             {
@@ -64,6 +78,18 @@ internal class Program
                     return 1;
                 }
                 var code = RedTreeGenerator.Generate(type);
+                Console.WriteLine(code);
+                return 0;
+            },
+            async (VisitorOptions opts) =>
+            {
+                var type = await LoadTypeFromProject(opts.Project, opts.TreeRoot);
+                if (type is null)
+                {
+                    Console.Error.WriteLine($"Could not load {opts.TreeRoot} from {opts.Project}");
+                    return 1;
+                }
+                var code = VisitorGenerator.Generate(type);
                 Console.WriteLine(code);
                 return 0;
             },

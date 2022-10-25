bde33cba074c9069a033da519ced8e480b0d6551
LPeter1997 <lenkefi.peti@gmail.com>
2022-10-25 23:42:52 +0200

    Removed System using from top-level

--- src/Draco.Compiler.Cli/Program.cs
+++ src/Draco.Compiler.Cli/Program.cs
@@ -8,14 +8,19 @@ internal class Program
 {
     internal static void Main(string[] args)
     {
-        ScriptingEngine.Execute("""
-            func abs(n: int32) =
+        ScriptingEngine.Execute($$"""
+            func abs(n: int32): int32 =
                 if (n < 0) -n
                 else n;
+
+            func fib(n: int32): int32 =
+                if (n < 2) 1
+                else fib(n - 1) + fib(n - 2);
 
             func main() {
                 println("Hello, \{1} + \{2} is \{1 + 2}");
                 println("|-12| = \{abs(-12)}");
+                println("fib(5) = \{fib(5)}");
             }
             """);
     }

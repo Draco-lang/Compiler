99141aa46e3ea71e7a2e1333fe5c658bcf769f9c
Kuinox <github@kuinox.io>
2022-10-27 20:05:30 +0200

    Renamed project and deleted some useless code.

--- src/Draco.Editor.Web/Program.cs
+++ src/Draco.Editor.Web/Program.cs
@@ -9,9 +9,6 @@ public class Program
     {
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<App>("#app");
-        builder.RootComponents.Add<HeadOutlet>("head::after");
-
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
         await builder.Build().RunAsync();
     }
 }

4af2baf1fb5c1fbc3e9e7f6a208e29dfe56a61d4
Kuinox <github@kuinox.io>
2022-10-27 20:56:03 +0200

    Fixed namespaces changes.

--- src/Draco.Editor.Web/Program.cs
+++ src/Draco.Editor.Web/Program.cs
@@ -1,8 +1,7 @@
-using Draco.WebCompiler.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace Draco.WebCompiler.Client;
+namespace Draco.Editor.Web;
 public class Program
 {
     public static async Task Main(string[] args)

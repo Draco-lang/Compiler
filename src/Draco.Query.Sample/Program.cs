33444074e5bf21c7a96bb863a2f2a58ac5b0471c
LPeter1997 <lenkefi.peti@gmail.com>
2022-10-31 22:43:09 +0100

    Added a sample project

---
+++ src/Draco.Query.Sample/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+
+namespace Draco.Query.Sample;
+
+internal class Program
+{
+    private static async QueryValueTask<int> ParseNum(string n)
+    {
+        Console.WriteLine($"ParseNum({n})");
+        return int.Parse(n);
+    }
+
+    private static async QueryValueTask<int> AddNums(string v1, string v2)
+    {
+        Console.WriteLine($"AddNums({v1}, {v2})");
+        var n1 = await ParseNum(v1);
+        var n2 = await ParseNum(v2);
+        return n1 + n2;
+    }
+
+    internal static async Task Main(string[] args)
+    {
+        var res1 = await AddNums("12", "23");
+        var res2 = await AddNums("12", "23");
+        Console.WriteLine(res1);
+        Console.WriteLine(res2);
+    }
+}

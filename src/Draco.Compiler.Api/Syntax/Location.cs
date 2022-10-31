0674d2c17cb226d0bc3d94dd49ec16c1891b234a
LPeter1997 <lenkefi.peti@gmail.com>
2022-10-31 21:00:04 +0100

    Create Location.cs

---
+++ src/Draco.Compiler.Api/Syntax/Location.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draco.Compiler.Api.Syntax;
+
+/// <summary>
+/// Represents a text position in a file.
+/// </summary>
+/// <param name="Line">The 0-based line number.</param>
+/// <param name="Column">The 0-based column number.</param>
+public readonly record struct Position(int Line, int Column);
+
+/// <summary>
+/// Represents a range in a text file.
+/// </summary>
+/// <param name="Start">The inclusive start of the range.</param>
+/// <param name="End">The exclusive end of the range.</param>
+public readonly record struct Range(Position Start, Position End);

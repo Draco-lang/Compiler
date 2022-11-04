43560f443ccf71b758fd49b696108a065e9f8ed8
thinker227 <thinker0227@gmail.com>
2022-11-04 15:41:25 +0100

    Rename T to TOk

--- src/Draco.Compiler.Internal/Utilities/Result.cs
+++ src/Draco.Compiler.Internal/Utilities/Result.cs
@@ -6,19 +6,19 @@ namespace Draco.Compiler.Internal.Utilities;
 /// <summary>
 /// A union type containing either the result of an operation or an error.
 /// </summary>
-/// <typeparam name="T">The type of the inner value.</typeparam>
+/// <typeparam name="TOk">The type of the inner ok value.</typeparam>
 /// <typeparam name="TError">The type of the inner error.</typeparam>
-public readonly struct Result<T, TError>
+public readonly struct Result<TOk, TError>
 {
-    private readonly T value;
+    private readonly TOk value;
     private readonly TError error;
     private readonly bool isSuccess;
 
     /// <summary>
-    /// The value of the result. Throws an exception if the result is not a value.
+    /// The value of the result. Throws an exception if the result is not an ok value.
     /// </summary>
     /// <exception cref="InvalidOperationException"></exception>
-    public T? Value => this.isSuccess
+    public TOk? Value => this.isSuccess
         ? this.value
         : throw new InvalidOperationException("Result does not contain a value.");
 
@@ -31,7 +31,7 @@ public readonly struct Result<T, TError>
         : this.error;
 
     /// <summary>
-    /// Whether the result is a successful value or not.
+    /// Whether the result is an ok value or not.
     /// </summary>
     [MemberNotNullWhen(true, nameof(Value))]
     [MemberNotNullWhen(false, nameof(Error))]
@@ -41,10 +41,10 @@ public readonly struct Result<T, TError>
 
 
     /// <summary>
-    /// Initializes a new <see cref="Result{T, TError}"/> with a successful value.
+    /// Initializes a new <see cref="Result{T, TError}"/> with an ok value.
     /// </summary>
-    /// <param name="value">The success value.</param>
-    public Result(T value)
+    /// <param name="value">The ok value.</param>
+    public Result(TOk value)
     {
         this.value = value;
         this.error = default!;
@@ -65,63 +65,63 @@ public readonly struct Result<T, TError>
 
 
     /// <summary>
-    /// Maps the inner value of the result to another type.
+    /// Maps the ok value of the result to another type.
     /// </summary>
-    /// <typeparam name="U">The type of the new inner value.</typeparam>
-    /// <param name="transform">A transformation function mapping the inner value.</param>
-    /// <returns>A new result of either the transformed value or the previous error.</returns>
-    public Result<U, TError> MapValue<U>(Func<T, U> transform) => this.isSuccess
+    /// <typeparam name="UOk">The type of the new ok value.</typeparam>
+    /// <param name="transform">A transformation function mapping the ok value.</param>
+    /// <returns>A new result of either the new transformed ok value or the previous error.</returns>
+    public Result<UOk, TError> MapValue<UOk>(Func<TOk, UOk> transform) => this.isSuccess
         ? new(transform(this.value))
         : new(this.error);
 
     /// <summary>
     /// Maps the inner error value of the result to another error type.
     /// </summary>
-    /// <typeparam name="UError">The type of the new inner error value.</typeparam>
-    /// <param name="transform">A transformation function mapping the inner error value.</param>
-    /// <returns>A new result of either the transformed error value or the previous successful value.</returns>
-    public Result<T, UError> MapError<UError>(Func<TError, UError> transform) => this.isSuccess
+    /// <typeparam name="UError">The type of the new error value.</typeparam>
+    /// <param name="transform">A transformation function mapping the error value.</param>
+    /// <returns>A new result of either the new transformed error value or the previous ok value.</returns>
+    public Result<TOk, UError> MapError<UError>(Func<TError, UError> transform) => this.isSuccess
         ? new(this.value)
         : new(transform(this.error));
 
     /// <summary>
-    /// Binds the inner value to a new result with another inner value type.
+    /// Binds the ok value to a new result with another ok value type.
     /// </summary>
-    /// <typeparam name="U">The type of the new inner value.</typeparam>
-    /// <param name="transform">A transformation function mapping the inner value to a new result.</param>
+    /// <typeparam name="UOk">The type of the new ok value.</typeparam>
+    /// <param name="transform">A transformation function mapping the ok value to a new result.</param>
     /// <returns>A new result of either the returned result or the previous error.</returns>
-    public Result<U, TError> BindValue<U>(Func<T, Result<U, TError>> transform) => this.isSuccess
+    public Result<UOk, TError> BindValue<UOk>(Func<TOk, Result<UOk, TError>> transform) => this.isSuccess
         ? transform(this.value)
         : new(this.error);
 
     /// <summary>
-    /// Binds the inner error value to a new result with another inner error type.
+    /// Binds the error value to a new result with another error type.
     /// </summary>
-    /// <typeparam name="UError">The type of the new inner error value.</typeparam>
-    /// <param name="transform">A transformation function mapping the inner error value to a new result.</param>
-    /// <returns>A new result of either the returned result or the previous successful value.</returns>
-    public Result<T, UError> BindError<UError>(Func<TError, Result<T, UError>> transform) =>
+    /// <typeparam name="UError">The type of the new error value.</typeparam>
+    /// <param name="transform">A transformation function mapping the error value to a new result.</param>
+    /// <returns>A new result of either the returned result or the previous ok value.</returns>
+    public Result<TOk, UError> BindError<UError>(Func<TError, Result<TOk, UError>> transform) =>
         this.isSuccess
             ? new(this.value)
             : transform(this.error);
 
     /// <summary>
-    /// Matches the inner value or error.
+    /// Matches the ok value or error.
     /// </summary>
     /// <typeparam name="TResult">The type of the resulting value.</typeparam>
-    /// <param name="ifValue">The function to apply if the result is a successful value.</param>
+    /// <param name="ifValue">The function to apply if the result is an ok value.</param>
     /// <param name="ifError">The function to apply if the result is an error value.</param>
-    /// <returns>The result of matching either the successful value or the error value.</returns>
-    public TResult Match<TResult>(Func<T, TResult> ifValue, Func<TError, TResult> ifError) => this.isSuccess
+    /// <returns>The result of matching either the ok value or the error value.</returns>
+    public TResult Match<TResult>(Func<TOk, TResult> ifValue, Func<TError, TResult> ifError) => this.isSuccess
         ? ifValue(this.value)
         : ifError(this.error);
 
     /// <summary>
-    /// Switches on the inner value or error.
+    /// Switches on the ok value or error.
     /// </summary>
-    /// <param name="ifValue">The function to apply if the result is a successful value.</param>
+    /// <param name="ifValue">The function to apply if the result is an ok value.</param>
     /// <param name="ifError">The function to apply if the result is an error value.</param>
-    public void Switch(Action<T> ifValue, Action<TError> ifError)
+    public void Switch(Action<TOk> ifValue, Action<TError> ifError)
     {
         if (this.isSuccess)
         {
@@ -136,16 +136,16 @@ public readonly struct Result<T, TError>
 
 
     /// <summary>
-    /// Implicitly converts a successful <see cref="T"/> value to a <see cref="Result{T, TError}"/>.
+    /// Implicitly converts an ok value to a <see cref="Result{T, TError}"/>.
     /// </summary>
-    /// <param name="value">The successful value to convert.</param>
-    public static implicit operator Result<T, TError>(T value) =>
+    /// <param name="value">The ok value to convert.</param>
+    public static implicit operator Result<TOk, TError>(TOk value) =>
         new(value);
 
     /// <summary>
-    /// Implicitly converts an error <see cref="TError"/> value to a <see cref="Result{T, TError}"/>.
+    /// Implicitly converts an error value to a <see cref="Result{T, TError}"/>.
     /// </summary>
     /// <param name="error">The error value to convert.</param>
-    public static implicit operator Result<T, TError>(TError error) =>
+    public static implicit operator Result<TOk, TError>(TError error) =>
         new(error);
 }

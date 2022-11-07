d1332960001f4382ffd655868120f6c9dbd85d3e
LPeter1997 <lenkefi.peti@gmail.com>
2022-11-07 11:08:17 +0100

    Removed visitor interface generation

--- src/Draco.RedGreenTree.Cli/Program.cs
+++ src/Draco.RedGreenTree.Cli/Program.cs
@@ -33,19 +33,6 @@ internal sealed class RedTreeOptions
     public string RedTreeRoot { get; set; } = string.Empty;
 }
 
-[Verb("visitor-interface", HelpText = "Generate visitor interface for the internal tree.")]
-internal sealed class VisitorInterfaceOptions
-{
-    [Option('p', "project", Required = true, HelpText = "The project to read types from.")]
-    public string Project { get; set; } = string.Empty;
-
-    [Option('t', "tree-root", Required = true, HelpText = "The fully qualified name of the green tree root type.")]
-    public string TreeRoot { get; set; } = string.Empty;
-
-    [Option('v', "visitor", Required = true, HelpText = "The fully qualified name of the visitor type to base the generation on.")]
-    public string Visitor { get; set; } = string.Empty;
-}
-
 [Verb("visitor-base", HelpText = "Generate visitor base for the internal tree.")]
 internal sealed class VisitorBaseOptions
 {
@@ -69,7 +56,6 @@ internal class Program
         await Parser.Default.ParseArguments<
             GreenTreeOptions,
             RedTreeOptions,
-            VisitorInterfaceOptions,
             VisitorBaseOptions>(args)
         .MapResult(
             async (GreenTreeOptions opts) =>
@@ -103,26 +89,6 @@ internal class Program
                     return 1;
                 }
                 var code = RedTreeGenerator.Generate(new(greenRootType: greenRootType, redRootType: redRootType));
-                Console.WriteLine(code);
-                return 0;
-            },
-            async (VisitorInterfaceOptions opts) =>
-            {
-                var project = await workspace.OpenProjectAsync(opts.Project);
-                var compilation = await project.GetCompilationAsync();
-                var rootType = compilation?.GetTypeByMetadataName(opts.TreeRoot);
-                if (rootType is null)
-                {
-                    Console.Error.WriteLine($"Could not load {opts.TreeRoot} from {opts.Project}");
-                    return 1;
-                }
-                var visitorType = compilation?.GetTypeByMetadataName(opts.Visitor);
-                if (visitorType is null)
-                {
-                    Console.Error.WriteLine($"Could not load {opts.Visitor} from {opts.Project}");
-                    return 1;
-                }
-                var code = VisitorInterfaceGenerator.Generate(new(rootType: rootType, visitorType: visitorType));
                 Console.WriteLine(code);
                 return 0;
             },

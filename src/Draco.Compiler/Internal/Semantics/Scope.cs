38292714999e97602f48c059d84631ab7a6b3133
LPeter1997 <lenkefi.peti@gmail.com>
2022-11-09 21:39:48 +0100

    Update Scope.cs

--- src/Draco.Compiler/Internal/Semantics/Scope.cs
+++ src/Draco.Compiler/Internal/Semantics/Scope.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,7 +33,85 @@ internal enum ScopeKind
 /// Represents a single scope.
 /// </summary>
 /// <param name="Kind">The kind of scope.</param>
-/// <param name="Symbols">The symbols within this scope.</param>
+/// <param name="Timelines">The symbol names in this scope associated with their
+/// <see cref="DeclarationTimeline"/>s.</param>
 internal sealed record class Scope(
     ScopeKind Kind,
-    ImmutableDictionary<string, Symbol> Symbols);
+    ImmutableDictionary<string, DeclarationTimeline> Timelines)
+{
+    /// <summary>
+    /// Attempts to look up a <see cref="Declaration"/> with a given name.
+    /// </summary>
+    /// <param name="name">The name of the <see cref="Declaration"/> to look for.</param>
+    /// <param name="referencedPosition">The position we allow lookup up until.</param>
+    /// <returns>The <see cref="Declaration"/> that has name <paramref name="name"/> and is visible from
+    /// position <paramref name="referencedPosition"/>, or null if there is none such.</returns>
+    public Declaration? LookUp(string name, int referencedPosition)
+    {
+        if (!this.Timelines.TryGetValue(name, out var timeline)) return null;
+        return timeline.LookUp(referencedPosition);
+    }
+}
+
+/// <summary>
+/// Represents the timeline of <see cref="Symbol"/>s that are introduced in the same <see cref="Scope"/>
+/// under the same name.
+/// </summary>
+internal readonly struct DeclarationTimeline
+{
+    /// <summary>
+    /// The <see cref="Declaration"/>s that introduce the <see cref="Symbol"/>s.
+    /// </summary>
+    public readonly ImmutableArray<Declaration> Declarations;
+
+    public DeclarationTimeline(IEnumerable<Declaration> declarations)
+    {
+        // Either there are no declarations, or all of them have the same name
+        Debug.Assert(!declarations.Any()
+                   || declarations.All(d => d.Name == declarations.First().Name));
+        this.Declarations = declarations
+            .OrderBy(decl => decl.Position)
+            .ToImmutableArray();
+    }
+
+    /// <summary>
+    /// Looks up a <see cref="Declaration"/> in this timeline.
+    /// </summary>
+    /// <param name="referencedPosition">The position we are trying to reference in the timeline.</param>
+    /// <returns>The <see cref="Declaration"/> that is the latest, but at most at
+    /// <paramref name="referencedPosition"/>, or null if there is none such declaration.</returns>
+    public Declaration? LookUp(int referencedPosition)
+    {
+        var comparer = Comparer<Declaration>.Create((d1, d2) => d1.Position - d2.Position);
+        var searchKey = new Declaration(referencedPosition, null!);
+        var index = this.Declarations.BinarySearch(searchKey);
+        if (index >= 0)
+        {
+            // Exact match, can reference
+            return this.Declarations[index];
+        }
+        else
+        {
+            // We are in-between, we need to get the previous one, which is defined
+            index = ~index - 1;
+            // Not found
+            if (index < 0) return null;
+            // Found one
+            return this.Declarations[index];
+        }
+    }
+}
+
+/// <summary>
+/// Represents the declaration of a <see cref="Semantics.Symbol"/> in a <see cref="Scope"/>.
+/// </summary>
+/// <param name="Position">The relative position of the delcaration relative to the containing scope.
+/// The position is where the symbol is available from.</param>
+/// <param name="Symbol">The declared <see cref="Semantics.Symbol"/>.</param>
+internal readonly record struct Declaration(int Position, Symbol Symbol)
+{
+    /// <summary>
+    /// The name of the contained <see cref="Symbol"/>.
+    /// </summary>
+    public string Name => this.Symbol.Name;
+}

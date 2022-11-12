4d458e5b030249fa8d0f832ef16ade53d85ea834
Kuinox <github@kuinox.io>
2022-11-12 20:38:55 +0100

    More comments.

--- src/Draco.Editor.Web/app/build.js
+++ src/Draco.Editor.Web/app/build.js
@@ -4,10 +4,13 @@ import * as fs from 'fs';
 import { fileURLToPath } from 'url';
 import { createThemeBasedLogo as getFavicon } from './favicon-downloader.js';
 
+// This file manage the build process of the webapp.
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
+// this is a plugin to handle wasm file. We handle it like it was a binary file.
 let wasmPlugin = {
     name: 'wasm',
     setup(build) {
@@ -24,6 +27,8 @@ const workerEntryPoints = [
 
 const distDir = process.argv[2];
 const outDir = path.join(__dirname, distDir);
+
+// Bundle monaco editor workers.
 build({
     entryPoints: workerEntryPoints.map((entry) => `node_modules/monaco-editor/esm/${entry}`),
     bundle: true,
@@ -31,10 +36,11 @@ build({
     outdir: outDir
 });
 
+// Bundle our app.
 build({
     entryPoints: ['ts/app.ts', 'css/app.css'],
     bundle: true,
-    format: 'esm',
+    format: 'esm', // We want ESM to use import in Blazor.
     outdir: outDir,
     loader: {
         '.ttf': 'file'
@@ -44,7 +50,7 @@ build({
 });
 
 if (!fs.existsSync(outDir)) fs.mkdirSync(outDir);
-fs.copyFileSync('index.html', path.join(outDir, 'index.html'),);
+fs.copyFileSync('index.html', path.join(outDir, 'index.html'),); // Copy index.html to wwwroot.
 
 const favicon = await getFavicon();
-fs.writeFileSync(path.join(outDir, 'favicon.svg'), favicon);
+fs.writeFileSync(path.join(outDir, 'favicon.svg'), favicon); // Write favicon to wwwroot.

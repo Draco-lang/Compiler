67791e103da2b365e681d2b6b1ef9490f236a8b2
jakubbinter <76395771+jakubbinter@users.noreply.github.com>
2022-11-13 19:48:49 +0100

    hopefully working favicon (#108)

--- src/Draco.Editor.Web/app/favicon-downloader.js
+++ src/Draco.Editor.Web/app/favicon-downloader.js
@@ -3,17 +3,25 @@ function stripXMLHeader(xml) {
     return xml.slice(indexOf);
 }
 
+function stripViewBox(xml) {
+    const indexOfViewBox = xml.indexOf('viewBox');
+    const nextNewline = xml.substring(indexOfViewBox).indexOf('\n') + indexOfViewBox;
+    return xml.slice(0, indexOfViewBox) + xml.slice(nextNewline);
+}
+
 export async function createThemeBasedLogo() {
     const bodyLight = await (await fetch('https://raw.githubusercontent.com/Draco-lang/Language-suggestions/main/Resources/Logo-Short.svg')).text();
     let logoLight = stripXMLHeader(bodyLight);
+    logoLight = stripViewBox(logoLight);
     // logoLight = optimize(logoLight).data;
 
     const bodyDark = await (await fetch('https://raw.githubusercontent.com/Draco-lang/Language-suggestions/main/Resources/Logo-Short-Inverted.svg')).text();
     let logoDark = stripXMLHeader(bodyDark);
+    logoDark = stripViewBox(logoDark);
     // logoDark = optimize(logoDark).data;
     const logoSvg =
         `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-        <svg xmlns="http://www.w3.org/2000/svg">
+        <svg viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg">
             <style>
             @media (prefers-color-scheme: dark) {
                 .light{

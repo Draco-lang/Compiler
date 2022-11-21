618beeadfa8acfcc77c57672072a33da294bc09f
jakubbinter <kuba.binter@gmail.com>
2022-11-21 20:07:39 +0100

    Removed nullables

--- src/Draco.Compiler/Internal/Semantics/AbstractSyntaxTree.cs
+++ src/Draco.Compiler/Internal/Semantics/AbstractSyntaxTree.cs
@@ -9,126 +9,128 @@ using Draco.Compiler.Internal.Syntax;
 
 namespace Draco.Compiler.Internal.Semantics;
 
-internal abstract partial record class AbstractSyntaxTree
+internal abstract record class AbstractSyntaxTree
 {
-    public abstract partial record class Decl : AbstractSyntaxTree
+    public abstract record class Decl : AbstractSyntaxTree
     {
         /// <summary>
         /// A function declaration.
         /// </summary>
-        public sealed partial record class Func(
+        public sealed record class Func(
             Symbol DeclarationSymbol,
             ImmutableArray<Symbol> Params,
-            Symbol? ReturnType,
+            Symbol ReturnType,
             FuncBody Body) : Decl;
 
         /// <summary>
         /// A label declaration.
         /// </summary>
-        public sealed partial record class Label(
+        public sealed record class Label(
             Symbol LabelSymbol) : Decl;
 
         /// <summary>
         /// A variable declaration.
         /// </summary>
-        public sealed partial record class Variable(
+        public sealed record class Variable(
             Symbol DeclarationSymbol,
-            Symbol? Type,
+            Symbol Type,
             Expr Value) : Decl;
     }
 
     /// <summary>
     /// A function body
     /// </summary>
-    public partial record class FuncBody : AbstractSyntaxTree
+    public record class FuncBody : AbstractSyntaxTree
     {
         /// <summary>
         /// A block function body.
         /// </summary>
-        public sealed partial record class BlockBody(
+        public sealed record class BlockBody(
             Expr.Block Block) : FuncBody;
 
         /// <summary>
         /// An in-line function body.
         /// </summary>
-        public sealed partial record class InlineBody(
+        public sealed record class InlineBody(
             Expr Expression) : FuncBody;
     }
 
-    public abstract partial record class Expr : AbstractSyntaxTree
+    public abstract record class Expr : AbstractSyntaxTree
     {
+        public record class Unit() : Expr;
         /// <summary>
         /// A block expression
         /// </summary>
-        public partial record class Block(
+        public record class Block(
             ImmutableArray<Stmt> Statements,
-            Expr? Value) : Expr;
+            Expr Value) : Expr;
 
         /// <summary>
         /// A literal expression, i.e. a number, string, boolean value, etc.
         /// </summary>
-        public sealed partial record class Literal(
-            Symbol Value) : Expr;
+        public sealed record class Literal(
+            object Value,
+            Symbol Type) : Expr;
 
         /// <summary>
         /// An if-expression with an option else clause.
         /// </summary>
-        public sealed partial record class If(
+        public sealed record class If(
             Expr Condition,
             Expr Then,
-            Expr? Else) : Expr;
+            Expr Else) : Expr;
 
         /// <summary>
         /// A while-expression.
         /// </summary>
-        public sealed partial record class While(
+        public sealed record class While(
             Expr Condition,
             Expr Expression) : Expr;
 
         /// <summary>
         /// A goto-expression.
         /// </summary>
-        public sealed partial record class Goto(
+        public sealed record class Goto(
             Symbol Target) : Expr;
 
         /// <summary>
         /// A return-expression.
         /// </summary>
-        public sealed partial record class Return(
-            Expr? Expression) : Expr;
+        public sealed record class Return(
+            Expr Expression) : Expr;
 
         /// <summary>
         /// Any call expression.
         /// </summary>
-        public sealed partial record class Call(
+        public sealed record class Call(
             Expr Called,
             ImmutableArray<Expr> Args) : Expr;
 
         /// <summary>
         /// Any index expression.
         /// </summary>
-        public sealed partial record class Index(
+        public sealed record class Index(
             Expr Called,
             ImmutableArray<Expr> Args) : Expr;
 
         /// <summary>
         /// A member access expression.
         /// </summary>
-        public sealed partial record class MemberAccess(
+        public sealed record class MemberAccess(
             Expr Object,
             Symbol MemberName) : Expr;
 
         /// <summary>
         /// A unary expression.
         /// </summary>
-        public sealed partial record class Unary(
+        public sealed record class Unary(
             Symbol Operator,
             Expr Operand) : Expr;
 
         /// <summary>
         /// A binary expression, including assignment and compound assignment.
         /// </summary>
-        public sealed partial record class Binary(
+        public sealed record class Binary(
             Expr Left,
             Symbol Operator,
             Expr Right) : Expr;
@@ -136,61 +138,54 @@ internal abstract partial record class AbstractSyntaxTree
         /// <summary>
         /// A relational expression chain.
         /// </summary>
-        public sealed partial record class Relational(
+        public sealed record class Relational(
             Expr Left,
             ImmutableArray<ComparisonElement> Comparisons) : Expr;
 
         /// <summary>
-        /// A grouping expression, enclosing a sub-expression.
-        /// </summary>
-        public sealed partial record class Grouping(
-            Expr Expression) : Expr;
-
-        /// <summary>
         /// A string expression composing string content and interpolation.
         /// </summary>
-        public sealed partial record class String(
+        public sealed record class String(
             ImmutableArray<StringPart> Parts) : Expr;
     }
 
     /// <summary>
     /// Part of a string literal/expression.
     /// </summary>
-    public abstract partial record class StringPart : AbstractSyntaxTree
+    public abstract record class StringPart : AbstractSyntaxTree
     {
-
         /// <summary>
         /// Content part of a string literal.
         /// </summary>
-        public sealed partial record class Content(
+        public sealed record class Content(
             Symbol Value) : StringPart;
 
         /// <summary>
         /// An interpolation hole.
         /// </summary>
-        public sealed partial record class Interpolation(
+        public sealed record class Interpolation(
             Expr Expression) : StringPart;
     }
 
     /// <summary>
     /// A single comparison element in a comparison chain.
     /// </summary>
-    public partial record class ComparisonElement(
+    public record class ComparisonElement(
         Symbol Operator,
         Expr Right) : AbstractSyntaxTree;
 
-    public abstract partial record class Stmt : AbstractSyntaxTree
+    public abstract record class Stmt : AbstractSyntaxTree
     {
         /// <summary>
         /// A declaration statement.
         /// </summary>
-        public new sealed partial record class Decl(
+        public new sealed record class Decl(
             AbstractSyntaxTree.Decl Declaration) : Stmt;
 
         /// <summary>
         /// An expression statement.
         /// </summary>
-        public new sealed partial record class Expr(
+        public new sealed record class Expr(
             ParseTree.Expr Expression) : Stmt;
     }
 }

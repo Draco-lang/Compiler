6b354e61e740402f559ac6695db1acf38e6b88f2
jakubbinter <kuba.binter@gmail.com>
2022-11-26 12:43:54 +0100

    removed unnecesarry suings from Formatter

--- src/Draco.Compiler/Api/Syntax/ParseTree.cs
+++ src/Draco.Compiler/Api/Syntax/ParseTree.cs
@@ -1,18 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 using Draco.Compiler.Api.Diagnostics;
 using Draco.Compiler.Internal.Syntax;
-using Draco.Compiler.Internal.Utilities;
 using Draco.RedGreenTree.Attributes;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Draco.Compiler.Api.Syntax;
 
--- src/Draco.LanguageServer/Handlers/DracoFormattingHandler.cs
+++ src/Draco.LanguageServer/Handlers/DracoFormattingHandler.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Draco.Compiler.Api.Syntax;
 using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
 using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
-using OmniSharp.Extensions.LanguageServer.Protocol.Server;
 
 namespace Draco.LanguageServer.Handlers;
 

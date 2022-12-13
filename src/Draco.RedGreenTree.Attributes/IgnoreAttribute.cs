8cee820f8e05878532a1ab257f18f8deb89c3cbe
LPeter1997 <lenkefi.peti@gmail.com>
2022-12-13 20:17:57 +0100

    Basic headers generated

--- src/Draco.RedGreenTree.Attributes/IgnoreAttribute.cs
+++ src/Draco.RedGreenTree.Attributes/IgnoreAttribute.cs
@@ -8,9 +8,10 @@ public enum IgnoreFlags
     TransformerTransform = 1 << 0,
     TransformerAll = 1 << 1,
     VisitorVisit = 1 << 2,
+    SyntaxFactoryConstruct = 1 << 3,
 }
 
-[AttributeUsage(AttributeTargets.Property)]
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Class)]
 public sealed class IgnoreAttribute : Attribute
 {
     public IgnoreFlags Flags { get; }
--- src/Draco.RedGreenTree/SyntaxFactoryGenerator.cs
+++ src/Draco.RedGreenTree/SyntaxFactoryGenerator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Draco.RedGreenTree.Attributes;
 using Microsoft.CodeAnalysis;
 
 namespace Draco.RedGreenTree;
@@ -38,10 +39,12 @@ public sealed class SyntaxFactoryGenerator : GeneratorBase
     }
 
     private readonly Settings settings;
+    private readonly HashSet<INamedTypeSymbol> greenTreeNodes;
     private readonly HashSet<INamedTypeSymbol> typesInParseTree = new(SymbolEqualityComparer.Default);
+    private readonly HashSet<string> generatedMethodNames = new();
+    private readonly HashSet<string> customMethodNames;
     private readonly CodeWriter headerWriter = new();
     private readonly CodeWriter contentWriter = new();
-    private readonly HashSet<string> customMethodNames;
 
     private INamedTypeSymbol FactoryType => this.settings.FactoryType;
     private INamedTypeSymbol GreenRootType => this.settings.GreenRootType;
@@ -51,13 +54,14 @@ public sealed class SyntaxFactoryGenerator : GeneratorBase
     private string GetRedClassName(INamedTypeSymbol type) => SymbolEquals(type, this.GreenRootType)
         ? this.RedRootType.Name
         : this.settings.GetRedName(type);
-    private string GetFullRedClassName(INamedTypeSymbol type)
+    private string GetFullRedClassName(ITypeSymbol type)
     {
-        if (!this.typesInParseTree.Contains(type) && !type.IsSubtypeOf(this.settings.GreenRootType))
+        if (type is not INamedTypeSymbol namedType) return type.ToDisplayString();
+        if (!this.typesInParseTree.Contains(namedType) && !namedType.IsSubtypeOf(this.settings.GreenRootType))
         {
             return type.ToDisplayString();
         }
-        var typeName = string.Join(".", type.EnumerateNestingChain().Select(this.GetRedClassName));
+        var typeName = string.Join(".", namedType.EnumerateNestingChain().Select(this.GetRedClassName));
         if (this.RedRootType.ContainingNamespace is null) return typeName;
         return $"{this.RedRootType.ContainingNamespace.ToDisplayString()}.{typeName}";
     }
@@ -67,8 +71,19 @@ public sealed class SyntaxFactoryGenerator : GeneratorBase
         this.settings = settings;
         foreach (var item in settings.GreenRootType.EnumerateContainedTypeTree()) this.typesInParseTree.Add(item);
 
+        this.greenTreeNodes = new(SymbolEqualityComparer.Default);
+        this.ExtractGreenTreeNodes();
+
         this.customMethodNames = new();
         this.ExtractCustomFactoryMethods();
+    }
+
+    private void ExtractGreenTreeNodes()
+    {
+        var relevantNodes = this.GreenRootType
+            .EnumerateContainedTypeTree();
+        foreach (var n in relevantNodes) this.greenTreeNodes.Add(n);
+        foreach (var n in this.greenTreeNodes) this.generatedMethodNames.Add(this.GenerateFactoryMethodName(n));
     }
 
     private void ExtractCustomFactoryMethods()
@@ -117,7 +132,9 @@ public sealed class SyntaxFactoryGenerator : GeneratorBase
         // Opening
         this.contentWriter
             .Write(this.FactoryType.DeclaredAccessibility)
-            .Write(this.FactoryType.GetTypeKind(partial: true));
+            .Write(this.FactoryType.GetTypeKind(partial: true))
+            .Write(this.FactoryType.Name)
+            .Write("{");
 
         foreach (var type in this.GreenRootType.EnumerateContainedTypeTree())
         {
@@ -132,6 +149,103 @@ public sealed class SyntaxFactoryGenerator : GeneratorBase
 
     private void GenerateRedFactory(INamedTypeSymbol greenType)
     {
-        // TODO
+        if (greenType.IsAbstract) return;
+        if (HasFlag(greenType, IgnoreFlags.SyntaxFactoryConstruct)) return;
+
+        var redName = this.GetFullRedClassName(greenType);
+        var factoryName = this.GenerateFactoryMethodName(greenType);
+
+        if (this.customMethodNames.Contains(factoryName)) return;
+
+        // Header
+        this.contentWriter
+            .Write(Accessibility.Public)
+            .Write("static")
+            .Write(redName)
+            .Write(factoryName)
+            .Write("(");
+        // Args
+        this.contentWriter.Write(string.Join(
+            ", ",
+            greenType
+                .GetMembers()
+                .OfType<IMethodSymbol>()
+                .Where(m => m.MethodKind == MethodKind.Constructor)
+                .SelectMany(m => m.Parameters)
+                .Select(param => $"{this.TranslareToRedType(param.Type)} {ToCamelCase(param.Name)}")));
+        this.contentWriter.Write(")");
+
+        // Body
+        this.contentWriter.Write("=> throw new System.NotImplementedException();");
+    }
+
+    private string GenerateFactoryMethodName(INamedTypeSymbol symbol)
+    {
+        // Nullable value types are unwrapped
+        if (symbol.IsValueType
+         && symbol.NullableAnnotation == NullableAnnotation.Annotated
+         && symbol.TypeArguments[0] is INamedTypeSymbol namedTypeArg)
+        {
+            symbol = namedTypeArg;
+        }
+        // For anything not part of the tree, we just generate a NAME
+        if (!this.greenTreeNodes.Contains(symbol)) return symbol.Name;
+        // For anything else, we read up the names in reverse order, excluding the root
+        var parts = symbol.EnumerateNestingChain().Skip(1).Reverse().Select(this.GetRedClassName);
+        return string.Join("", parts);
+    }
+
+    private string TranslareToRedType(ITypeSymbol symbol)
+    {
+        // Not named, we don't deal with it
+        if (symbol is not INamedTypeSymbol namedSymbol) return symbol.ToDisplayString();
+
+        var isNullable = namedSymbol.NullableAnnotation == NullableAnnotation.Annotated;
+        var nullableSuffix = isNullable ? "?" : string.Empty;
+        // Any nullable value type, special case
+        if (namedSymbol.IsValueType && isNullable)
+        {
+            var subtype = this.TranslareToRedType(namedSymbol.TypeArguments[0]);
+            return $"{subtype}?";
+        }
+        // Tuple types
+        if (namedSymbol.IsTupleType)
+        {
+            var elements = namedSymbol
+                .TupleElements
+                .Select(e =>
+                {
+                    var type = this.TranslareToRedType(e.Type);
+                    return $"{type} {e.Name}";
+                }).ToList();
+            return $"({string.Join(", ", elements)})";
+        }
+        // Generic types
+        if (namedSymbol.IsGenericType)
+        {
+            var originalDef = namedSymbol.OriginalDefinition;
+            var name = this.GetFullRedClassName(originalDef);
+            var genericsStart = name.IndexOf('<');
+            if (genericsStart >= 0) name = name.Substring(0, genericsStart);
+            var typeArgs = namedSymbol.TypeArguments.Select(this.TranslareToRedType).ToList();
+            return $"{name}<{string.Join(", ", typeArgs)}>{nullableSuffix}";
+        }
+        // Green subclasses
+        if (namedSymbol.IsSubtypeOf(this.GreenRootType))
+        {
+            return $"{this.GetFullRedClassName(namedSymbol)}{nullableSuffix}";
+        }
+        // Anything else
+        return symbol.ToDisplayString();
+    }
+
+    private static bool HasFlag(INamedTypeSymbol type, IgnoreFlags ignoreFlags)
+    {
+        if (type.HasAttribute(typeof(IgnoreAttribute), out var args))
+        {
+            var flags = (IgnoreFlags)args[0]!;
+            return flags.HasFlag(ignoreFlags);
+        }
+        return false;
     }
 }

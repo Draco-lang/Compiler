560878a0c7d7d873c2d1a32afabb95c0599f5ab1
jakubbinter <kuba.binter@gmail.com>
2022-12-18 19:43:06 +0100

    Start cfg builder

--- src/Draco.Compiler/Internal/Semantics/DFA/CFG.cs
+++ src/Draco.Compiler/Internal/Semantics/DFA/CFG.cs
@@ -1,16 +1,17 @@
-using System.Collections.Generic;
+using System.Collections.Immutable;
 using Draco.Compiler.Internal.Semantics.AbstractSyntax;
 
 namespace Draco.Compiler.Internal.Semantics.DFA;
 
 internal sealed record class CFG
 {
+    // TODO: add builder
     /// <summary>
     /// Represents a single block in <see cref="CFG"/> consisting non-branching statements, the block ends branching.
     /// </summary>
     /// <param name="Statements">List of statements contained in the <see cref="Block"/>.</param>
     /// <param name="Branches">List of possible <see cref="Branch"/>es.</param>
-    internal sealed record class Block(List<Ast.Stmt> Statements, List<Branch> Branches);
+    internal sealed record class Block(ImmutableArray<Ast.Stmt> Statements, ImmutableArray<Branch> Branches);
 
     /// <summary>
     /// Represents single <see cref="Branch"/>.
@@ -18,4 +19,9 @@ internal sealed record class CFG
     /// <param name="Target">The <see cref="Block"/> targeted by this <see cref="Branch"/>.</param>
     /// <param name="Condition">The condition that must be met so the execution continues to the <paramref name="Target"/>.</param>
     internal sealed record class Branch(Block Target, Ast.Expr Condition);
+
+    internal sealed record class Builder
+    {
+        private Stack
+    }
 }
--- src/Draco.Compiler/Internal/Semantics/DFA/CFGBuilder.cs
+++ src/Draco.Compiler/Internal/Semantics/DFA/CFGBuilder.cs
@@ -1,11 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using Draco.Compiler.Internal.Semantics.AbstractSyntax;
 
 namespace Draco.Compiler.Internal.Semantics.DFA;
 
 internal sealed class CFGBuilder
 {
+    // TODO: rework the api to something like this:
+    // cfg.PushStatement(var_x);
+    // cfg.StartBranching();
+    // cfg.PushBranch(x_is_0);
+    // // build then block
+    // cfg.PopBranch();
+    // cfg.PushBranch(@else);
+    // // build else block
+    // cfg.PopBranch();
+    // cfg.EndBranching();
     public static CFG.Block ToCFG(Ast node)
     {
         if (node is Ast.Expr.Block) return ToCFG((Ast.Expr.Block)node);
@@ -27,6 +38,17 @@ internal sealed class CFGBuilder
             else if (stmt is Ast.Stmt.Decl decl && decl.Declaration is Ast.Decl.Label) throw new NotImplementedException();
             statements.Add(stmt);
         }
-        return new CFG.Block(statements, new List<CFG.Branch>());
+        return new CFG.Block(statements.ToImmutableArray(), new ImmutableArray<CFG.Branch>());
+    }
+
+    public static CFG.Block ToCFG(Ast.Expr.If expr)
+    {
+        // TODO: return builder type
+        var branchThen = new CFG.Branch(ToCFG(expr.Then), expr.Condition);
+        if (expr.Else != Ast.Expr.Unit.Default)
+        {
+            throw new NotImplementedException();
+        }
+        return new CFG.Block(new ImmutableArray<Ast.Stmt>(), new ImmutableArray<CFG.Branch>() { branchThen });
     }
 }

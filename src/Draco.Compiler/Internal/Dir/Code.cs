975d0b8dd67246c7acbd62b809f28734f0b438eb
LPeter1997 <lenkefi.peti@gmail.com>
2022-12-18 12:38:25 +0100

    Moved

--- src/Draco.Compiler/Internal/Dir/Code.cs
+++ src/Draco.Compiler/Internal/Dir/Code.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Draco.Compiler.Internal.Codegen.Dir;
+namespace Draco.Compiler.Internal.Dir;
 
 /// <summary>
 /// A unit of compilation resulting in a single binary.

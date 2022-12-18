73b85e09d8d86fa9b54c0fa3227419aa515818c8
LPeter1997 <lenkefi.peti@gmail.com>
2022-12-18 17:53:00 +0100

    Update Model.cs

--- src/Draco.Compiler/Internal/DracoIr/Model.cs
+++ src/Draco.Compiler/Internal/DracoIr/Model.cs
@@ -49,23 +49,83 @@ internal abstract partial record class Instr
 internal abstract partial record class Instr
 {
     /// <summary>
+    /// Returns from the procedure.
+    /// </summary>
+    /// <param name="Value">The value to return.</param>
+    public sealed record class Ret(Value Value) : Instr
+    {
+        public override bool IsJump => true;
+    }
+
+    /// <summary>
+    /// Unconditionally jumps to the given target.
+    /// </summary>
+    /// <param name="Target">The target to jump to.</param>
+    public sealed record class Jmp(BasicBlock Target) : Instr
+    {
+        public override bool IsJump => true;
+    }
+
+    /// <summary>
+    /// Conditionally jumps to one target or the other.
+    /// </summary>
+    /// <param name="Condition">Condition to base the jump on.</param>
+    /// <param name="TrueTarget">The target to jump to if <paramref name="Condition"/> is true.</param>
+    /// <param name="FalseTarget">The target to jump to if <paramref name="Condition"/> is false.</param>
+    public sealed record class JmpIf(
+        Value Condition,
+        BasicBlock TrueTarget,
+        BasicBlock FalseTarget) : Instr
+    {
+        public override bool IsJump => true;
+    }
+
+    /// <summary>
     /// Adds together the two integer operands.
     /// </summary>
-    /// <param name="Target">The target register to store in.</param>
+    /// <param name="Target">The target register to store the result in.</param>
     /// <param name="Left">The left operand.</param>
     /// <param name="Right">The right operand.</param>
     public sealed record class AddInt(Value.Reg Target, Value Left, Value Right) : Instr;
+
+    /// <summary>
+    /// Less-than compares the two integer operands.
+    /// </summary>
+    /// <param name="Target">The target register to store the result in.</param>
+    /// <param name="Left">The left operand.</param>
+    /// <param name="Right">The right operand.</param>
+    public sealed record class LessInt(Value.Reg Target, Value Left, Value Right) : Instr;
+
+    /// <summary>
+    /// Less-or-equal compares the two integer operands.
+    /// </summary>
+    /// <param name="Target">The target register to store the result in.</param>
+    /// <param name="Left">The left operand.</param>
+    /// <param name="Right">The right operand.</param>
+    public sealed record class LessEqualInt(Value.Reg Target, Value Left, Value Right) : Instr;
+
+    /// <summary>
+    /// Equality compares the two integer operands.
+    /// </summary>
+    /// <param name="Target">The target register to store the result in.</param>
+    /// <param name="Left">The left operand.</param>
+    /// <param name="Right">The right operand.</param>
+    public sealed record class EqualInt(Value.Reg Target, Value Left, Value Right) : Instr;
+}
+
+// Constants
+internal abstract partial record class Type
+{
+    public static readonly Builtin Void = new(typeof(void));
+    public static readonly Builtin Int32 = new(typeof(int));
+    public static readonly Builtin Bool = new(typeof(bool));
 }
 
 /// <summary>
 /// The base of all IR types.
 /// </summary>
-internal abstract record class Type
+internal abstract partial record class Type
 {
-    public static readonly Builtin Void = new(typeof(void));
-    public static readonly Builtin Int32 = new(typeof(int));
-    public static readonly Builtin Bool = new(typeof(bool));
-
     /// <summary>
     /// A builtin type.
     /// </summary>
@@ -76,17 +136,32 @@ internal abstract record class Type
 /// <summary>
 /// The base of all IR values that can be used as operands.
 /// </summary>
-internal abstract record class Value
+internal abstract partial record class Value
+{
+    /// <summary>
+    /// The type of the value.
+    /// </summary>
+    public abstract Type Type { get; }
+}
+
+// Implementations
+internal abstract partial record class Value
 {
     /// <summary>
     /// Represents a procedure parameter.
     /// </summary>
     /// <param name="Type">The type of the parameter.</param>
-    public sealed record class Param(Type Type) : Value;
+    public sealed record class Param(Type Type) : Value
+    {
+        public override Type Type { get; } = Type;
+    }
 
     /// <summary>
     /// A single virtual register that can be only assigned once.
     /// </summary>
     /// <param name="Type">The type of the value the register can store.</param>
-    public sealed record class Reg(Type Type) : Value;
+    public sealed record class Reg(Type Type) : Value
+    {
+        public override Type Type { get; } = Type;
+    }
 }

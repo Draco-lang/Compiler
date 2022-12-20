73d01e710b54bedd4c65e10950182404e7eed78c
LPeter1997 <lenkefi.peti@gmail.com>
2022-12-20 21:20:33 +0100

    Update Model.cs

--- src/Draco.Compiler/Internal/DracoIr/Model.cs
+++ src/Draco.Compiler/Internal/DracoIr/Model.cs
@@ -73,7 +73,7 @@ internal interface IReadOnlyInstruction
     /// <typeparam name="T">The type of the operand.</typeparam>
     /// <param name="index">The indef of the operand.</param>
     /// <returns>The operand at index <paramref name="index"/>.</returns>
-    public T OperandAt<T>(int index);
+    public T GetOperandAt<T>(int index);
 }
 
 /// <summary>
@@ -109,4 +109,169 @@ internal enum InstructionKind
 
 // Implementations /////////////////////////////////////////////////////////////
 
-// TODO
+/// <summary>
+/// A base class for instructions.
+/// </summary>
+internal abstract partial class Instruction : IReadOnlyInstruction
+{
+    private static readonly InstructionKind[] branchInstructions = new[]
+    {
+        InstructionKind.Ret,
+        InstructionKind.Jmp,
+        InstructionKind.JmpIf,
+    };
+
+    public InstructionKind Kind { get; set; }
+    public bool IsBranch => branchInstructions.Contains(this.Kind);
+
+    public Instruction(InstructionKind kind)
+    {
+        this.Kind = kind;
+    }
+
+    public abstract T GetOperandAt<T>(int index);
+    public abstract void SetOperandAt<T>(int index, T value);
+}
+
+// Implementations
+internal abstract partial class Instruction
+{
+    private sealed class Instruction0 : Instruction
+    {
+        public Instruction0(InstructionKind kind)
+            : base(kind)
+        {
+        }
+
+        public override T GetOperandAt<T>(int index) => throw new NotSupportedException("nullary instruction has no operands");
+        public override void SetOperandAt<T>(int index, T value) => throw new NotSupportedException("nullary instruction has no operands");
+    }
+
+    private sealed class Instruction1<T1> : Instruction
+    {
+        private T1 operand1;
+
+        public Instruction1(InstructionKind kind, T1 operand1)
+            : base(kind)
+        {
+            this.operand1 = operand1;
+        }
+
+        public override T GetOperandAt<T>(int index)
+        {
+            if (index != 0) throw new ArgumentOutOfRangeException(nameof(index));
+            if (typeof(T) != typeof(T1)) throw new InvalidOperationException("invalid operand type");
+            return (T)(object)this.operand1!;
+        }
+
+        public override void SetOperandAt<T>(int index, T value)
+        {
+            if (index != 0) throw new ArgumentOutOfRangeException(nameof(index));
+            if (typeof(T) != typeof(T1)) throw new InvalidOperationException("invalid operand type");
+            this.operand1 = (T1)(object)value!;
+        }
+    }
+
+    private sealed class Instruction2<T1, T2> : Instruction
+    {
+        private T1 operand1;
+        private T2 operand2;
+
+        public Instruction2(InstructionKind kind, T1 operand1, T2 operand2)
+            : base(kind)
+        {
+            this.operand1 = operand1;
+            this.operand2 = operand2;
+        }
+
+        public override T GetOperandAt<T>(int index)
+        {
+            if (index == 0)
+            {
+                if (typeof(T) != typeof(T1)) throw new InvalidOperationException("invalid operand type");
+                return (T)(object)this.operand1!;
+            }
+            if (index == 1)
+            {
+                if (typeof(T) != typeof(T2)) throw new InvalidOperationException("invalid operand type");
+                return (T)(object)this.operand2!;
+            }
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
+        public override void SetOperandAt<T>(int index, T value)
+        {
+            if (index == 0)
+            {
+                if (typeof(T) != typeof(T1)) throw new InvalidOperationException("invalid operand type");
+                this.operand1 = (T1)(object)value!;
+                return;
+            }
+            if (index == 1)
+            {
+                if (typeof(T) != typeof(T2)) throw new InvalidOperationException("invalid operand type");
+                this.operand2 = (T2)(object)value!;
+                return;
+            }
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+    }
+
+    private sealed class Instruction3<T1, T2, T3> : Instruction
+    {
+        private T1 operand1;
+        private T2 operand2;
+        private T3 operand3;
+
+        public Instruction3(InstructionKind kind, T1 operand1, T2 operand2, T3 operand3)
+            : base(kind)
+        {
+            this.operand1 = operand1;
+            this.operand2 = operand2;
+            this.operand3 = operand3;
+        }
+
+        public override T GetOperandAt<T>(int index)
+        {
+            if (index == 0)
+            {
+                if (typeof(T) != typeof(T1)) throw new InvalidOperationException("invalid operand type");
+                return (T)(object)this.operand1!;
+            }
+            if (index == 1)
+            {
+                if (typeof(T) != typeof(T2)) throw new InvalidOperationException("invalid operand type");
+                return (T)(object)this.operand2!;
+            }
+            if (index == 2)
+            {
+                if (typeof(T) != typeof(T3)) throw new InvalidOperationException("invalid operand type");
+                return (T)(object)this.operand3!;
+            }
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
+        public override void SetOperandAt<T>(int index, T value)
+        {
+            if (index == 0)
+            {
+                if (typeof(T) != typeof(T1)) throw new InvalidOperationException("invalid operand type");
+                this.operand1 = (T1)(object)value!;
+                return;
+            }
+            if (index == 1)
+            {
+                if (typeof(T) != typeof(T2)) throw new InvalidOperationException("invalid operand type");
+                this.operand2 = (T2)(object)value!;
+                return;
+            }
+            if (index == 2)
+            {
+                if (typeof(T) != typeof(T3)) throw new InvalidOperationException("invalid operand type");
+                this.operand3 = (T3)(object)value!;
+                return;
+            }
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+    }
+}

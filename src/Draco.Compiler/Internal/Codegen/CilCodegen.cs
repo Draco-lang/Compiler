4e4a2e1675d069f7ea3be0051598c4d1e3a917b9
LPeter1997 <lenkefi.peti@gmail.com>
2022-12-27 16:25:01 +0100

    Update CilCodegen.cs

--- src/Draco.Compiler/Internal/Codegen/CilCodegen.cs
+++ src/Draco.Compiler/Internal/Codegen/CilCodegen.cs
@@ -27,218 +27,43 @@ internal sealed class CilCodegen
     {
         var codegen = new CilCodegen(assembly);
 
-        codegen.CreateModuleAndAssembly();
-        codegen.Translate(assembly);
-        codegen.CreateFreeFunctionsClass();
-        codegen.GeneratePe(peStream);
+        // TODO
+        throw new NotImplementedException();
     }
 
     private readonly IReadOnlyAssembly assembly;
-
-    private readonly MetadataBuilder metadataBuilder = new();
-    private readonly BlobBuilder ilBuilder = new();
-
-    private readonly List<MethodDefinitionHandle> compiledFreeFunctions = new();
-
-    // Context local to procedures
-    private Dictionary<IReadOnlyBasicBlock, LabelHandle> labelTranslations = new();
-    private Dictionary<Value, int> localTranslations = new();
-    private LocalVariablesEncoder localsEncoder;
-    private InstructionEncoder ilEncoder;
 
     private CilCodegen(IReadOnlyAssembly assembly)
     {
         this.assembly = assembly;
     }
 
-    public void Translate(IReadOnlyAssembly asm)
+    private void TranslateProcedureSignature(MethodSignatureEncoder encoder, IReadOnlyProcecude procedure)
     {
-        foreach (var proc in asm.Procedures.Values) this.Translate(proc);
+        encoder.Parameters(procedure.Parameters.Count, out var returnTypeEncoder, out var parametersEncoder);
+        this.TranslateReturnType(returnTypeEncoder, procedure.ReturnType);
+        foreach (var param in procedure.Parameters) this.TranslateParameter(parametersEncoder, param);
     }
 
-    private void Translate(IReadOnlyProcecude proc)
+    private void TranslateParameter(ParametersEncoder encoder, Value.Parameter param)
     {
-        this.labelTranslations.Clear();
-        this.localTranslations.Clear();
-
-        var signature = new BlobBuilder();
-        new BlobEncoder(signature)
-            .MethodSignature()
-            .Parameters(
-                parameterCount: 0,
-                returnType: returnType => returnType.Void(),
-                parameters: parameters => { });
-
-        this.ilBuilder.Align(4);
-        var methodBodyStream = new MethodBodyStreamEncoder(this.ilBuilder);
-
-        this.localsEncoder = new LocalVariablesEncoder(this.ilBuilder);
-        this.ilEncoder = new InstructionEncoder(this.ilBuilder, new ControlFlowBuilder());
-
-        // "Forward-declare" labels
-        foreach (var bb in proc.BasicBlocks)
-        {
-            this.labelTranslations.Add(bb, this.ilEncoder.DefineLabel());
-        }
-
-        // Actual codegen
-        foreach (var bb in proc.BasicBlocks) this.Translate(bb);
-
-        var offset = methodBodyStream.AddMethodBody(this.ilEncoder);
-        var handle = this.metadataBuilder.AddMethodDefinition(
-            attributes: MethodAttributes.Public | MethodAttributes.Static | MethodAttributes.HideBySig,
-            implAttributes: MethodImplAttributes.IL,
-            name: this.metadataBuilder.GetOrAddString(proc.Name),
-            signature: this.metadataBuilder.GetOrAddBlob(signature),
-            bodyOffset: offset,
-            parameterList: default);
-
-        this.compiledFreeFunctions.Add(handle);
+        var typeEncoder = encoder.AddParameter();
+        this.TranslateSignatureType(typeEncoder.Type(), param.Type);
     }
 
-    private void Translate(IReadOnlyBasicBlock block)
+    private void TranslateReturnType(ReturnTypeEncoder encoder, Type type)
     {
-        this.ilEncoder.MarkLabel(this.labelTranslations[block]);
-        foreach (var instr in block.Instructions) this.Translate(instr);
+        if (type == Type.Unit) { encoder.Void(); return; }
+
+        this.TranslateSignatureType(encoder.Type(), type);
     }
 
-    private void Translate(IReadOnlyInstruction instr)
+    private void TranslateSignatureType(SignatureTypeEncoder encoder, Type type)
     {
-        switch (instr.Kind)
-        {
-        case InstructionKind.Nop:
-        {
-            this.ilEncoder.OpCode(ILOpCode.Nop);
-            break;
-        }
-        case InstructionKind.Ret:
-        {
-            var returnedValue = instr.GetOperandAt<Value>(0);
-            this.PushOnStack(returnedValue);
-            this.ilEncoder.OpCode(ILOpCode.Ret);
-            break;
-        }
-        case InstructionKind.AddInt:
-        {
-            var result = instr.GetOperandAt<Value>(0);
-            var a = instr.GetOperandAt<Value>(1);
-            var b = instr.GetOperandAt<Value>(2);
-            this.PushOnStack(a);
-            this.PushOnStack(b);
-            this.ilEncoder.OpCode(ILOpCode.Add);
-            this.StoreLocal(result);
-            break;
-        }
-        default:
-            throw new ArgumentOutOfRangeException(nameof(instr));
-        }
-    }
+        if (type == Type.Bool) { encoder.Boolean(); return; }
+        if (type == Type.Int32) { encoder.Int32(); return; }
 
-    private void StoreLocal(Value target)
-    {
-        this.localTranslations.Add(target, this.localTranslations.Count);
-        var typeEncoder = this.localsEncoder
-            .AddVariable()
-            .Type();
-        this.TranslateType(ref typeEncoder, target.Type);
-    }
-
-    private void PushOnStack(Value value)
-    {
-        if (value.Type == Type.Unit) return;
-
-        switch (value)
-        {
-        case Value.Constant c when c.Value is int intValue:
-        {
-            this.ilEncoder.LoadConstantI4(intValue);
-            break;
-        }
-        case Value.Register r:
-        {
-            this.ilEncoder.LoadArgument(this.localTranslations[r]);
-            break;
-        }
-        default:
-            throw new ArgumentOutOfRangeException(nameof(value));
-        }
-    }
-
-    private void TranslateType(ref SignatureTypeEncoder encoder, Type type)
-    {
-        if (type == Type.Int32) encoder.Int32();
-        else throw new NotImplementedException();
-    }
-
-    public void CreateModuleAndAssembly()
-    {
-        this.metadataBuilder.AddModule(
-            generation: 0,
-            moduleName: this.metadataBuilder.GetOrAddString(this.assembly.Name),
-            // TODO: Proper module-version ID
-            mvid: this.metadataBuilder.GetOrAddGuid(Guid.NewGuid()),
-            // TODO: What are these? Encryption?
-            encId: default,
-            encBaseId: default);
-
-        this.metadataBuilder.AddAssembly(
-            name: this.metadataBuilder.GetOrAddString(this.assembly.Name),
-            // TODO: Proper versioning
-            version: new Version(1, 0, 0, 0),
-            culture: default,
-            publicKey: default,
-            flags: 0,
-            hashAlgorithm: AssemblyHashAlgorithm.None);
-    }
-
-    public void CreateFreeFunctionsClass()
-    {
-        var mscorlibAssemblyRef = this.metadataBuilder.AddAssemblyReference(
-            name: this.metadataBuilder.GetOrAddString("mscorlib"),
-            // TODO: What version?
-            version: new Version(4, 0, 0, 0),
-            culture: default,
-            // TODO: What the hell?
-            publicKeyOrToken: this.metadataBuilder.GetOrAddBlob(new byte[]
-            {
-                0xB7, 0x7A, 0x5C, 0x56, 0x19, 0x34, 0xE0, 0x89,
-            }),
-            flags: default,
-            hashValue: default);
-
-        var systemObjectTypeRef = this.metadataBuilder.AddTypeReference(
-            mscorlibAssemblyRef,
-            this.metadataBuilder.GetOrAddString("System"),
-            this.metadataBuilder.GetOrAddString("Object"));
-
-        this.metadataBuilder.AddTypeDefinition(
-            attributes: TypeAttributes.Public | TypeAttributes.Class | TypeAttributes.AutoLayout | TypeAttributes.BeforeFieldInit | TypeAttributes.Abstract | TypeAttributes.Sealed,
-            @namespace: default,
-            name: this.metadataBuilder.GetOrAddString("FreeFunctions"),
-            baseType: systemObjectTypeRef,
-            // TODO: What's this?
-            fieldList: MetadataTokens.FieldDefinitionHandle(1),
-            methodList: this.compiledFreeFunctions.Count == 0
-                ? MetadataTokens.MethodDefinitionHandle(1)
-                : this.compiledFreeFunctions[0]);
-    }
-
-    public void GeneratePe(Stream peStream)
-    {
-        var peHeaderBuilder = new PEHeaderBuilder(
-            imageCharacteristics: Characteristics.Dll);
-        var peBuilder = new ManagedPEBuilder(
-            header: peHeaderBuilder,
-            metadataRootBuilder: new(this.metadataBuilder),
-            ilStream: this.ilBuilder,
-            // TODO: When entry point is exposed from assembly
-            entryPoint: default,
-            flags: CorFlags.ILOnly,
-            // TODO: For deterministic builds
-            deterministicIdProvider: null);
-
-        var peBlob = new BlobBuilder();
-        var contentId = peBuilder.Serialize(peBlob);
-        peBlob.WriteContentTo(peStream);
+        // TODO
+        throw new NotImplementedException();
     }
 }

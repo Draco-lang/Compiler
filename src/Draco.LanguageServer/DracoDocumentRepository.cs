fe1d8bd7ba4a66ac3969a60f99e7199cc495f4f8
lucyelle <35396043+lucyelle@users.noreply.github.com>
2022-12-30 19:47:45 +0100

    Goto definition (#152)
    * Extended Range and Position API
    
    * Update ParseTree.cs
    
    * Implemented GoToDefinitionHandler
    
    Co-authored-by: lucyelle <lucyelle@users.noreply.github.com>

--- src/Draco.LanguageServer/DracoDocumentRepository.cs
+++ src/Draco.LanguageServer/DracoDocumentRepository.cs
@@ -1,20 +1,23 @@
+using System;
 using System.Collections.Generic;
+using Draco.Compiler.Api.Syntax;
 using OmniSharp.Extensions.LanguageServer.Protocol;
 
 namespace Draco.LanguageServer;
 
 internal sealed class DracoDocumentRepository
 {
-    private readonly Dictionary<DocumentUri, string> documents = new();
+    private readonly Dictionary<DocumentUri, SourceText> documents = new();
 
-    public void AddOrUpdateDocument(DocumentUri uri, string contents) => this.documents[uri] = contents;
+    public void AddOrUpdateDocument(DocumentUri uri, string contents) =>
+        this.documents[uri] = SourceText.FromText(uri.ToUri(), contents.AsMemory());
 
-    public string GetDocument(DocumentUri uri)
+    public SourceText GetDocument(DocumentUri uri)
     {
         if (this.documents.TryGetValue(uri, out var contents))
         {
             return contents;
         }
-        return string.Empty;
+        return SourceText.None;
     }
 }

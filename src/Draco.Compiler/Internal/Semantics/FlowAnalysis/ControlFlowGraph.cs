65f32d3ae0954d078dfced2bcdc9ab60f96acc00
LPeter1997 <lenkefi.peti@gmail.com>
2023-01-02 15:36:06 +0100

    Update ControlFlowGraph.cs

--- src/Draco.Compiler/Internal/Semantics/FlowAnalysis/ControlFlowGraph.cs
+++ src/Draco.Compiler/Internal/Semantics/FlowAnalysis/ControlFlowGraph.cs
@@ -49,3 +49,86 @@ internal interface IBasicBlock<TStatement>
     /// </summary>
     public IEnumerable<IBasicBlock<TStatement>> Successors { get; }
 }
+
+/// <summary>
+/// Builder for CFGs.
+/// </summary>
+/// <typeparam name="TStatement">The statement type.</typeparam>
+internal sealed class CfgBuilder<TStatement>
+{
+    /// <summary>
+    /// Builds the control-flow graph.
+    /// </summary>
+    /// <returns>The built CFG.</returns>
+    public IControlFlowGraph<TStatement> Build()
+    {
+        // TODO
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Adds a statement to the currently built CFG.
+    /// </summary>
+    /// <param name="statement">The statement to add.</param>
+    public void AddStatement(TStatement statement)
+    {
+        // TODO
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Starts branching in the CFG. Needs to call <see cref="NextBranch"/> before any further <see cref="AddStatement(TStatement)"/>s.
+    /// </summary>
+    public void StartBranching()
+    {
+        // TODO
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Ends branching in the CFG.
+    /// </summary>
+    public void EndBranching()
+    {
+        // TODO
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Opens a new branch in the CFG.
+    /// </summary>
+    public void NextBranch()
+    {
+        // TODO
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Marks the current place in the CFG as a potential jump-target.
+    /// </summary>
+    /// <returns>The marker for the place.</returns>
+    public object MarkPlace()
+    {
+        // TODO
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Connects the current flow of CFG to the given marked place.
+    /// </summary>
+    /// <param name="mark">The mark to connect to.</param>
+    public void Jump(object mark)
+    {
+        // TODO
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Marks the current point in the CFG as an exit point.
+    /// </summary>
+    public void Exit()
+    {
+        // TODO
+        throw new NotImplementedException();
+    }
+}

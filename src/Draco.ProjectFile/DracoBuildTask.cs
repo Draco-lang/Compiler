7e1c48f667c5678fb92e9327c63cae2d8809f0e1
jakubbinter <kuba.binter@gmail.com>
2023-01-07 20:33:46 +0100

    emits runtimeconfig.json, generated exe is named after the project

--- src/Draco.ProjectFile/DracoBuildTask.cs
+++ src/Draco.ProjectFile/DracoBuildTask.cs
@@ -5,50 +5,56 @@ using System.Linq;
 using System.Reflection;
 using Microsoft.Build.Framework;
 
-namespace Draco.ProjectFile
+namespace Draco.ProjectFile;
+
+public class DracoBuildTask : Microsoft.Build.Utilities.ToolTask
 {
-    public class DracoBuildTask : Microsoft.Build.Utilities.ToolTask
+    public override bool Execute()
     {
-        // TODO: Emit outputFileName.runtimeconfig.json, for start we just need
-        //{
-        //  "runtimeOptions": {
-        //    "tfm": "net7.0",
-        //    "framework": {
-        //      "name": "Microsoft.NETCore.App",
-        //      "version": "7.0.0"
-        //      }
-        //  }
-        //}
-        public override bool Execute()
-        {
-            var files = Directory.EnumerateFiles(this.ProjectDirectory, "*.draco", SearchOption.TopDirectoryOnly).Where(x => x == Path.Combine(this.ProjectDirectory, "main.draco"));
-            if (files.Count() == 0) return false;
-            var mainFile = files.First();
-            var output = $"{Path.GetFileNameWithoutExtension(mainFile)}.exe";
-            this.Log.LogMessage(this.GenerateFullPathToTool());
-            this.ExecuteTool(this.GenerateFullPathToTool(), "", $"compile {mainFile} {output.ToCliFlag("output")}");
-            // TODO: Retarget standard output and show diags as errors/warnings/...
-            return true;
-        }
+        var files = Directory.EnumerateFiles(this.ProjectDirectory, "*.draco", SearchOption.TopDirectoryOnly).Where(x => x == Path.Combine(this.ProjectDirectory, "main.draco"));
+        if (files.Count() == 0) return false;
+        var mainFile = files.First();
+        var output = $"{this.ProjectName}.exe";
 
-        protected override string GenerateFullPathToTool() => Path.GetFullPath(Path.Combine(Assembly.GetExecutingAssembly().Location, @"..\..\..\..\..\Draco.Compiler.Cli\bin\Debug\net7.0\Draco.Compiler.Cli.exe"));
-
-        /// <summary>
-        /// Output type of the given project.
-        /// </summary>
-        //[Required]
-        public string OutputType { get; set; }
-
-        /// <summary>
-        /// The directory the current project is located in.
-        /// </summary>
-        //[Required]
-        public string ProjectDirectory { get; set; }
-
-        protected override string ToolName => "Draco.Compiler.Cli.exe";
+        this.ExecuteTool(this.GenerateFullPathToTool(), "", $"compile {mainFile} {output.ToCliFlag("output")}");
+        // TODO: Only emit if there were no errors while compiling
+        File.WriteAllText($"{this.ProjectName}.runtimeconfig.json", this.GenerateRuntimeConfigContents());
+        // TODO: Retarget standard output and show diags as errors/warnings/messages in the correct colors
+        return true;
     }
-    internal static class CliFlag
-    {
-        public static string ToCliFlag(this object flagValue, string flagName) => $"--{flagName} {flagValue}";
-    }
+
+    protected override string GenerateFullPathToTool() => Path.GetFullPath(Path.Combine(Assembly.GetExecutingAssembly().Location, @"..\..\..\..\..\Draco.Compiler.Cli\bin\Debug\net7.0\Draco.Compiler.Cli.exe"));
+
+    // TODO: change the target frameword based on users choise
+    private string GenerateRuntimeConfigContents() => """
+            {
+              "runtimeOptions": {
+                "tfm": "net7.0",
+                "framework": {
+                  "name": "Microsoft.NETCore.App",
+                  "version": "7.0.0"
+                }
+              }
+            }
+            """;
+    /// <summary>
+    /// Output type of the given project.
+    /// </summary>
+    public string OutputType { get; set; }
+
+    /// <summary>
+    /// The directory the current project is located in.
+    /// </summary>
+    public string ProjectDirectory { get; set; }
+
+    /// <summary>
+    /// Name of the current project.
+    /// </summary>
+    public string ProjectName { get; set; }
+
+    protected override string ToolName => "Draco.Compiler.Cli.exe";
+}
+internal static class CliFlag
+{
+    public static string ToCliFlag(this object flagValue, string flagName) => $"--{flagName} {flagValue}";
 }

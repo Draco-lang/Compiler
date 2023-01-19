09d6cfc91dcaa47c68e57e6b7cd235a5de08e09a
LPeter1997 <lenkefi.peti@gmail.com>
2023-01-19 20:31:35 +0100

    Language server and client deployment (#183)
    * Made the langserver pack as a tool
    
    * Update extension.ts
    
    * Added config
    
    * Detecting if dotnet tool is present
    
    * Update extension.ts
    
    * Update settings.ts
    
    * Completed the settings flow
    
    * Made the langserver lifecycle flow
    
    * Update extension.ts
    
    * Update extension.ts
    
    * Packaging info
    
    * Update Directory.Build.props
    
    * Added check-for-updates for langserver
    
    * Update settings.ts
    
    * Update publish.yaml
    
    * Update publish.yaml
    
    * Version bumps
    
    * Added PAT to VSC publishing
    
    * Update publish.yaml
    
    * Added update mechanism
    
    * Update settings.ts
    
    * Update extension.ts
    
    * Update package.json
    
    * Update publish.yaml
    
    * Cleanup
    
    * Update tests.yaml
    
    * New workflow
    
    * Update publish.yaml
    
    * Made license copy
    
    * Update extension.ts
    
    * Update publish.yaml
    
    * Update publish.yaml
    
    * Update publish.yaml

--- src/Draco.Extension.VsCode/src/extension.ts
+++ src/Draco.Extension.VsCode/src/extension.ts
@@ -1,36 +1,71 @@
-import * as path from 'path';
-import * as vscode from 'vscode';
-import * as lsp from 'vscode-languageclient/node';
+import { start } from "repl";
+import * as vscode from "vscode";
+import { window, workspace } from "vscode";
+import * as lsp from "vscode-languageclient/node";
+import { prompt, PromptResult } from "./prompt";
+import * as settings from "./settings";
 
-let languageClient: lsp.LanguageClient;
+let languageClient: lsp.LanguageClient | undefined;
 
-export function activate(context: vscode.ExtensionContext) {
-    // Path for the server
-	let serverPath = context.asAbsolutePath(path.join('out', 'Draco.LanguageServer.exe'));
+export async function activate(context: vscode.ExtensionContext): Promise<void> {
+    // Subscribe to setting changes, and prompt the user if the language server should be restarted
+    const listener = workspace.onDidChangeConfiguration(async event => {
+        if (event.affectsConfiguration('draco')) {
+            if (languageClient !== undefined) {
+                // Langserver is already running, ask if it should be restarted
+                const promptResult = await prompt(
+                    'Settings changed. Restart Draco language server?',
+                    { title: 'Yes', result: PromptResult.yes },
+                    { title: 'No', result: PromptResult.no });
+                if (promptResult == PromptResult.yes) {
+                    await startLanguageServer();
+                }
+            } else {
+                // Just start the language server, it was not running
+                // NOTE: Is this a good idea?
+                await startLanguageServer();
+            }
+        }
+    });
+    context.subscriptions.push(listener);
 
-    // Server options
-	let serverOptions: lsp.ServerOptions = {
-		command: serverPath,
-		transport: lsp.TransportKind.stdio,
-	};
-
-	// Client options
-	let clientOptions: lsp.LanguageClientOptions = {
-		documentSelector: [{ scheme: 'file', language: 'draco' }],
-	};
-
-	languageClient = new lsp.LanguageClient(
-		"dracoLanguageServer",
-		'Draco Language Server',
-		serverOptions,
-		clientOptions,
-	);
-
-	// Start the client, which also starts the server
-	languageClient.start();
+    await startLanguageServer();
 }
 
-export function deactivate(): Thenable<void> | undefined {
-	if (!languageClient) return undefined;
-	return languageClient.stop();
+export async function deactivate(): Promise<void> {
+    await stopLanguageServer();
+}
+
+async function stopLanguageServer() {
+    if (languageClient !== undefined) {
+        await languageClient.stop();
+        languageClient = undefined;
+    }
+}
+
+async function startLanguageServer(): Promise<void> {
+    // If there's a client running already, stop it
+    await stopLanguageServer();
+
+    // Server options
+    let serverOptions = await settings.getLanguageServerOptions();
+    if (serverOptions === undefined) {
+        await window.showErrorMessage('Could not start Draco language server.');
+        return;
+    }
+
+    // Client options
+    let clientOptions: lsp.LanguageClientOptions = {
+        documentSelector: [{ scheme: 'file', language: 'draco' }],
+    };
+
+    languageClient = new lsp.LanguageClient(
+        "dracoLanguageServer",
+        'Draco Language Server',
+        serverOptions,
+        clientOptions,
+    );
+
+    // Start the client, which also starts the server
+    await languageClient.start();
 }

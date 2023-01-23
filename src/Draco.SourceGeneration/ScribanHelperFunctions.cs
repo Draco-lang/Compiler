f46d48c65768f9cc0cf09150d4011e15ee1e13ab
LPeter1997 <lenkefi.peti@gmail.com>
2023-01-23 20:17:04 +0100

    Fixes, tweaks

--- src/Draco.SourceGeneration/ScribanHelperFunctions.cs
+++ src/Draco.SourceGeneration/ScribanHelperFunctions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Scriban.Runtime;
 
@@ -7,10 +8,22 @@ namespace Draco.SourceGeneration;
 
 public sealed class ScribanHelperFunctions : ScriptObject
 {
+    private static readonly string[] keywords = new[]
+    {
+        "if", "else", "while", "for", "foreach",
+        "params", "ref", "out", "operator",
+        "object",
+    };
+
+    public static string EscapeKeyword(string name) => keywords.Contains(name)
+        ? $"@{name}"
+        : name;
+
     public static string CamelCase(string str)
     {
         if (str.Length == 0) return str;
-        return $"{char.ToLower(str[0])}{str[1..]}";
+        var result = $"{char.ToLower(str[0])}{str[1..]}";
+        return EscapeKeyword(result);
     }
 
     public static ScribanHelperFunctions Instance { get; } = new();

04426a30ec01786bef78c77394974de929cb8609
LPeter1997 <lenkefi.peti@gmail.com>
2023-01-24 20:46:20 +0100

    Update SyntaxNode.cs

--- src/Draco.Compiler/Api/Syntax/SyntaxNode.cs
+++ src/Draco.Compiler/Api/Syntax/SyntaxNode.cs
@@ -9,6 +9,29 @@ namespace Draco.Compiler.Api.Syntax;
 /// </summary>
 public abstract class SyntaxNode
 {
+    /// <summary>
+    /// The <see cref="SyntaxTree"/> this node belongs to.
+    /// </summary>
+    public SyntaxTree Tree => throw new NotImplementedException();
+
+    /// <summary>
+    /// The parent <see cref="SyntaxNode"/> of this one.
+    /// </summary>
+    public SyntaxNode? Parent { get; }
+
+    /// <summary>
+    /// The internal green node that this node wraps.
+    /// </summary>
+    internal abstract Internal.Syntax.SyntaxNode Green { get; }
+
+    private readonly Internal.Syntax.SyntaxTree greenTree;
+
+    internal SyntaxNode(Internal.Syntax.SyntaxTree tree, SyntaxNode? parent)
+    {
+        this.greenTree = tree;
+        this.Parent = parent;
+    }
+
     public abstract void Accept(SyntaxVisitor visitor);
     public abstract TResult Accept<TResult>(SyntaxVisitor<TResult> visitor);
 }

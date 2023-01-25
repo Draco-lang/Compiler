cd5d9356cf25c11ef9aef09bca7b50176677bfcf
LPeter1997 <lenkefi.peti@gmail.com>
2023-01-25 19:41:00 +0100

    Minor additions

--- src/Draco.Compiler/Internal/Syntax/SyntaxNode.cs
+++ src/Draco.Compiler/Internal/Syntax/SyntaxNode.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Draco.Compiler.Internal.Syntax;
@@ -9,6 +10,11 @@ namespace Draco.Compiler.Internal.Syntax;
 /// </summary>
 internal abstract class SyntaxNode
 {
+    /// <summary>
+    /// The width of this node in characters.
+    /// </summary>
+    public virtual int Width => this.Children.Select(c => c.Width).Sum();
+
     /// <summary>
     /// The immediate descendant nodes of this one.
     /// </summary>
--- src/Draco.Compiler/Internal/Syntax/SyntaxToken.cs
+++ src/Draco.Compiler/Internal/Syntax/SyntaxToken.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Draco.Compiler.Api.Syntax;
 
 namespace Draco.Compiler.Internal.Syntax;
 
 /// <summary>
 /// A single token in the source code, possibly surrounded by trivia.
 /// </summary>
-internal sealed class SyntaxToken : SyntaxNode
+internal sealed partial class SyntaxToken : SyntaxNode
 {
+    /// <summary>
+    /// The <see cref="TokenType"/> of this token.
+    /// </summary>
+    public TokenType Type => throw new NotImplementedException();
+
     public override IEnumerable<SyntaxNode> Children => throw new NotImplementedException();
 
     public override Api.Syntax.SyntaxToken ToRedNode(SyntaxTree tree, Api.Syntax.SyntaxNode? parent) => throw new NotImplementedException();

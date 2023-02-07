4d0f1e139fffa9db1b9f1bff911f93fa0b0a39d5
LPeter1997 <lenkefi.peti@gmail.com>
2023-02-07 21:30:27 +0100

    Compiles

---
+++ src/Draco.Compiler/Internal/BoundTree/BoundNode.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draco.Compiler.Internal.BoundTree;
+
+/// <summary>
+/// The base for all bound nodes in the bound tree.
+/// </summary>
+internal abstract partial class BoundNode
+{
+}

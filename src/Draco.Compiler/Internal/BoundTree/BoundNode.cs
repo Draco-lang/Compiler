6485456a126d5578ca9b112adc22ab6f3e0337fb
LPeter1997 <lenkefi.peti@gmail.com>
2023-02-08 18:03:42 +0100

    Update BoundNode.cs

--- src/Draco.Compiler/Internal/BoundTree/BoundNode.cs
+++ src/Draco.Compiler/Internal/BoundTree/BoundNode.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,4 +12,10 @@ namespace Draco.Compiler.Internal.BoundTree;
 /// </summary>
 internal abstract partial class BoundNode
 {
+    protected static bool Equals<TNode>(ImmutableArray<TNode> left, ImmutableArray<TNode> right)
+        where TNode : BoundNode
+    {
+        if (left.Length != right.Length) return false;
+        return left.SequenceEqual(right);
+    }
 }

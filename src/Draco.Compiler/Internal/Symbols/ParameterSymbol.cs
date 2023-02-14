160c64d9d99032f03938ac6f6925b8083d58129d
LPeter1997 <lenkefi.peti@gmail.com>
2023-02-14 18:15:35 +0100

    Create ParameterSymbol.cs

---
+++ src/Draco.Compiler/Internal/Symbols/ParameterSymbol.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draco.Compiler.Internal.Symbols;
+
+/// <summary>
+/// Represents a parameter in a function.
+/// </summary>
+internal abstract partial class ParameterSymbol : Symbol
+{
+}

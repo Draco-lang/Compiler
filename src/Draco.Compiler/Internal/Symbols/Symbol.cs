0ac0307f3d7067358a8eb75835230cdad90af212
LPeter1997 <lenkefi.peti@gmail.com>
2023-02-14 15:15:00 +0100

    Added bases for types and symbols

---
+++ src/Draco.Compiler/Internal/Symbols/Symbol.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draco.Compiler.Internal.Symbols;
+
+/// <summary>
+/// Base for all symbols within the language.
+/// </summary>
+internal abstract partial class Symbol
+{
+}
---
+++ src/Draco.Compiler/Internal/Types/Type.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draco.Compiler.Internal.Types;
+
+/// <summary>
+/// The base for all types within the language.
+/// </summary>
+internal abstract partial class Type
+{
+}

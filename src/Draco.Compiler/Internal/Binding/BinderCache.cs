a2dcdfeb8e288668d78816141a63d4a75f95bca4
LPeter1997 <lenkefi.peti@gmail.com>
2023-02-20 10:50:37 +0100

    Create BinderCache.cs

---
+++ src/Draco.Compiler/Internal/Binding/BinderCache.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Draco.Compiler.Api.Syntax;
+using Draco.Compiler.Internal.Declarations;
+
+namespace Draco.Compiler.Internal.Binding;
+
+/// <summary>
+/// Responsible for caching the binders for syntax nodes and declarations.
+/// </summary>
+internal sealed class BinderCache
+{
+    /// <summary>
+    /// Retrieves a <see cref="Binder"/> for the given declaration.
+    /// </summary>
+    /// <param name="declaration">The declaration to retrieve the binder for.</param>
+    /// <returns>The binder for <paramref name="declaration"/>.</returns>
+    public Binder Get(Declaration declaration) =>
+        throw new NotImplementedException();
+
+    /// <summary>
+    /// Retrieves a <see cref="Binder"/> for the given syntax node.
+    /// </summary>
+    /// <param name="syntaxNode">The syntax node to retrieve the binder for.</param>
+    /// <returns>The binder for <paramref name="syntaxNode"/>.</returns>
+    public Binder Get(SyntaxNode syntaxNode) =>
+        throw new NotImplementedException();
+}

e82a9566b3304b172bb544371898a4a62ecb6a71
LPeter1997 <lenkefi.peti@gmail.com>
2023-02-22 21:25:55 +0100

    Update FunctionBinder.cs

--- src/Draco.Compiler/Internal/Binding/FunctionBinder.cs
+++ src/Draco.Compiler/Internal/Binding/FunctionBinder.cs
@@ -23,8 +23,28 @@ internal sealed class FunctionBinder : Binder
         this.symbol = symbol;
     }
 
-    public override void LookupValueSymbol(LookupResult result, string name, SyntaxNode? reference) =>
-        throw new NotImplementedException();
+    public override void LookupValueSymbol(LookupResult result, string name, SyntaxNode? reference)
+    {
+        foreach (var param in this.symbol.Parameters)
+        {
+            if (param.Name != name) continue;
+            result.Add(param);
+        }
+
+        // TODO: This is identical to the condition in the LocalBinder
+        // While this might be the same in the end... From here on out (outside a function)
+        // we can only reference global variables
+        // Maybe the current lookup system is just bad and we should do it iteratively in the base class, carrying flags
+        // and only ask each scope for their symbols
+
+        // If we are collecting an overload-set or the result is empty, we try to continue upwards
+        // Otherwise we can stop
+        if (!result.FoundAny || result.IsOverloadSet)
+        {
+            var parentReference = BinderFacts.GetScopeDefiningAncestor(reference?.Parent);
+            this.Parent?.LookupValueSymbol(result, name, parentReference);
+        }
+    }
 
     public override void LookupTypeSymbol(LookupResult result, string name, SyntaxNode? reference) =>
         throw new NotImplementedException();

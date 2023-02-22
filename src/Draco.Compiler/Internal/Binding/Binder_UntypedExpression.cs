c9c183745a78ccf61eb326336af3f7d5c401a954
LPeter1997 <lenkefi.peti@gmail.com>
2023-02-22 09:54:40 +0100

    Update Binder_UntypedExpression.cs

--- src/Draco.Compiler/Internal/Binding/Binder_UntypedExpression.cs
+++ src/Draco.Compiler/Internal/Binding/Binder_UntypedExpression.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Draco.Compiler.Api.Syntax;
 using Draco.Compiler.Internal.BoundTree;
+using Draco.Compiler.Internal.Symbols;
 using Draco.Compiler.Internal.UntypedTree;
 
 namespace Draco.Compiler.Internal.Binding;
@@ -18,9 +20,18 @@ internal partial class Binder
     /// <returns>The untyped expression for <paramref name="syntax"/>.</returns>
     protected UntypedExpression BindExpression(SyntaxNode syntax) => syntax switch
     {
+        NameExpressionSyntax name => this.BindNameExpression(name),
         IfExpressionSyntax @if => this.BindIfExpression(@if),
+        RelationalExpressionSyntax rel => this.BindRelationalExpression(rel),
         _ => throw new ArgumentOutOfRangeException(nameof(syntax)),
     };
+    private UntypedExpression BindNameExpression(NameExpressionSyntax syntax)
+    {
+        // TODO: Look up name properly
+        var symbol = (Symbol?)null ?? throw new NotImplementedException();
+        // TODO: Based on symbol return something
+        throw new NotImplementedException();
+    }
 
     private UntypedExpression BindIfExpression(IfExpressionSyntax syntax)
     {
@@ -29,12 +40,20 @@ internal partial class Binder
         var @else = syntax.Else is null
             ? UntypedTreeFactory.UnitExpression()
             : this.BindExpression(syntax.Else.Expression);
-        return this.BindIfExpression(syntax, condition, then, @else);
+        return new UntypedIfExpression(syntax, condition, then, @else);
     }
 
-    private UntypedExpression BindIfExpression(
-        IfExpressionSyntax syntax,
-        UntypedExpression condition,
-        UntypedExpression then,
-        UntypedExpression @else) => new UntypedIfExpression(syntax, condition, then, @else);
+    private UntypedExpression BindRelationalExpression(RelationalExpressionSyntax syntax)
+    {
+        var first = this.BindExpression(syntax.Left);
+        var comparisons = ImmutableArray.CreateBuilder<UntypedComparison>();
+        foreach (var cmpSyntax in syntax.Comparisons)
+        {
+            // TODO: Look up operator properly
+            var symbol = (ComparisonOperatorSymbol?)null ?? throw new NotImplementedException();
+            var right = this.BindExpression(cmpSyntax.Right);
+            comparisons.Add(new UntypedComparison(cmpSyntax, symbol, right));
+        }
+        return new UntypedRelationalExpression(syntax, first, comparisons.ToImmutable());
+    }
 }

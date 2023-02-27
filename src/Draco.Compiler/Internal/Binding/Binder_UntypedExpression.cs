56b254a4b0195ce0fe03063aa02b977020a96d35
LPeter1997 <lenkefi.peti@gmail.com>
2023-02-27 12:58:20 +0100

    Update Binder_UntypedExpression.cs

--- src/Draco.Compiler/Internal/Binding/Binder_UntypedExpression.cs
+++ src/Draco.Compiler/Internal/Binding/Binder_UntypedExpression.cs
@@ -23,6 +23,7 @@ internal partial class Binder
         LiteralExpressionSyntax lit => this.BindLiteralExpression(lit),
         NameExpressionSyntax name => this.BindNameExpression(name),
         IfExpressionSyntax @if => this.BindIfExpression(@if),
+        UnaryExpressionSyntax ury => this.BindUnaryExpression(ury),
         RelationalExpressionSyntax rel => this.BindRelationalExpression(rel),
         _ => throw new ArgumentOutOfRangeException(nameof(syntax)),
     };
@@ -60,23 +61,34 @@ internal partial class Binder
         return new UntypedIfExpression(syntax, condition, then, @else);
     }
 
+    private UntypedExpression BindUnaryExpression(UnaryExpressionSyntax ury)
+    {
+        var subexpr = this.BindExpression(ury.Operand);
+        // TODO: Look up operator
+        throw new NotImplementedException();
+    }
+
     private UntypedExpression BindRelationalExpression(RelationalExpressionSyntax syntax)
     {
         var first = this.BindExpression(syntax.Left);
-        var comparisons = ImmutableArray.CreateBuilder<UntypedComparison>();
-        foreach (var cmpSyntax in syntax.Comparisons)
+        var comparisons = syntax.Comparisons
+            .Select(this.BindComparison)
+            .ToImmutableArray();
+        return new UntypedRelationalExpression(syntax, first, comparisons);
+    }
+
+    private UntypedComparison BindComparison(ComparisonElementSyntax syntax)
+    {
+        // Get the comparison operator symbol
+        var symbolName = ComparisonOperatorSymbol.GetComparisonOperatorName(syntax.Operator.Kind);
+        var lookup = this.LookupValueSymbol(symbolName, syntax);
+        if (!lookup.FoundAny || lookup.Symbols.Count > 1)
         {
-            var symbolName = ComparisonOperatorSymbol.GetComparisonOperatorName(cmpSyntax.Operator.Kind);
-            var lookup = this.LookupValueSymbol(symbolName, syntax);
-            if (!lookup.FoundAny || lookup.Symbols.Count > 1)
-            {
-                // TODO: Handle overload or illegal
-                throw new NotImplementedException();
-            }
-            var symbol = (ComparisonOperatorSymbol)lookup.Symbols[0];
-            var right = this.BindExpression(cmpSyntax.Right);
-            comparisons.Add(new UntypedComparison(cmpSyntax, symbol, right));
+            // TODO: Handle overload or illegal
+            throw new NotImplementedException();
         }
-        return new UntypedRelationalExpression(syntax, first, comparisons.ToImmutable());
+        var symbol = (ComparisonOperatorSymbol)lookup.Symbols[0];
+        var right = this.BindExpression(syntax.Right);
+        return new UntypedComparison(syntax, symbol, right);
     }
 }

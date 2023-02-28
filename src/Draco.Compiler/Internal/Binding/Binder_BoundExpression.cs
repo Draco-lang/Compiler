fe7ef9d85130bf33f5cf6844f849af2fb9e84270
LPeter1997 <lenkefi.peti@gmail.com>
2023-02-28 19:42:21 +0100

    Update Binder_BoundExpression.cs

--- src/Draco.Compiler/Internal/Binding/Binder_BoundExpression.cs
+++ src/Draco.Compiler/Internal/Binding/Binder_BoundExpression.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,53 @@ internal partial class Binder
     /// <returns>The bound expression for <paramref name="expression"/>.</returns>
     protected BoundExpression TypeExpression(UntypedExpression expression, ConstraintBag constraints, DiagnosticBag diagnostics) => expression switch
     {
+        UntypedLiteralExpression literal => this.TypeLiteralExpression(literal, constraints, diagnostics),
+        UntypedParameterExpression @param => this.TypeParameterExpression(param, constraints, diagnostics),
+        UntypedReturnExpression @return => this.TypeReturnExpression(@return, constraints, diagnostics),
+        UntypedIfExpression @if => this.TypeIfExpression(@if, constraints, diagnostics),
+        UntypedUnaryExpression ury => this.TypeUnaryExpression(ury, constraints, diagnostics),
+        UntypedRelationalExpression rel => this.TypeRelationalExpression(rel, constraints, diagnostics),
         _ => throw new ArgumentOutOfRangeException(nameof(expression)),
     };
+
+    private BoundExpression TypeLiteralExpression(UntypedLiteralExpression literal, ConstraintBag constraints, DiagnosticBag diagnostics) =>
+        new BoundLiteralExpression(literal.Syntax, literal.Value);
+
+    private BoundExpression TypeParameterExpression(UntypedParameterExpression param, ConstraintBag constraints, DiagnosticBag diagnostics) =>
+        new BoundParameterExpression(param.Syntax, param.Parameter);
+
+    private BoundExpression TypeReturnExpression(UntypedReturnExpression @return, ConstraintBag constraints, DiagnosticBag diagnostics)
+    {
+        var typedValue = this.TypeExpression(@return.Value, constraints, diagnostics);
+        return new BoundReturnExpression(@return.Syntax, typedValue);
+    }
+
+    private BoundExpression TypeIfExpression(UntypedIfExpression @if, ConstraintBag constraints, DiagnosticBag diagnostics)
+    {
+        var typedCondition = this.TypeExpression(@if.Condition, constraints, diagnostics);
+        var typedThen = this.TypeExpression(@if.Then, constraints, diagnostics);
+        var typedElse = this.TypeExpression(@if.Else, constraints, diagnostics);
+        return new BoundIfExpression(@if.Syntax, typedCondition, typedThen, typedElse);
+    }
+
+    private BoundExpression TypeUnaryExpression(UntypedUnaryExpression ury, ConstraintBag constraints, DiagnosticBag diagnostics)
+    {
+        var typedOperand = this.TypeExpression(ury.Operand, constraints, diagnostics);
+        return new BoundUnaryExpression(ury.Syntax, ury.Operator, typedOperand);
+    }
+
+    private BoundExpression TypeRelationalExpression(UntypedRelationalExpression rel, ConstraintBag constraints, DiagnosticBag diagnostics)
+    {
+        var first = this.TypeExpression(rel.First, constraints, diagnostics);
+        var comparisons = rel.Comparisons
+            .Select(cmp => this.TypeComparison(cmp, constraints, diagnostics))
+            .ToImmutableArray();
+        return new BoundRelationalExpression(rel.Syntax, first, comparisons);
+    }
+
+    private BoundComparison TypeComparison(UntypedComparison cmp, ConstraintBag constraints, DiagnosticBag diagnostics)
+    {
+        var next = this.TypeExpression(cmp.Next, constraints, diagnostics);
+        return new BoundComparison(cmp.Syntax, cmp.Operator, next);
+    }
 }

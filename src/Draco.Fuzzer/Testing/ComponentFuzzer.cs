b313d95e554c25d46c7983611cc420b056ea0fd8
jakubbinter <kuba.binter@gmail.com>
2023-03-02 20:26:47 +0100

    ctrl + c works fine now and SO is handeled

--- src/Draco.Fuzzer/Testing/ComponentFuzzer.cs
+++ src/Draco.Fuzzer/Testing/ComponentFuzzer.cs
@@ -1,4 +1,3 @@
-using System.Runtime.CompilerServices;
 using Draco.Fuzzer.Testing.Generators;
 
 namespace Draco.Fuzzer.Testing;
@@ -6,18 +5,29 @@ namespace Draco.Fuzzer.Testing;
 internal abstract class ComponentFuzzer<T>
 {
     private readonly List<(string input, Exception ex)> errors = new();
-
+    private string currentInput = string.Empty;
+    private string path = "lastinputlog.txt";
+    private bool shouldStop = false;
     protected readonly IInputGenerator<T> generator;
 
     public ComponentFuzzer(IInputGenerator<T> generator)
     {
         this.generator = generator;
         Console.CancelKeyPress += new ConsoleCancelEventHandler(this.HandleCancel);
+        if (File.Exists(this.path)) File.Delete(this.path);
+        Console.WriteLine($"in case of stack overflow look at this log for the last input: {Path.GetFullPath(this.path)}");
     }
 
-    public void AddError(Exception ex, string input) => this.errors.Add((input, ex));
+    private void AddError(Exception ex) => this.errors.Add((this.currentInput, ex));
 
-    public void PrintResult()
+    // Note: We are writing the input to the log from the start, because if we get stack overflow, we wouldn't be able to get to the input
+    private void AddInput(string input)
+    {
+        this.currentInput = input;
+        File.WriteAllText(this.path, input);
+    }
+
+    private void PrintResult()
     {
         var color = Console.ForegroundColor;
         var errorCount = this.errors.GroupBy(x => x.ex.StackTrace);
@@ -36,21 +46,26 @@ internal abstract class ComponentFuzzer<T>
         }
     }
 
-    private void HandleCancel(object? sender, ConsoleCancelEventArgs args) => this.PrintResult();
+    private void HandleCancel(object? sender, ConsoleCancelEventArgs args)
+    {
+        this.shouldStop = true;
+        args.Cancel = true;
+    }
 
     public void StartTesting(int numEpochs, int numMutations)
     {
         // If number of epochs is -1 we run forever
-        for (var i = 0; i < numEpochs || numEpochs == -1; i++)
+        for (var i = 0; (i < numEpochs || numEpochs == -1) && !this.shouldStop; i++)
         {
             var input = this.generator.NextExpoch();
+            this.AddInput(input!.ToString()!);
             try
             {
                 this.RunEpoch(input);
             }
             catch (Exception ex)
             {
-                this.AddError(ex, input.ToString()!);
+                this.AddError(ex);
             }
             for (var j = 0; j < numMutations; j++)
             {

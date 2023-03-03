9f9164d6f017095ef27182acdc22e7ca7632ef1c
jakubbinter <kuba.binter@gmail.com>
2023-03-03 17:24:48 +0100

    remove unnecesarry code

--- src/Draco.Fuzzer/Testing/ComponentFuzzer.cs
+++ src/Draco.Fuzzer/Testing/ComponentFuzzer.cs
@@ -5,7 +5,6 @@ namespace Draco.Fuzzer.Testing;
 internal abstract class ComponentFuzzer<T>
 {
     private readonly List<(string input, Exception ex)> errors = new();
-    private string currentInput = string.Empty;
     private string path = "lastinputlog.txt";
     private bool shouldStop = false;
     protected readonly IInputGenerator<T> generator;
@@ -18,14 +17,10 @@ internal abstract class ComponentFuzzer<T>
         Console.WriteLine($"in case of stack overflow look at this log for the last input: {Path.GetFullPath(this.path)}");
     }
 
-    private void AddError(Exception ex) => this.errors.Add((this.currentInput, ex));
+    private void AddError(Exception ex, string input) => this.errors.Add((input, ex));
 
     // Note: We are writing the input to the log from the start, because if we get stack overflow, we wouldn't be able to get to the input
-    private void AddInput(string input)
-    {
-        this.currentInput = input;
-        File.WriteAllText(this.path, input);
-    }
+    private void AddInput(string input) => File.WriteAllText(this.path, input);
 
     private void PrintResult()
     {
@@ -57,15 +52,15 @@ internal abstract class ComponentFuzzer<T>
         // If number of epochs is -1 we run forever
         for (var i = 0; (i < numEpochs || numEpochs == -1) && !this.shouldStop; i++)
         {
-            var input = this.generator.NextExpoch();
-            this.AddInput(input!.ToString()!);
+            var input = this.generator.NextExpoch()!;
+            this.AddInput(input.ToString()!);
             try
             {
                 this.RunEpoch(input);
             }
             catch (Exception ex)
             {
-                this.AddError(ex);
+                this.AddError(ex, input.ToString()!);
             }
             for (var j = 0; j < numMutations; j++)
             {

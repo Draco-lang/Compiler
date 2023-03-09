c3f5650fffc7772245a55536eacac33a4f5f3ef6
LPeter1997 <lenkefi.peti@gmail.com>
2023-03-09 12:45:33 +0100

    Update TypeVariable.cs

--- src/Draco.Compiler/Internal/Types/TypeVariable.cs
+++ src/Draco.Compiler/Internal/Types/TypeVariable.cs
@@ -13,32 +13,12 @@ namespace Draco.Compiler.Internal.Types;
 /// </summary>
 internal sealed class TypeVariable : Type
 {
-    private static int idCounter = -1;
+    private readonly int index;
 
-    /// <summary>
-    /// The substitution of this type variable.
-    /// </summary>
-    public Type Substitution
+    public TypeVariable(int index)
     {
-        get
-        {
-            if (this.substitution is null) return this;
-            // Pruning
-            if (this.substitution is TypeVariable var) this.substitution = var.Substitution;
-            return this.substitution;
-        }
-        set
-        {
-            if (this.substitution is not null) throw new InvalidOperationException("tried to substitute type variable multiple times");
-            this.substitution = value;
-        }
+        this.index = index;
     }
-    private Type? substitution;
 
-    // NOTE: This makes printing types nondeterministic
-    private readonly int id = Interlocked.Increment(ref idCounter);
-
-    public override string ToString() => this.substitution is null
-        ? $"{StringUtils.IndexToExcelColumnName(this.id)}'"
-        : this.substitution.ToString();
+    public override string ToString() => $"{StringUtils.IndexToExcelColumnName(this.index)}'";
 }

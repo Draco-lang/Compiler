97778681d06091db48a0dbe9a19014add3de4eae
jakubbinter <kuba.binter@gmail.com>
2023-03-13 21:02:46 +0100

    Update TokenArray.cs

--- src/Draco.Fuzzer/TokenArray.cs
+++ src/Draco.Fuzzer/TokenArray.cs
@@ -5,11 +5,12 @@ namespace Draco.Fuzzer;
 internal sealed class TokenArray
 {
     private readonly IEnumerable<SyntaxToken> tokens;
-    public ReadOnlyMemory<SyntaxToken> Memory => new ReadOnlyMemory<SyntaxToken>(this.tokens.ToArray());
+    public ReadOnlyMemory<SyntaxToken> Memory { get; }
 
     public TokenArray(IEnumerable<SyntaxToken> tokens)
     {
         this.tokens = tokens;
+        this.Memory = new ReadOnlyMemory<SyntaxToken>(tokens.ToArray());
     }
 
     public override string ToString() => string.Join("", this.tokens.Select(x => x.Text));

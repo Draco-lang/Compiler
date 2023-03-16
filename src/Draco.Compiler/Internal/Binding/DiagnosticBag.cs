7507592b9ed99b6ee1e9af8f74bae12706b9e1c8
LPeter1997 <lenkefi.peti@gmail.com>
2023-03-16 20:41:00 +0100

    Update DiagnosticBag.cs

--- src/Draco.Compiler/Internal/Binding/DiagnosticBag.cs
+++ src/Draco.Compiler/Internal/Binding/DiagnosticBag.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Draco.Compiler.Internal.Diagnostics;
 
 namespace Draco.Compiler.Internal.Binding;
 
@@ -11,4 +12,21 @@ namespace Draco.Compiler.Internal.Binding;
 /// </summary>
 internal sealed class DiagnosticBag
 {
+    /// <summary>
+    /// True, if the bad contains errors.
+    /// </summary>
+    public bool HasErrors { get; private set; }
+
+    private readonly List<Diagnostic> diagnostics = new();
+
+    /// <summary>
+    /// Adds a diagnostic to this bag.
+    /// </summary>
+    /// <param name="diagnostic">The diagnostic to add.</param>
+    public void Add(Diagnostic diagnostic)
+    {
+        this.diagnostics.Add(diagnostic);
+        this.HasErrors = this.HasErrors
+                      || diagnostic.Severity == Api.Diagnostics.DiagnosticSeverity.Error;
+    }
 }

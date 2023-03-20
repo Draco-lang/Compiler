777002e412d858c32bd6d45a0a5295d460cb01bc
LPeter1997 <lenkefi.peti@gmail.com>
2023-03-20 10:31:30 +0100

    Update SyntaxOffset.cs

--- src/Draco.Compiler/Internal/Syntax/SyntaxOffset.cs
+++ src/Draco.Compiler/Internal/Syntax/SyntaxOffset.cs
@@ -11,4 +11,14 @@ namespace Draco.Compiler.Internal.Syntax;
 /// </summary>
 /// <param name="Lines">The number of lines offset.</param>
 /// <param name="Columns">The number of columns offset.</param>
-internal readonly record struct SyntaxOffset(int Lines, int Columns);
+internal readonly record struct SyntaxOffset(int Lines, int Columns)
+{
+    /// <summary>
+    /// Offsets thus syntax offset by another.
+    /// </summary>
+    /// <param name="offset">The other offset to offset by.</param>
+    /// <returns>The new syntax offset, offset by <paramref name="offset"/>.</returns>
+    public SyntaxOffset OffsetBy(SyntaxOffset offset) => offset.Lines == 0
+        ? new(Lines: this.Lines, Columns: this.Columns + offset.Columns)
+        : new(Lines: this.Lines + offset.Lines, Columns: offset.Columns);
+}

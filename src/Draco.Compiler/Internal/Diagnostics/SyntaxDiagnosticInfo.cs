a9698ac560890141fec40b900fe33b0c5b74687c
LPeter1997 <lenkefi.peti@gmail.com>
2023-03-21 08:30:14 +0100

    Create SyntaxDiagnosticInfo.cs

---
+++ src/Draco.Compiler/Internal/Diagnostics/SyntaxDiagnosticInfo.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Draco.Compiler.Api.Diagnostics;
+using Draco.Compiler.Api.Syntax;
+
+namespace Draco.Compiler.Internal.Diagnostics;
+
+/// <summary>
+/// Represents diagnostic information attached to nodes of the green tree.
+///
+/// Since the green tree has no access to parents or the tree itself, we need this separate concept for
+/// syntax diagnostics. Once the tree is wrapped around in the red tree, we can convert this to diagnostics
+/// with locations.
+/// </summary>
+/// <param name="Info">The diagnostic information.</param>
+/// <param name="Offset">The offset relative to the attached node, not including the leading trivia.</param>
+/// <param name="Width">The width of the diagnostic area.</param>
+internal readonly record struct SyntaxDiagnosticInfo(
+    DiagnosticInfo Info,
+    int Offset,
+    int Width)
+{
+    /// <summary>
+    /// Converts this syntax diagnostic information to a diagnostic with source location.
+    /// </summary>
+    /// <param name="node">The red node wrapper of the green node this diagnostic was attached to.</param>
+    /// <returns>The diagnostic with source location pointing at <paramref name="node"/>.</returns>
+    public Diagnostic ToDiagnostic(SyntaxNode node) =>
+        throw new NotImplementedException();
+}

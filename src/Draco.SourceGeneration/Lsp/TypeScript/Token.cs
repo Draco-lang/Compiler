cd2dfc509a278a548eaaf8face4535f876498f38
lucyelle <lucyelle@users.noreply.github.com>
2023-03-27 20:20:16 +0200

    Added all of TS processing

--- src/Draco.SourceGeneration/Lsp/TypeScript/Token.cs
+++ src/Draco.SourceGeneration/Lsp/TypeScript/Token.cs
@@ -7,27 +7,10 @@ namespace Draco.SourceGeneration.Lsp.TypeScript;
 /// <summary>
 /// A single TypeScript token.
 /// </summary>
-internal sealed class Token
-{
-    /// <summary>
-    /// The kind of this token.
-    /// </summary>
-    public TokenKind Kind { get; }
-
-    /// <summary>
-    /// The text this token was produced from.
-    /// </summary>
-    public string Text { get; }
-
-    /// <summary>
-    /// The leading comment before this token.
-    /// </summary>
-    public string? LeadingComment { get; }
-
-    public Token(TokenKind kind, string text, string? leadingComment)
-    {
-        this.Kind = kind;
-        this.Text = text;
-        this.LeadingComment = leadingComment;
-    }
-}
+/// <param name="Kind">The kind of this token.</param>
+/// <param name="Text">The text this token was produced from.</param>
+/// <param name="LeadingComment">The leading comment before this token.</param>
+internal sealed record class Token(
+    TokenKind Kind,
+    string Text,
+    string? LeadingComment);

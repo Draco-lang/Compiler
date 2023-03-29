7d28d4fc8c741d70341c61935d87bd092947f650
LPeter1997 <lenkefi.peti@gmail.com>
2023-03-29 11:21:24 +0200

    Goto definition added

---
+++ src/Draco.LanguageServer/Capabilities/GotoDefinition.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Draco.Compiler.Api.Syntax;
+using Draco.Compiler.Api;
+using Draco.Lsp.Model;
+using Draco.Lsp.Server.Language;
+using StreamJsonRpc;
+
+namespace Draco.LanguageServer;
+
+internal sealed partial class DracoLanguageServer : IGotoDefinition
+{
+    public DefinitionRegistrationOptions GotoDefinitionRegistrationOptions => new()
+    {
+        DocumentSelector = this.DocumentSelector,
+    };
+
+    public Task<IList<Location>> GotoDefinitionAsync(DefinitionParams param, CancellationToken cancellationToken)
+    {
+        var cursorPosition = Translator.ToCompiler(param.Position);
+        // TODO: Share compilation
+        var souceText = this.documentRepository.GetDocument(param.TextDocument.Uri);
+        var syntaxTree = SyntaxTree.Parse(souceText);
+        var compilation = Compilation.Create(
+            syntaxTrees: ImmutableArray.Create(syntaxTree));
+        var semanticModel = compilation.GetSemanticModel(syntaxTree);
+
+        var referencedSymbol = syntaxTree
+            .TraverseSubtreesAtPosition(cursorPosition)
+            .Select(semanticModel.GetReferencedSymbol)
+            .LastOrDefault(symbol => symbol is not null);
+
+        if (referencedSymbol is not null && referencedSymbol.Definition is not null)
+        {
+            var location = Translator.ToLsp(referencedSymbol.Definition);
+            return Task.FromResult<IList<Location>>(new[] { location ?? new() });
+        }
+        else
+        {
+            return Task.FromResult<IList<Location>>(Array.Empty<Location>());
+        }
+    }
+}
---
+++ src/Draco.Lsp/Server/Language/IGotoDefinition.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Draco.Lsp.Attributes;
+using Draco.Lsp.Model;
+
+namespace Draco.Lsp.Server.Language;
+
+public interface IGotoDefinition
+{
+    [Capability(nameof(ServerCapabilities.DefinitionProvider))]
+    public DefinitionOptions? Capability => null;
+
+    [RegistrationOptions("textDocument/definition")]
+    public DefinitionRegistrationOptions GotoDefinitionRegistrationOptions { get; }
+
+    [Request("textDocument/definition")]
+    public Task<IList<Location>> GotoDefinitionAsync(DefinitionParams param, CancellationToken cancellationToken);
+}

f8ea038183090a99ac303b203d920139b5bfdcf8
LPeter1997 <lenkefi.peti@gmail.com>
2023-03-29 18:34:54 +0200

    Update DocumentUriConverter.cs

--- src/Draco.Lsp/Serialization/DocumentUriConverter.cs
+++ src/Draco.Lsp/Serialization/DocumentUriConverter.cs
@@ -10,8 +10,10 @@ namespace Draco.Lsp.Serialization;
 internal sealed class DocumentUriConverter : JsonConverter
 {
     public override bool CanConvert(Type objectType) => objectType == typeof(DocumentUri);
+
     public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer) =>
         new DocumentUri((string)reader.Value!);
+
     public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer) =>
         writer.WriteValue(((DocumentUri)value!).ToString());
 }

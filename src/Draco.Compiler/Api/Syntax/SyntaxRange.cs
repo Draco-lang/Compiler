a0b3aa75e7ba716c9a0fc7e3e7df36fc3843702e
lucyelle <35396043+lucyelle@users.noreply.github.com>
2023-03-29 13:42:37 +0200

    Traverse intersecting range (#216)
    * Wrote initial algorythm
    
    * Documentation
    
    * Naming fix
    
    * Docs fix
    
    ---------
    
    Co-authored-by: lucyelle <lucyelle@users.noreply.github.com>

--- src/Draco.Compiler/Api/Syntax/SyntaxRange.cs
+++ src/Draco.Compiler/Api/Syntax/SyntaxRange.cs
@@ -23,4 +23,25 @@ public readonly record struct SyntaxRange(SyntaxPosition Start, SyntaxPosition E
     /// <param name="position">The position to check for containment.</param>
     /// <returns>True, if <paramref name="position"/> is contained, false otherwise.</returns>
     public bool Contains(SyntaxPosition position) => this.Start <= position && position < this.End;
+
+    /// <summary>
+    /// Checks if this range contains the given range.
+    /// </summary>
+    /// <param name="range">The range to check for containment.</param>
+    /// <returns>True, if <paramref name="range"/> is contained, false otherwise.</returns>
+    public bool Contains(SyntaxRange range) => this.Start <= range.Start && this.End >= range.End;
+
+    /// <summary>
+    /// Checks if this range is disjunct with the given range.
+    /// </summary>
+    /// <param name="range">The range to check for disjunction.</param>
+    /// <returns>True, if this range and <paramref name="range"/> are disjunct, false otherwise.</returns>
+    public bool IsDisjunctWith(SyntaxRange range) => this.End <= range.Start || range.End <= this.Start;
+
+    /// <summary>
+    /// Checks if this range intersects with the given range.
+    /// </summary>
+    /// <param name="range">The range to check for intersection.</param>
+    /// <returns>True, if this range and <paramref name="range"/> are intersecting, false otherwise.</returns>
+    public bool Intersects(SyntaxRange range) => !this.IsDisjunctWith(range);
 }

a201bbd99a26ccb23e7b7cd05439e061dda347e8
LPeter1997 <lenkefi.peti@gmail.com>
2023-03-30 19:40:35 +0200

    Added arithmetic instruction

---
+++ src/Draco.Compiler/Internal/OptimizingIr/Model/ArithmeticInstruction.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draco.Compiler.Internal.OptimizingIr.Model;
+
+/// <summary>
+/// Represents some kind of binary arithmetic instruction.
+/// </summary>
+internal sealed class ArithmeticInstruction : InstructionBase
+{
+    public override IEnumerable<IOperand> Operands => new[] { this.Target, this.Left, this.Right };
+
+    /// <summary>
+    /// The register to store the result at.
+    /// </summary>
+    public Register Target { get; set; }
+
+    /// <summary>
+    /// The arithmetic operation performed.
+    /// </summary>
+    public ArithmeticOp Op { get; set; }
+
+    /// <summary>
+    /// The left operand of the operation.
+    /// </summary>
+    public IOperand Left { get; set; }
+
+    /// <summary>
+    /// The right operand of the operation.
+    /// </summary>
+    public IOperand Right { get; set; }
+
+    public ArithmeticInstruction(Register target, ArithmeticOp op, IOperand left, IOperand right)
+    {
+        this.Target = target;
+        this.Op = op;
+        this.Left = left;
+        this.Right = right;
+    }
+
+    public override string ToString() =>
+        $"{this.Target.ToOperandString()} := {this.Left.ToOperandString()} {this.OpToString()} {this.Right.ToOperandString()}";
+
+    private string OpToString() => this.Op switch
+    {
+        ArithmeticOp.Add => "+",
+        ArithmeticOp.Sub => "-",
+        ArithmeticOp.Mul => "*",
+        ArithmeticOp.Div => "/",
+        ArithmeticOp.Rem => "rem",
+        ArithmeticOp.Less => "<",
+        ArithmeticOp.Equal => "=",
+        _ => throw new InvalidOperationException(),
+    };
+}
---
+++ src/Draco.Compiler/Internal/OptimizingIr/Model/ArithmeticOp.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draco.Compiler.Internal.OptimizingIr.Model;
+
+/// <summary>
+/// The different arithmetic operations supported.
+/// </summary>
+internal enum ArithmeticOp
+{
+    /// <summary>
+    /// Arithmetic addition.
+    /// </summary>
+    Add,
+
+    /// <summary>
+    /// Arithmetic subtraction.
+    /// </summary>
+    Sub,
+
+    /// <summary>
+    /// Arithmetic multiplication.
+    /// </summary>
+    Mul,
+
+    /// <summary>
+    /// Arithmetic division.
+    /// </summary>
+    Div,
+
+    /// <summary>
+    /// Arithmetic remainder.
+    /// </summary>
+    Rem,
+
+    /// <summary>
+    /// Less-than comparison.
+    /// </summary>
+    Less,
+
+    /// <summary>
+    /// Equality comparison.
+    /// </summary>
+    Equal,
+}

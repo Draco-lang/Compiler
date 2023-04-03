0aaa0ab3bbcc29f5ec99d9e73815fac99fde2996
LPeter1997 <lenkefi.peti@gmail.com>
2023-04-03 20:14:16 +0200

    Added pseudo-instructions for scopes

---
+++ src/Draco.Compiler/Internal/OptimizingIr/Model/EndScope.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draco.Compiler.Internal.OptimizingIr.Model;
+
+/// <summary>
+/// A pseudo-instruction for representing the end of a local scope.
+/// </summary>
+internal sealed class EndScope : InstructionBase
+{
+    public override EndScope Clone() => new();
+
+    public override string ToString() => "@scope end";
+}

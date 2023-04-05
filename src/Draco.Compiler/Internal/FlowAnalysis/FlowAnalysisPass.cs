712b2198b9d21d5f5366237bbff737327e581a6e
LPeter1997 <lenkefi.peti@gmail.com>
2023-04-05 18:21:02 +0200

    Update FlowAnalysisPass.cs

--- src/Draco.Compiler/Internal/FlowAnalysis/FlowAnalysisPass.cs
+++ src/Draco.Compiler/Internal/FlowAnalysis/FlowAnalysisPass.cs
@@ -6,6 +6,19 @@ using System.Threading.Tasks;
 
 namespace Draco.Compiler.Internal.FlowAnalysis;
 
-internal abstract class FlowAnalysisPass
+/// <summary>
+/// A base class for implementing dataflow passes.
+/// </summary>
+/// <typeparam name="TState">The state being tracked by the pass.</typeparam>
+internal abstract class FlowAnalysisPass<TState>
 {
+    /// <summary>
+    /// The lattice used by this pass.
+    /// </summary>
+    protected ILattice<TState> Lattice { get; }
+
+    protected FlowAnalysisPass(ILattice<TState> lattice)
+    {
+        this.Lattice = lattice;
+    }
 }

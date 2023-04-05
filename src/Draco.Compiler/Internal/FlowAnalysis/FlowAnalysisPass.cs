821fda358a6af748ecf3f400fce2adbf58fd75fc
LPeter1997 <lenkefi.peti@gmail.com>
2023-04-05 20:51:41 +0200

    Update FlowAnalysisPass.cs

--- src/Draco.Compiler/Internal/FlowAnalysis/FlowAnalysisPass.cs
+++ src/Draco.Compiler/Internal/FlowAnalysis/FlowAnalysisPass.cs
@@ -14,10 +14,53 @@ namespace Draco.Compiler.Internal.FlowAnalysis;
 /// <typeparam name="TState">The state being tracked by the pass.</typeparam>
 internal abstract class FlowAnalysisPass<TState> : BoundTreeVisitor
 {
+    // Lattice operations
+
     /// <summary>
-    /// The lattice used by this pass.
+    /// The top element of this lattice, which generally represents "reachable, but no information yet",
+    /// which is usually the state of the starting point.
     /// </summary>
-    protected ILattice<TState> Lattice { get; }
+    public abstract TState Top { get; }
+
+    /// <summary>
+    /// The bottom element of this lattice, usually representing "unreachable".
+    /// </summary>
+    public abstract TState Bottom { get; }
+
+    /// <summary>
+    /// Joins two elements of the lattice, usually corresponding to multiple paths
+    /// converging at the same point. Also known as the "least upper bound".
+    ///
+    /// Rules to hold:
+    ///  1) Join(Bottom, X) = X
+    ///  2) Join(Top, X) = Top
+    /// </summary>
+    /// <param name="target">The target state to join into.</param>
+    /// <param name="other">The state to merge into <paramref name="target"/>.</param>
+    /// <returns>True, if <paramref name="target"/> was changed, false otherwise.</returns>
+    public abstract bool Join(ref TState target, in TState other);
+
+    /// <summary>
+    /// Additively combines two elements of the lattice.
+    /// Also known as the "greatest lower bound".
+    ///
+    /// Rules to hold:
+    ///  1) Meet(Bottom, X) = Bottom
+    ///  2) Meet(Top, X) = X
+    /// </summary>
+    /// <param name="target">The target state to meet into.</param>
+    /// <param name="other">The state to merge into <paramref name="target"/>.</param>
+    /// <returns>True, if <paramref name="target"/> was changed, false otherwise.</returns>
+    public abstract bool Meet(ref TState target, in TState other);
+
+    /// <summary>
+    /// Deep-copies the given state.
+    /// </summary>
+    /// <param name="state">The state to deep-copy.</param>
+    /// <returns>An equivalent clone of <paramref name="state"/>.</returns>
+    public abstract TState Clone(in TState state);
+
+    // Flow analysis related things
 
     // NOTE: This is a field for a reason, we pass refs to this
     /// <summary>
@@ -29,16 +72,9 @@ internal abstract class FlowAnalysisPass<TState> : BoundTreeVisitor
 
     protected FlowAnalysisPass(ILattice<TState> lattice)
     {
-        this.Lattice = lattice;
+        // Assume beginning is reachable
         this.State = lattice.Top;
     }
-
-    private TState Top => this.Lattice.Top;
-    private TState Bottom => this.Lattice.Bottom;
-
-    private bool Join(in TState other) => this.Lattice.Join(ref this.State, in other);
-    private bool Meet(in TState other) => this.Lattice.Meet(ref this.State, in other);
-    private TState CloneState() => this.Lattice.Clone(this.State);
 
     private TState GetLabeledState(LabelSymbol label)
     {
@@ -56,7 +92,7 @@ internal abstract class FlowAnalysisPass<TState> : BoundTreeVisitor
         // First, the condition always executes
         this.VisitExpression(node.Condition);
         // Then we have two alternatives, so we save
-        var elseState = this.CloneState();
+        var elseState = this.Clone(this.State);
         // We run the 'then' alternative
         this.VisitExpression(node.Then);
         var thenState = this.State;
@@ -64,7 +100,7 @@ internal abstract class FlowAnalysisPass<TState> : BoundTreeVisitor
         this.State = elseState;
         this.VisitExpression(node.Else);
         // Finally, we merge
-        this.Join(in thenState);
+        this.Join(ref this.State, in thenState);
     }
 
     public override void VisitWhileExpression(BoundWhileExpression node)
@@ -80,13 +116,18 @@ internal abstract class FlowAnalysisPass<TState> : BoundTreeVisitor
         // Look up the previously saved label state
         var state = this.GetLabeledState(node.Label);
         // Join in
-        this.Join(in state);
+        this.Join(ref this.State, in state);
         // Save a copy of this new state for the label
-        this.labeledStates[node.Label] = this.CloneState();
+        this.labeledStates[node.Label] = this.Clone(this.State);
     }
 
     public override void VisitGotoExpression(BoundGotoExpression node)
     {
-        // TODO: Detach
+        // We join in with the referenced label
+        var state = this.GetLabeledState(node.Target);
+        this.Join(ref this.State, in state);
+        this.labeledStates[node.Target] = this.State;
+        // Below that the state is unreachable, detach
+        this.State = this.Bottom;
     }
 }

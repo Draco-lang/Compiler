126803c31ef0856754a5bc76fcc6d31a9fad0560
LPeter1997 <lenkefi.peti@gmail.com>
2023-04-05 10:21:11 +0200

    Update SourceGlobalSymbol.cs

--- src/Draco.Compiler/Internal/Symbols/Source/SourceGlobalSymbol.cs
+++ src/Draco.Compiler/Internal/Symbols/Source/SourceGlobalSymbol.cs
@@ -4,14 +4,22 @@ using Draco.Compiler.Api.Syntax;
 using Draco.Compiler.Internal.Binding;
 using Draco.Compiler.Internal.BoundTree;
 using Draco.Compiler.Internal.Declarations;
+using Draco.Compiler.Internal.Solver;
 using Draco.Compiler.Internal.Types;
 
 namespace Draco.Compiler.Internal.Symbols.Source;
 
 internal sealed class SourceGlobalSymbol : GlobalSymbol
 {
-    public override Type Type => this.type ??= this.BuildType();
-    private Type? type;
+    public override Type Type
+    {
+        get
+        {
+            if (this.type is null) this.Build();
+            Debug.Assert(this.type is not null);
+            return this.type;
+        }
+    }
 
     public override bool IsMutable => this.declaration.Syntax.Keyword.Kind == TokenKind.KeywordVar;
     public override Symbol? ContainingSymbol { get; }
@@ -19,14 +27,21 @@ internal sealed class SourceGlobalSymbol : GlobalSymbol
 
     public override VariableDeclarationSyntax DeclarationSyntax => this.declaration.Syntax;
 
-    public BoundExpression? Value => this.DeclarationSyntax.Value is null
-        ? null
-        : (this.value ??= this.BuildValue());
-    private BoundExpression? value;
+    public BoundExpression? Value
+    {
+        get
+        {
+            if (this.value is null) this.Build();
+            Debug.Assert(this.value is not null);
+            return this.value;
+        }
+    }
 
     public override string Documentation => this.DeclarationSyntax.Documentation;
 
     private readonly GlobalDeclaration declaration;
+    private Type? type;
+    private BoundExpression? value;
 
     public SourceGlobalSymbol(Symbol? containingSymbol, GlobalDeclaration declaration)
     {
@@ -34,57 +49,12 @@ internal sealed class SourceGlobalSymbol : GlobalSymbol
         this.declaration = declaration;
     }
 
-    private Type BuildType()
+    private void Build()
     {
         Debug.Assert(this.DeclaringCompilation is not null);
         var diagnostics = this.DeclaringCompilation.GlobalDiagnosticBag;
 
-        if (this.DeclarationSyntax.Type is not null)
-        {
-            // var x: T;
-            // Type is present
-            var binder = this.DeclaringCompilation.GetBinder(this.DeclarationSyntax.Type.Type);
-            var typeSymbol = (TypeSymbol)binder.BindType(this.DeclarationSyntax.Type.Type, diagnostics);
-            var type = typeSymbol.Type;
-            // Check for value
-            if (this.DeclarationSyntax.Value is not null)
-            {
-                var value = this.Value!;
-                // TODO: In reality we should check assignability, but that's
-                // not exposed, only in constraint bag
-                if (!ReferenceEquals(type, value.TypeRequired))
-                {
-                    diagnostics.Add(Diagnostic.Create(
-                        template: TypeCheckingErrors.TypeMismatch,
-                        location: this.DeclarationSyntax.Value.Value.Location,
-                        formatArgs: new[] { type, value.TypeRequired }));
-                }
-            }
-            return type;
-        }
-        else if (this.DeclarationSyntax.Value is not null)
-        {
-            // var x = value;
-            // Infer from value type
-            return this.Value!.TypeRequired;
-        }
-        else
-        {
-            // A global without a type or value, error
-            diagnostics.Add(Diagnostic.Create(
-                template: TypeCheckingErrors.CouldNotInferType,
-                location: this.DeclarationSyntax.Location,
-                formatArgs: this.Name));
-            return ErrorType.Instance;
-        }
-    }
-
-    private BoundExpression BuildValue()
-    {
-        Debug.Assert(this.DeclaringCompilation is not null);
-
-        var binder = this.DeclaringCompilation.GetBinder(this.DeclarationSyntax.Value!.Value);
-        var diagnostics = this.DeclaringCompilation.GlobalDiagnosticBag;
-        return binder.BindGlobalValue(this.DeclarationSyntax.Value!.Value, diagnostics);
+        // TODO
+        throw new System.NotImplementedException();
     }
 }

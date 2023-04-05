2eedd309fec2e78e6a6d870609fae2c75fd3cce0
LPeter1997 <lenkefi.peti@gmail.com>
2023-04-05 20:30:56 +0200

    Update FlowAnalysisPass.cs

--- src/Draco.Compiler/Internal/FlowAnalysis/FlowAnalysisPass.cs
+++ src/Draco.Compiler/Internal/FlowAnalysis/FlowAnalysisPass.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Draco.Compiler.Internal.BoundTree;
+using Draco.Compiler.Internal.Symbols;
 
 namespace Draco.Compiler.Internal.FlowAnalysis;
 
@@ -24,15 +25,31 @@ internal abstract class FlowAnalysisPass<TState> : BoundTreeVisitor
     /// </summary>
     protected TState State;
 
+    private readonly Dictionary<LabelSymbol, TState> labeledStates = new();
+
     protected FlowAnalysisPass(ILattice<TState> lattice)
     {
         this.Lattice = lattice;
         this.State = lattice.Top;
     }
 
-    private bool Join(in TState other) => this.Lattice.Join(ref this.State, other);
-    private bool Meet(in TState other) => this.Lattice.Meet(ref this.State, other);
+    private TState Top => this.Lattice.Top;
+    private TState Bottom => this.Lattice.Bottom;
+
+    private bool Join(in TState other) => this.Lattice.Join(ref this.State, in other);
+    private bool Meet(in TState other) => this.Lattice.Meet(ref this.State, in other);
     private TState CloneState() => this.Lattice.Clone(this.State);
+
+    private TState GetLabeledState(LabelSymbol label)
+    {
+        if (!this.labeledStates.TryGetValue(label, out var state))
+        {
+            // By default labels are unreachable
+            state = this.Bottom;
+            this.labeledStates.Add(label, state);
+        }
+        return state;
+    }
 
     public override void VisitIfExpression(BoundIfExpression node)
     {
@@ -60,7 +77,12 @@ internal abstract class FlowAnalysisPass<TState> : BoundTreeVisitor
 
     public override void VisitLabelStatement(BoundLabelStatement node)
     {
-        // TODO: Join in?
+        // Look up the previously saved label state
+        var state = this.GetLabeledState(node.Label);
+        // Join in
+        this.Join(in state);
+        // Save a copy of this new state for the label
+        this.labeledStates[node.Label] = this.CloneState();
     }
 
     public override void VisitGotoExpression(BoundGotoExpression node)

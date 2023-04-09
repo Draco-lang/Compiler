1bf78fe0700754d6a811fca537424aa56e2db21c
LPeter1997 <lenkefi.peti@gmail.com>
2023-04-09 18:28:27 +0200

    Update ModuleSymbol.cs

--- src/Draco.Compiler/Internal/Symbols/ModuleSymbol.cs
+++ src/Draco.Compiler/Internal/Symbols/ModuleSymbol.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
 namespace Draco.Compiler.Internal.Symbols;
 
 /// <summary>
@@ -7,4 +12,22 @@ internal abstract partial class ModuleSymbol : Symbol
 {
     public override void Accept(SymbolVisitor visitor) => visitor.VisitModule(this);
     public override TResult Accept<TResult>(SymbolVisitor<TResult> visitor) => visitor.VisitModule(this);
+
+    // TODO: Doc, make up something nicer?
+    // NOTE: Some very janky lookup capability
+    public IEnumerable<Symbol> Lookup(ImmutableArray<string> parts)
+    {
+        if (parts.Length == 0) return new[] { this };
+
+        var current = this as Symbol;
+        for (var i = 0; i < parts.Length - 1; ++i)
+        {
+            var part = parts[i];
+            current = current.Members
+                .Where(m => m.Name == part)
+                .Single();
+        }
+
+        return current.Members.Where(m => m.Name == parts[^1]);
+    }
 }

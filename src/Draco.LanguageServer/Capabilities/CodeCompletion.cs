204c8fc44ab44414f51a08e55e7b861d14021bad
jakubbinter <kuba.binter@gmail.com>
2023-04-11 15:27:03 +0200

    use shared parse tree

--- src/Draco.LanguageServer/Capabilities/CodeCompletion.cs
+++ src/Draco.LanguageServer/Capabilities/CodeCompletion.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using CompletionService = Draco.Compiler.Api.CodeCompletion.CompletionService;
-using Draco.Compiler.Api.Syntax;
 using Draco.Lsp.Model;
 using Draco.Lsp.Server.Language;
 
@@ -19,9 +18,6 @@ internal sealed partial class DracoLanguageServer : ICodeCompletion
     public Task<IList<CompletionItem>> CompleteAsync(CompletionParams param, CancellationToken cancellationToken)
     {
         var cursorPosition = Translator.ToCompiler(param.Position);
-        var souceText = this.documentRepository.GetDocument(param.TextDocument.Uri);
-        var syntaxTree = SyntaxTree.Parse(souceText);
-        var completionItems = new List<CompletionItem>();
-        return Task.FromResult<IList<CompletionItem>>(CompletionService.GetCompletions(syntaxTree, cursorPosition).Select(x => Translator.ToLsp(x)).ToList());
+        return Task.FromResult<IList<CompletionItem>>(CompletionService.GetCompletions(this.syntaxTree, cursorPosition).Select(x => Translator.ToLsp(x)).ToList());
     }
 }

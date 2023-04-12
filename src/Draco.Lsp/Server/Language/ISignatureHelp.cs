ae21dbbf105465a951848bee21c3ccfc3258e310
jakubbinter <kuba.binter@gmail.com>
2023-04-12 16:24:44 +0200

    started signature help request

---
+++ src/Draco.Lsp/Server/Language/ISignatureHelp.cs
@@ -0,0 +1,20 @@
+using Draco.Lsp.Attributes;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using Draco.Lsp.Attributes;
+using Draco.Lsp.Model;
+
+namespace Draco.Lsp.Server.Language;
+
+public interface ISignatureHelp
+{
+    [Capability(nameof(ServerCapabilities.SignatureHelpProvider))]
+    public SignatureHelpOptions? Capability => null;
+
+    [RegistrationOptions("textDocument/signatureHelp")]
+    public SignatureHelpRegistrationOptions SignatureHelpRegistrationOptions { get; }
+
+    [Request("textDocument/signatureHelp")]
+    public Task<IList<TextEdit>?> FormatTextDocumentAsync(SignatureHelppa param, CancellationToken cancellationToken);
+}

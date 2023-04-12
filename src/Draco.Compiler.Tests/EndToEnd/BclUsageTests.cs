a0da8f268749d5ddbbeb03b91f8696ebbb2cc0b8
LPeter1997 <lenkefi.peti@gmail.com>
2023-04-12 14:51:36 +0200

    Update BclUsageTests.cs

--- src/Draco.Compiler.Tests/EndToEnd/BclUsageTests.cs
+++ src/Draco.Compiler.Tests/EndToEnd/BclUsageTests.cs
@@ -49,4 +49,31 @@ public sealed class BclUsageTests : EndToEndTestsBase
 
         Assert.Equal("1 + 2 = 3", stringWriter.ToString());
     }
+
+    [Fact]
+    public void BasicStringBuilding()
+    {
+        var assembly = Compile("""
+            import System.Console;
+            import System.Text;
+
+            func main() {
+                var sb = StringBuilder();
+                sb.Append("Hello, ");
+                sb.Append(123);
+                sb.Append(true);
+                sb.Append(" - and bye!");
+                Write(sb.ToString());
+            }
+            """);
+
+        var stringWriter = new StringWriter();
+        var _ = Invoke<object?>(
+            assembly: assembly,
+            methodName: "main",
+            stdin: null,
+            stdout: stringWriter);
+
+        Assert.Equal("Hello, 123True - and bye!", stringWriter.ToString());
+    }
 }

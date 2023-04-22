121070343acc9c45e0265d6d1bc22c7b11f047c9
jakubbinter <kuba.binter@gmail.com>
2023-04-22 20:08:24 +0200

    reorder members of text edit

--- src/Draco.Compiler/Api/TextEdit.cs
+++ src/Draco.Compiler/Api/TextEdit.cs
@@ -5,6 +5,6 @@ namespace Draco.Compiler.Api;
 /// <summary>
 /// Represents an edit in source text.
 /// </summary>
-/// <param name="Text">The text that should be placed into the source document.</param>
 /// <param name="Range">The range of the text that will be replaced by <paramref name="Text"/>.</param>
-public record class TextEdit(string Text, SyntaxRange Range);
+/// <param name="Text">The text that should be placed into the source document.</param>
+public record class TextEdit(SyntaxRange Range, string Text);

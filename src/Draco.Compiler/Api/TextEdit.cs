6e29e8bf22f7a6cc078bbcc2526a52fe8adcea9e
jakubbinter <76395771+jakubbinter@users.noreply.github.com>
2023-04-22 21:19:32 +0200

    Update src/Draco.Compiler/Api/TextEdit.cs
    Co-authored-by: LPeter1997 <lenkefi.peti@gmail.com>

--- src/Draco.Compiler/Api/TextEdit.cs
+++ src/Draco.Compiler/Api/TextEdit.cs
@@ -6,5 +6,5 @@ namespace Draco.Compiler.Api;
 /// Represents an edit in source text.
 /// </summary>
 /// <param name="Range">The range of the text that will be replaced by <paramref name="Text"/>.</param>
-/// <param name="Text">The text that should be placed into the source document.</param>
+/// <param name="Text">The text that should be inserted into the source text.</param>
 public record class TextEdit(SyntaxRange Range, string Text);

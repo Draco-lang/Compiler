a7cb7459e14a80c5f5b0c193362936013b237407
jakubbinter <76395771+jakubbinter@users.noreply.github.com>
2023-04-22 20:06:34 +0200

    Update src/Draco.Compiler/Api/CodeCompletion/CompletionItem.cs
    Co-authored-by: LPeter1997 <lenkefi.peti@gmail.com>

--- src/Draco.Compiler/Api/CodeCompletion/CompletionItem.cs
+++ src/Draco.Compiler/Api/CodeCompletion/CompletionItem.cs
@@ -7,7 +7,7 @@ namespace Draco.Compiler.Api.CodeCompletion;
 /// <summary>
 /// Represents a suggestion for completing the code.
 /// </summary>
-/// <param name="Edits">The <see cref="TextEdit"/>s this item provides.</param>
+/// <param name="Edits">The <see cref="TextEdit"/>s the completion would perform.</param>
 /// <param name="Symbols">All <see cref="ISymbol"/>s representing this completion (usually symbol representing type etc. or multiple <see cref="FunctionSymbol"/>s representing an overload).</param>
 /// <param name="Kind">The <see cref="CompletionKind"/> of this completion.</param>
 public sealed record class CompletionItem(ImmutableArray<TextEdit> Edits, string DisplayText, ImmutableArray<ISymbol> Symbols, CompletionKind Kind)

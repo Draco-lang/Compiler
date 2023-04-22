a9138a6a5ebff4e59166e930cc7d13bfdd67f36a
LPeter1997 <lenkefi.peti@gmail.com>
2023-04-22 21:09:32 +0200

    Create InstantiateConstraint.cs

---
+++ src/Draco.Compiler/Internal/Solver/InstantiateConstraint.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Draco.Compiler.Api.Semantics;
+
+namespace Draco.Compiler.Internal.Solver;
+
+/// <summary>
+/// Represents a generic type instantiation.
+/// </summary>
+internal sealed class InstantiateConstraint : Constraint
+{
+    /// <summary>
+    /// The type to instantiate.
+    /// </summary>
+    public TypeSymbol ToInstantiate { get; }
+
+    /// <summary>
+    /// The instantiated type.
+    /// </summary>
+    public TypeSymbol Instantiated { get; }
+
+    /// <summary>
+    /// The arguments the type gets instantiated with.
+    /// </summary>
+    public ImmutableArray<TypeSymbol> Arguments { get; }
+
+    /// <summary>
+    /// The promise of this constraint.
+    /// </summary>
+    public ConstraintPromise<TypeSymbol> Promise { get; }
+
+    public InstantiateConstraint(
+        TypeSymbol toInstantiate,
+        TypeSymbol instantiated,
+        ImmutableArray<TypeSymbol> arguments)
+    {
+        this.ToInstantiate = toInstantiate;
+        this.Instantiated = instantiated;
+        this.Arguments = arguments;
+        this.Promise = ConstraintPromise.Create<TypeSymbol>(this);
+    }
+}

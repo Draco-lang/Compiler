55a590ec56c45b8c39b8708a566a84d6677ea3ba
jakubbinter <kuba.binter@gmail.com>
2023-04-23 21:08:53 +0200

    changed ICodeAction to return IList

--- src/Draco.LanguageServer/Capabilities/CodeAction.cs
+++ src/Draco.LanguageServer/Capabilities/CodeAction.cs
@@ -17,27 +17,27 @@ internal sealed partial class DracoLanguageServer : ICodeAction
         ResolveProvider = false
     };
 
-    public Task<OneOf<Command, CodeAction>[]?> CodeActionAsync(CodeActionParams param, CancellationToken cancellationToken)
+    public Task<IList<OneOf<Command, CodeAction>>?> CodeActionAsync(CodeActionParams param, CancellationToken cancellationToken)
     {
         var fixes = this.codeFixService.GetCodeFixes(this.syntaxTree, this.semanticModel, Translator.ToCompiler(param.Range));
-        var actions = new OneOf<Command, CodeAction>[fixes.Length];
+        var actions = new List<OneOf<Command, CodeAction>>();
 
-        for (int i = 0; i < fixes.Length; i++)
+        foreach (var fix in fixes)
         {
-            actions[i] = new CodeAction()
+            actions.Add(new CodeAction()
             {
-                Title = fixes[i].DisplayText,
+                Title = fix.DisplayText,
                 //TODO: we might have some other fixes in future
                 Kind = CodeActionKind.QuickFix,
                 Edit = new WorkspaceEdit()
                 {
                     Changes = new Dictionary<DocumentUri, IList<Lsp.Model.TextEdit>>()
                     {
-                        { param.TextDocument.Uri, fixes[i].Edits.Select(x => Translator.ToLsp(x)).ToList() }
+                        { param.TextDocument.Uri, fix.Edits.Select(x => Translator.ToLsp(x)).ToList() }
                     }
                 }
-            };
+            });
         }
-        return Task.FromResult<OneOf<Command, CodeAction>[]?>(actions);
+        return Task.FromResult<IList<OneOf<Command, CodeAction>>?>(actions);
     }
 }

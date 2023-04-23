8bb005b11ef220080dcc182b6bad0e5adadd4a87
jakubbinter <76395771+jakubbinter@users.noreply.github.com>
2023-04-23 19:17:37 +0200

    Update src/Draco.Compiler/Api/CodeFixes/CodeFix.cs
    Co-authored-by: LPeter1997 <lenkefi.peti@gmail.com>

--- src/Draco.Compiler/Api/CodeFixes/CodeFix.cs
+++ src/Draco.Compiler/Api/CodeFixes/CodeFix.cs
@@ -8,5 +8,5 @@ namespace Draco.Compiler.Api.CodeFixes;
 /// Represents a possible codefix.
 /// </summary>
 /// <param name="DisplayText">The user-friendly description of this <see cref="CodeFix"/>.</param>
-/// <param name="Edits">The <see cref="TextEdit"/>s this <see cref="CodeFix"/> will do if aplied.</param>
+/// <param name="Edits">The <see cref="TextEdit"/>s this <see cref="CodeFix"/> will do, if aplied.</param>
 public sealed record class CodeFix(string DisplayText, ImmutableArray<TextEdit> Edits);

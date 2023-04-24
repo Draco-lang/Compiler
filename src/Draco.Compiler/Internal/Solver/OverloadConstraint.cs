3ce3e228e940664e04f88ae48e7a789168ada912
LPeter1997 <lenkefi.peti@gmail.com>
2023-04-24 15:31:13 +0200

    Update OverloadConstraint.cs

--- src/Draco.Compiler/Internal/Solver/OverloadConstraint.cs
+++ src/Draco.Compiler/Internal/Solver/OverloadConstraint.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -53,31 +54,63 @@ internal sealed class OverloadConstraint : Constraint<FunctionSymbol>
         if (this.scoreVectors is null)
         {
             // Score vectors have not been calculated yet
-            this.scoreVectors = new();
-
-            for (var i = 0; i < this.Candidates.Count;)
-            {
-                var candidate = this.Candidates[i];
-
-                // Broad filtering, skip candidates that have the wrong no. parameters
-                if (candidate.Parameters.Length != this.Arguments.Length)
-                {
-                    this.Candidates.RemoveAt(i);
-                }
-                else
-                {
-                    // Initialize score vector
-                    var scoreVector = new int?[this.Arguments.Length];
-                    Array.Fill(scoreVector, null);
-                    this.scoreVectors.Add(scoreVector);
-                    ++i;
-                }
-            }
+            this.scoreVectors = this.InitializeScoreVectors();
             return SolveState.Advanced;
         }
 
         // The score vectors are already initialized
+        Debug.Assert(this.Candidates.Count == this.scoreVectors.Count);
+        for (var i = 0; i < this.Candidates.Count;)
+        {
+            var candidate = this.Candidates[i];
+            var scoreVector = this.scoreVectors[i];
+
+            // TODO
+            throw new NotImplementedException();
+        }
+
         // TODO
         throw new NotImplementedException();
+    }
+
+    private List<int?[]> InitializeScoreVectors()
+    {
+        var scoreVectors = new List<int?[]>();
+
+        for (var i = 0; i < this.Candidates.Count;)
+        {
+            var candidate = this.Candidates[i];
+
+            // Broad filtering, skip candidates that have the wrong no. parameters
+            if (candidate.Parameters.Length != this.Arguments.Length)
+            {
+                this.Candidates.RemoveAt(i);
+            }
+            else
+            {
+                // Initialize score vector
+                var scoreVector = new int?[this.Arguments.Length];
+                Array.Fill(scoreVector, null);
+                scoreVectors.Add(scoreVector);
+                ++i;
+            }
+        }
+
+        return scoreVectors;
+    }
+
+    private static bool IsWellDefined(int?[] vector) => vector.All(x => x is not null);
+
+    private static bool Dominates(int?[] a, int?[] b)
+    {
+        Debug.Assert(a.Length == b.Length);
+
+        for (var i = 0; i < a.Length; ++i)
+        {
+            if (a[i] is null || b[i] is null) return false;
+            if (a[i] < b[i]) return false;
+        }
+
+        return true;
     }
 }

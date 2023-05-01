e7a3e2157e2ad54e005b2936c1b750606090bbab
LPeter1997 <lenkefi.peti@gmail.com>
2023-05-01 12:04:17 +0200

    Elaboration

---
+++ src/Draco.Compiler/Internal/Symbols/Generic/FunctionInstanceSymbol.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draco.Compiler.Internal.Symbols.Generic;
+
+/// <summary>
+/// Represents a generic instantiated function.
+/// </summary>
+internal sealed class FunctionInstanceSymbol : FunctionSymbol
+{
+    public override ImmutableArray<TypeParameterSymbol> GenericParameters => throw new NotImplementedException();
+    public override ImmutableArray<ParameterSymbol> Parameters => throw new NotImplementedException();
+    public override TypeSymbol ReturnType => throw new NotImplementedException();
+
+    public override bool IsMember => this.GenericDefinition.IsMember;
+    public override bool IsVirtual => this.GenericDefinition.IsVirtual;
+
+    public override Symbol? ContainingSymbol => this.GenericDefinition.ContainingSymbol;
+    public override FunctionSymbol GenericDefinition { get; }
+
+    public override FunctionSymbol GenericInstantiate(ImmutableDictionary<TypeParameterSymbol, TypeSymbol> substitutions) =>
+        throw new NotImplementedException();
+}
---
+++ src/Draco.Compiler/Internal/Symbols/Generic/GenericContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draco.Compiler.Internal.Symbols.Generic;
+
+internal readonly struct GenericContext
+{
+    // TODO
+}

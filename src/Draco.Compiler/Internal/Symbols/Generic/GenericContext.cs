6a16d528a97aec43adb5f0c08b1ebeedc7dc877d
LPeter1997 <lenkefi.peti@gmail.com>
2023-05-01 13:04:34 +0200

    Update GenericContext.cs

--- src/Draco.Compiler/Internal/Symbols/Generic/GenericContext.cs
+++ src/Draco.Compiler/Internal/Symbols/Generic/GenericContext.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +13,30 @@ namespace Draco.Compiler.Internal.Symbols.Generic;
 /// <summary>
 /// Represents a generic context (substituted type-variables).
 /// </summary>
-internal readonly struct GenericContext
+internal readonly struct GenericContext : IReadOnlyDictionary<TypeParameterSymbol, TypeSymbol>
 {
-    // TODO
+    public int Count => this.substitutions.Count;
+
+    public IEnumerable<TypeParameterSymbol> Keys => this.substitutions.Keys;
+    public IEnumerable<TypeSymbol> Values => this.substitutions.Values;
+
+    public TypeSymbol this[TypeParameterSymbol key] => this.substitutions[key];
+
+    private readonly ImmutableDictionary<TypeParameterSymbol, TypeSymbol> substitutions;
+
+    public GenericContext(ImmutableDictionary<TypeParameterSymbol, TypeSymbol> substitutions)
+    {
+        this.substitutions = substitutions;
+    }
+
+    public GenericContext(IEnumerable<KeyValuePair<TypeParameterSymbol, TypeSymbol>> substitutions)
+        : this(substitutions.ToImmutableDictionary())
+    {
+    }
+
+    public bool TryGetValue(TypeParameterSymbol key, [MaybeNullWhen(false)] out TypeSymbol value) =>
+        this.substitutions.TryGetValue(key, out value);
+    public bool ContainsKey(TypeParameterSymbol key) => this.substitutions.ContainsKey(key);
+    public IEnumerator<KeyValuePair<TypeParameterSymbol, TypeSymbol>> GetEnumerator() => this.substitutions.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
 }

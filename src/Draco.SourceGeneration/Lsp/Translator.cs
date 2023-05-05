85ee01ecc4934d5dabb2e586b96949c604743a18
LPeter1997 <lenkefi.peti@gmail.com>
2023-05-05 15:36:34 +0200

    Update Translator.cs

--- src/Draco.SourceGeneration/Lsp/Translator.cs
+++ src/Draco.SourceGeneration/Lsp/Translator.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Draco.SourceGeneration.Lsp.Metamodel;
+using Cs = Draco.SourceGeneration.Lsp.CsModel;
+using Ts = Draco.SourceGeneration.Lsp.Metamodel;
 
 namespace Draco.SourceGeneration.Lsp;
 
@@ -9,4 +12,11 @@ namespace Draco.SourceGeneration.Lsp;
 /// </summary>
 internal sealed class Translator
 {
+    private readonly Ts.MetaData sourceModel;
+    private readonly Cs.Model targetModel = new();
+
+    private Translator(MetaData sourceModel)
+    {
+        this.sourceModel = sourceModel;
+    }
 }

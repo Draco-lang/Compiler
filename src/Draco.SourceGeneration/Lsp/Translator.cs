02a07cb0c3ecf42c980fb5435c4c39deb1788d2f
LPeter1997 <lenkefi.peti@gmail.com>
2023-05-05 15:17:32 +0200

    Small fixes

---
+++ src/Draco.SourceGeneration/Lsp/Translator.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Draco.SourceGeneration.Lsp;
+
+/// <summary>
+/// Translates the TS-based metamodel into a C# model.
+/// </summary>
+internal sealed class Translator
+{
+}

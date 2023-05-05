63fe52a23dcf3995f48fe58f39c400e1857f2a8c
LPeter1997 <lenkefi.peti@gmail.com>
2023-05-05 12:04:53 +0200

    Update AggregateType.cs

--- src/Draco.SourceGeneration/Lsp/Metamodel/AggregateType.cs
+++ src/Draco.SourceGeneration/Lsp/Metamodel/AggregateType.cs
@@ -9,7 +9,7 @@ namespace Draco.SourceGeneration.Lsp.Metamodel;
 /// </summary>
 internal sealed class AggregateType : Type
 {
-    public string Kind { get; set; } = string.Empty;
+    public override string Kind { get; set; } = string.Empty;
 
     public IList<Type> Items { get; set; } = Array.Empty<Type>();
 }

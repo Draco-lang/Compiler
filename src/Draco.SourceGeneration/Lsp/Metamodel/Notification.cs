dc9a85682f0f0f579592107e3e8c9d584d148994
LPeter1997 <lenkefi.peti@gmail.com>
2023-05-05 12:01:58 +0200

    Metadata typed up

--- src/Draco.SourceGeneration/Lsp/Metamodel/Notification.cs
+++ src/Draco.SourceGeneration/Lsp/Metamodel/Notification.cs
@@ -13,4 +13,51 @@ internal sealed class Notification
     /// The notification's method name.
     /// </summary>
     public string Method { get; set; } = string.Empty;
+
+    // NOTE: The metamodel specifies Type | Type[], but an array is never used here.
+    /// <summary>
+    /// The parameter type(s) if any.
+    /// </summary>
+    public Type? Params { get; set; }
+
+    /// <summary>
+    /// Optional a dynamic registration method if it
+	/// different from the notification's method.
+    /// </summary>
+    public string? RegistrationMethod { get; set; }
+
+    /// <summary>
+    /// Optional registration options if the notification
+	/// supports dynamic registration.
+    /// </summary>
+    public Type? RegistrationOptions { get; set; }
+
+    /// <summary>
+    /// The direction in which this request is sent
+	/// in the protocol.
+    /// </summary>
+    public MessageDirection MessageDirection { get; set; }
+
+    /// <summary>
+    /// An optional documentation.
+    /// </summary>
+    public string? Documentation { get; set; }
+
+    /// <summary>
+    /// Since when (release number) this notification is
+	/// available.Is undefined if not known.
+    /// </summary>
+    public string? Since { get; set; }
+
+    /// <summary>
+    /// Whether this is a proposed notification. If omitted
+	/// the notification is final.
+    /// </summary>
+    public bool? Proposed { get; set; }
+
+    /// <summary>
+    /// Whether the notification is deprecated or not. If deprecated
+	/// the property contains the deprecation message.
+    /// </summary>
+    public string? Deprecated { get; set; }
 }
--- src/Draco.SourceGeneration/Lsp/Metamodel/Request.cs
+++ src/Draco.SourceGeneration/Lsp/Metamodel/Request.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Draco.SourceGeneration.Lsp.Metamodel;
@@ -13,4 +14,67 @@ internal sealed class Request
     /// The request's method name.
     /// </summary>
     public string Method { get; set; } = string.Empty;
+
+    // NOTE: The metamodel specifies Type | Type[], but an array is never used here.
+    /// <summary>
+    /// The parameter type(s) if any.
+    /// </summary>
+    public Type? Params { get; set; }
+
+    /// <summary>
+    /// The result type.
+    /// </summary>
+    public Type Result { get; set; } = null!;
+
+    /// <summary>
+    /// Optional partial result type if the request
+	/// supports partial result reporting.
+    /// </summary>
+    public Type? PartialResult { get; set; }
+
+    /// <summary>
+    /// An optional error data type.
+    /// </summary>
+    public Type? ErrorData { get; set; }
+
+    /// <summary>
+    /// Optional a dynamic registration method if it
+	/// different from the request's method.
+    /// </summary>
+    public string? RegistrationMethod { get; set; }
+
+    /// <summary>
+    /// Optional registration options if the request
+	/// supports dynamic registration.
+    /// </summary>
+    public Type? RegistrationOptions { get; set; }
+
+    /// <summary>
+    /// The direction in which this notification is sent
+	/// in the protocol.
+    /// </summary>
+    public MessageDirection MessageDirection { get; set; }
+
+    /// <summary>
+    /// An optional documentation.
+    /// </summary>
+    public string? Documentation { get; set; }
+
+    /// <summary>
+    /// Since when (release number) this request is
+	/// available.Is undefined if not known.
+    /// </summary>
+    public string? Since { get; set; }
+
+    /// <summary>
+    /// Whether this is a proposed feature. If omitted
+	/// the feature is final.
+    /// </summary>
+    public bool? Proposed { get; set; }
+
+    /// <summary>
+    /// Whether the request is deprecated or not. If deprecated
+	/// the property contains the deprecation message.
+    /// </summary>
+    public string? Deprecated { get; set; }
 }

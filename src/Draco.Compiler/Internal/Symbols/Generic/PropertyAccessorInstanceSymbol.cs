452e578903d5099b989b9f0b82bc78a9c76d4f9a
Binto86 <76395771+Binto86@users.noreply.github.com>
2023-05-27 16:54:05 +0200

    Update src/Draco.Compiler/Internal/Symbols/Generic/PropertyAccessorInstanceSymbol.cs
    Co-authored-by: LPeter1997 <lenkefi.peti@gmail.com>

--- src/Draco.Compiler/Internal/Symbols/Generic/PropertyAccessorInstanceSymbol.cs
+++ src/Draco.Compiler/Internal/Symbols/Generic/PropertyAccessorInstanceSymbol.cs
@@ -3,7 +3,9 @@ namespace Draco.Compiler.Internal.Symbols.Generic;
 internal sealed class PropertyAccessorInstanceSymbol : FunctionInstanceSymbol, IPropertyAccessorSymbol
 {
     public PropertySymbol Property { get; }
-    public PropertyAccessorInstanceSymbol(Symbol? containingSymbol, FunctionSymbol genericDefinition, GenericContext context, PropertySymbol property) : base(containingSymbol, genericDefinition, context)
+
+    public PropertyAccessorInstanceSymbol(Symbol? containingSymbol, FunctionSymbol genericDefinition, GenericContext context, PropertySymbol property)
+        : base(containingSymbol, genericDefinition, context)
     {
         this.Property = property;
     }

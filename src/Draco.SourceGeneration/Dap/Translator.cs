3063f0a5416a50cfe17288b068f1c2dc7569cc59
LPeter1997 <lenkefi.peti@gmail.com>
2023-05-29 16:01:13 +0200

    Fixes

--- src/Draco.SourceGeneration/Dap/Translator.cs
+++ src/Draco.SourceGeneration/Dap/Translator.cs
@@ -49,30 +49,33 @@ internal sealed class Translator
         if (typeToTranslate.Name is null) throw new KeyNotFoundException($"the type {typeName} could not be found for translation");
 
         var target = new Class();
+        target.Name = typeName;
         this.translatedTypes.Add(path, target);
         this.targetModel.Classes.Add(target);
-        this.TranslateType(typeName, typeToTranslate.Value, target);
+        this.TranslateType(typeToTranslate.Value, target);
         return target;
     }
 
-    private void TranslateType(string name, JsonElement sourceType, Class targetType)
+    private void TranslateType(JsonElement sourceType, Class targetType)
     {
-        if (sourceType.ValueKind != JsonValueKind.Object) throw new ArgumentException($"definition {name} is not an object");
-
-        ExtractDocumentation(sourceType, targetType);
-        targetType.Name = name;
+        if (sourceType.ValueKind != JsonValueKind.Object) throw new ArgumentException($"definition {targetType.Name} is not an object");
 
         if (sourceType.TryGetProperty("allOf", out var elements))
         {
             var toImplement = elements.EnumerateArray().ToList();
-            if (toImplement.Count != 2) throw new NotSupportedException($"definition {name} does not mix in 2 types");
+            if (toImplement.Count != 2) throw new NotSupportedException($"definition {targetType.Name} does not mix in 2 types");
 
             // Base class
-            if (!TryGetRef(toImplement[0], out var path)) throw new NotSupportedException($"definition {name} uses allOf, but does not mix in a type as its first element");
+            if (!TryGetRef(toImplement[0], out var path)) throw new NotSupportedException($"definition {targetType.Name} uses allOf, but does not mix in a type as its first element");
             targetType.Base = this.TranslateByPath(path);
+
+            // Mixin
+            this.TranslateType(toImplement[1], targetType);
         }
         else
         {
+            ExtractDocumentation(sourceType, targetType);
+
             // TODO
         }
     }

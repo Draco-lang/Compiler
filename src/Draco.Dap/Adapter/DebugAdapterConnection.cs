b07ee127f232ff4bb28fcfd6ef5647e09f60b594
LPeter1997 <lenkefi.peti@gmail.com>
2023-05-30 14:24:51 +0200

    Fix

---
+++ src/Draco.Dap/Adapter/DebugAdapterConnection.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Buffers;
+using System.Buffers.Text;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.IO.Pipelines;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Draco.Dap.Adapter;
+
+public sealed class DebugAdapterConnection
+{
+}

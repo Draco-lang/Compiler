836c1e9a98cbf8b5ea9852db055241a0fddd03d5
LPeter1997 <lenkefi.peti@gmail.com>
2023-06-01 11:13:32 +0200

    Update DracoDebugAdapter.cs

--- src/Draco.DebugAdapter/DracoDebugAdapter.cs
+++ src/Draco.DebugAdapter/DracoDebugAdapter.cs
@@ -1,17 +1,21 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Draco.Dap.Adapter;
 using Draco.Dap.Model;
+using Draco.Debugger;
 
 namespace Draco.DebugAdapter;
 
 internal sealed partial class DracoDebugAdapter : IDebugAdapter
 {
     private readonly IDebugClient client;
+    private DebuggerHost debuggerHost = null!;
+    private Debugger.Debugger debugger = null!;
 
     public DracoDebugAdapter(IDebugClient client)
     {
@@ -20,22 +24,54 @@ internal sealed partial class DracoDebugAdapter : IDebugAdapter
 
     public void Dispose() { }
 
-    public Task InitializeAsync(InitializeRequestArguments args) => Task.CompletedTask;
+    public Task InitializeAsync(InitializeRequestArguments args)
+    {
+        var dbgShim = FindDbgShim();
+        this.debuggerHost = DebuggerHost.Create(dbgShim);
+
+        return Task.CompletedTask;
+    }
+
+    // TODO: Temporary
+    private static string FindDbgShim()
+    {
+        var root = "C:\\Program Files\\dotnet\\shared\\Microsoft.NETCore.App";
+
+        if (!Directory.Exists(root))
+        {
+            throw new InvalidOperationException($"Cannot find dbgshim.dll: '{root}' does not exist");
+        }
+
+        foreach (var dir in Directory.EnumerateDirectories(root).Reverse())
+        {
+            var dbgshim = Directory.EnumerateFiles(dir, "dbgshim.dll").FirstOrDefault();
+            if (dbgshim is not null) return dbgshim;
+        }
+
+        throw new InvalidOperationException($"Failed to find a runtime containing dbgshim.dll under '{root}'");
+    }
 
     public async Task<LaunchResponse> LaunchAsync(LaunchRequestArguments args)
     {
         var toRun = args.LaunchAttributes!["program"].GetString()!;
         if (args.NoDebug == true)
         {
-            var process = new Process()
+            await Task.Delay(10000);
+            this.debugger = this.debuggerHost.StartProcess("dotnet", toRun);
+
+            this.debugger.OnStandardOut += async (_, args) =>
             {
-                StartInfo = new()
+                await this.client.SendOutput(new()
                 {
-                    FileName = "dotnet",
-                    Arguments = toRun,
-                }
+                    Category = OutputEvent.OutputCategory.Stdout,
+                    Output = args,
+                });
             };
-            process.Start();
+
+            this.debugger.OnBreakpoint += (_, a) =>
+            {
+                this.debugger.Continue();
+            };
         }
         else
         {

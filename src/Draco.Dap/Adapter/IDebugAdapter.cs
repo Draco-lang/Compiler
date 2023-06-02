edcfc206b2ff5cf8b9b25b04a6c4003f009edcaf
LPeter1997 <lenkefi.peti@gmail.com>
2023-06-02 22:52:40 +0200

    Update src/Draco.Dap/Adapter/IDebugAdapter.cs

--- src/Draco.Dap/Adapter/IDebugAdapter.cs
+++ src/Draco.Dap/Adapter/IDebugAdapter.cs
@@ -6,7 +6,7 @@ using Draco.Dap.Model;
 namespace Draco.Dap.Adapter;
 
 /// <summary>
-/// The interface that debg adapters need to implement.
+/// The interface that debug adapters need to implement.
 /// </summary>
 public interface IDebugAdapter
     : IDisposable

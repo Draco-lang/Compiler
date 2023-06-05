43bbdb846bb801a535af42cfd9f9a13aa14667cb
LPeter1997 <lenkefi.peti@gmail.com>
2023-06-05 21:51:48 +0200

    Update debugadapter.ts

--- src/Draco.Extension.VsCode/src/debugadapter.ts
+++ src/Draco.Extension.VsCode/src/debugadapter.ts
@@ -2,6 +2,7 @@ import * as vscode from "vscode";
 import * as fs from "fs/promises";
 import * as path from "path";
 import { DracoDebugAdapterCommandName } from "./settings";
+import { PathLike } from "fs";
 
 export function activateDebugAdapter(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider(
@@ -31,7 +32,7 @@ class DracoDebugConfigurationProvider implements vscode.DebugConfigurationProvid
             request: 'launch',
             preLaunchTask: 'build',
             // TODO: Hardcoded config and framework
-            program: path.join('${workspaceFolder}', 'bin', 'Debug', `${path.parse(f).name}.dll`),
+            program: path.join('${workspaceFolder}', 'bin', 'Debug', 'net7.0', `${path.parse(f).name}.dll`),
             stopAtEntry: false
         }));
     }
@@ -42,10 +43,20 @@ class DracoDebugConfigurationProvider implements vscode.DebugConfigurationProvid
 }
 
 async function addTasksJsonIfNecessary(workspaceRoot: string): Promise<void> {
-
+    // TODO: Extremely naive...
+    // TODO: Logic duplication with settings
+    let vscodePath =  path.join(workspaceRoot, '.vscode');
+    if (!await exists(vscodePath)) {
+        await fs.mkdir(vscodePath);
+    }
+    let tasksJsonPath = path.join(vscodePath, 'tasks.json');
+    if (!await exists(tasksJsonPath)) {
+        let tasksJson = generateTasksJson();
+        await fs.writeFile(tasksJsonPath, JSON.stringify(tasksJson, null, 4));
+    }
 }
 
-function generateTasksJson(projectPath: string): any {
+function generateTasksJson(): any {
     return {
         version: '2.0.0',
         tasks: [
@@ -54,13 +65,28 @@ function generateTasksJson(projectPath: string): any {
                 command: 'dotnet',
                 type: 'process',
                 args: [
-                    'build',
-                    path.join('${workspaceFolder}', projectPath),
+                    'build'
+                    // TODO: Missing project/projects
                 ],
                 problemMatcher: '$msCompile',
             }
         ]
     };
+}
+
+// TODO: Copy-pasta from settings.ts
+/**
+ * Checks if the given path exists and is available for writing.
+ * @param path The path to check.
+ * @returns True, if the path exists and can be written, false otherwise.
+ */
+async function exists(path: PathLike): Promise<boolean> {
+    try {
+        await fs.access(path, fs.constants.W_OK);
+        return true;
+    } catch {
+        return false;
+    }
 }
 
 /**

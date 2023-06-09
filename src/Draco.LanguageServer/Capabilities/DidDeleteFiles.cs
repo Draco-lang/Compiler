00f83b89aa3ef88d932a0bfa3160bbc7f7f2322b
Binto86 <76395771+Binto86@users.noreply.github.com>
2023-06-09 19:03:04 +0200

    Fix LSP forgetting to remove deleted source file (#268)
    * Fixed the bug
    
    * Update TextDocumentSync.cs
    
    * Added did delete files
    
    * Update DidDeleteFiles.cs
    
    * Added FileDelete
    
    * Removed DeleteSyntaxTree
    
    * Update Compilation.cs
    
    * Update src/Draco.Compiler/Api/Compilation.cs
    
    Co-authored-by: LPeter1997 <lenkefi.peti@gmail.com>
    
    * PR comments
    
    ---------
    
    Co-authored-by: LPeter1997 <lenkefi.peti@gmail.com>

---
+++ src/Draco.LanguageServer/Capabilities/DidDeleteFiles.cs
@@ -0,0 +1,51 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Draco.Compiler.Api.Syntax;
+using Draco.Lsp.Model;
+using Draco.Lsp.Server.Workspace;
+
+namespace Draco.LanguageServer;
+
+internal partial class DracoLanguageServer : IDidDeleteFiles
+{
+    public FileOperationRegistrationOptions DidDeleteFileRegistrationOptions => new()
+    {
+        Filters = new FileOperationFilter[]
+        {
+            new()
+            {
+                Pattern = new()
+                {
+                    Glob = this.DocumentSelector[0].Pattern!
+                }
+            }
+        }
+    };
+
+    public async Task DidDeleteFilesAsync(DeleteFilesParams param, CancellationToken cancellationToken)
+    {
+        foreach (var file in param.Files)
+        {
+            await this.DeleteDocument(DocumentUri.From(file.Uri));
+        }
+    }
+
+    private async Task DeleteDocument(DocumentUri documentUri)
+    {
+        var uri = documentUri.ToUri();
+        var oldTree = this.compilation.SyntaxTrees
+            .First(tree => tree.SourceText.Path == uri);
+        this.compilation = this.compilation.UpdateSyntaxTree(oldTree, null);
+        if (this.syntaxTree == oldTree)
+        {
+            this.syntaxTree = SyntaxTree.Create(SyntaxFactory.CompilationUnit());
+            this.semanticModel = this.compilation.GetSemanticModel(this.syntaxTree);
+        }
+        else
+        {
+            this.semanticModel = this.compilation.GetSemanticModel(this.syntaxTree);
+            await this.PublishDiagnosticsAsync(DocumentUri.From(this.syntaxTree.SourceText.Path!));
+        }
+    }
+}

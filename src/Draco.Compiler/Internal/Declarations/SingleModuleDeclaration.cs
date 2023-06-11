0d863a30fafcb315be7e2c1954ad33f6f5175ba7
LPeter1997 <lenkefi.peti@gmail.com>
2023-06-11 13:35:08 +0200

    Update SingleModuleDeclaration.cs

--- src/Draco.Compiler/Internal/Declarations/SingleModuleDeclaration.cs
+++ src/Draco.Compiler/Internal/Declarations/SingleModuleDeclaration.cs
@@ -33,15 +33,22 @@ internal sealed class SingleModuleDeclaration : Declaration
     private ImmutableArray<Declaration> BuildChildren() =>
         this.Syntax.Declarations.Select(BuildChild).OfType<Declaration>().ToImmutableArray();
 
-    private static Declaration? BuildChild(SyntaxNode node) => node switch
+    private static Declaration? BuildChild(SyntaxNode node)
     {
+        switch (node)
+        {
         // NOTE: We ignore import declarations in the declaration tree, unlike Roslyn
         // We handle import declarations during constructing the binders
         // Since we allow for imports in local scopes too, this is the most sensible choice
-        ImportDeclarationSyntax => null,
-        VariableDeclarationSyntax var => new GlobalDeclaration(var),
-        FunctionDeclarationSyntax func => new FunctionDeclaration(func),
-        UnexpectedDeclarationSyntax => null,
-        _ => throw new ArgumentOutOfRangeException(nameof(node)),
-    };
+        case ImportDeclarationSyntax:
+        case UnexpectedDeclarationSyntax:
+            return null;
+        case VariableDeclarationSyntax var:
+            return new GlobalDeclaration(var);
+        case FunctionDeclarationSyntax func:
+            return new FunctionDeclaration(func);
+        default:
+            throw new ArgumentOutOfRangeException(nameof(node)),
+        }
+    }
 }

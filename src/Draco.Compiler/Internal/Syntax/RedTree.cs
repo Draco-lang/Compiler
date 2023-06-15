5a4903fb6fdd9aa06b9533944ed5331b7dbea99f
jakubbinter <kuba.binter@gmail.com>
2023-06-15 18:19:10 +0200

    Move abstract declaration to xml

--- src/Draco.Compiler/Internal/Syntax/RedTree.cs
+++
@@ -1,9 +0,0 @@
-namespace Draco.Compiler.Api.Syntax;
-
-public abstract partial class ContainerSyntax : DeclarationSyntax
-{
-    /// <summary>
-    /// All declaration syntaxes within the container.
-    /// </summary>
-    public abstract SyntaxList<DeclarationSyntax> Declarations { get; }
-}

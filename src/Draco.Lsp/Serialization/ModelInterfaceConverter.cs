6de0589c9822d6a388b0b242d460fdf19b8331e2
LPeter1997 <lenkefi.peti@gmail.com>
2023-07-17 09:23:54 +0200

    Usings

--- src/Draco.Lsp/Serialization/ModelInterfaceConverter.cs
+++ src/Draco.Lsp/Serialization/ModelInterfaceConverter.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Diagnostics;
 using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
--- src/Draco.Lsp/Serialization/OneOfConverter.cs
+++ src/Draco.Lsp/Serialization/OneOfConverter.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text.Json;

78e68753e229c38aed6a0f2525650e509921061e
LPeter1997 <lenkefi.peti@gmail.com>
2023-08-07 14:58:51 +0200

    M000AR

--- src/Draco.Compiler.Tests/Semantics/DocumentationCommentsTests.cs
+++ src/Draco.Compiler.Tests/Semantics/DocumentationCommentsTests.cs
@@ -31,7 +31,7 @@ public sealed class DocumentationCommentsTests : SemanticTestsBase
         var funcDecl = tree.FindInChildren<FunctionDeclarationSyntax>(0);
 
         // Act
-        var compilation = Compilation.Create(ImmutableArray.Create(tree));
+        var compilation = CreateCompilation(tree);
         var semanticModel = compilation.GetSemanticModel(tree);
 
         var funcSym = GetInternalSymbol<FunctionSymbol>(semanticModel.GetDeclaredSymbol(funcDecl));
@@ -63,7 +63,7 @@ public sealed class DocumentationCommentsTests : SemanticTestsBase
         var xDecl = tree.FindInChildren<VariableDeclarationSyntax>(0);
 
         // Act
-        var compilation = Compilation.Create(ImmutableArray.Create(tree));
+        var compilation = CreateCompilation(tree);
         var semanticModel = compilation.GetSemanticModel(tree);
 
         var xSym = GetInternalSymbol<GlobalSymbol>(semanticModel.GetDeclaredSymbol(xDecl));
@@ -99,7 +99,7 @@ public sealed class DocumentationCommentsTests : SemanticTestsBase
         var labelDecl = tree.FindInChildren<LabelDeclarationSyntax>(0);
 
         // Act
-        var compilation = Compilation.Create(ImmutableArray.Create(tree));
+        var compilation = CreateCompilation(tree);
         var semanticModel = compilation.GetSemanticModel(tree);
 
         var labelSym = GetInternalSymbol<LabelSymbol>(semanticModel.GetDeclaredSymbol(labelDecl));

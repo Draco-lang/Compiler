975422fc6f50ea2040563254faa89a99af479c0e
LPeter1997 <lenkefi.peti@gmail.com>
2023-08-08 20:02:06 +0200

    Update SourceFunctionSymbol.cs

--- src/Draco.Compiler/Internal/Symbols/Source/SourceFunctionSymbol.cs
+++ src/Draco.Compiler/Internal/Symbols/Source/SourceFunctionSymbol.cs
@@ -16,16 +16,13 @@ namespace Draco.Compiler.Internal.Symbols.Source;
 /// </summary>
 internal sealed class SourceFunctionSymbol : FunctionSymbol, ISourceSymbol
 {
-    public override ImmutableArray<TypeParameterSymbol> GenericParameters =>
-        InterlockedUtils.InitializeDefault(ref this.genericParameters, () => this.BindGenericParameters(this.DeclaringCompilation!.GlobalDiagnosticBag));
+    public override ImmutableArray<TypeParameterSymbol> GenericParameters => this.BindGenericParametersIfNeeded(this.DeclaringCompilation!);
     private ImmutableArray<TypeParameterSymbol> genericParameters;
 
-    public override ImmutableArray<ParameterSymbol> Parameters =>
-        InterlockedUtils.InitializeDefault(ref this.parameters, () => this.BindParameters(this.DeclaringCompilation!.GlobalDiagnosticBag));
+    public override ImmutableArray<ParameterSymbol> Parameters => this.BindParametersIfNeeded(this.DeclaringCompilation!);
     private ImmutableArray<ParameterSymbol> parameters;
 
-    public override TypeSymbol ReturnType =>
-        InterlockedUtils.InitializeNull(ref this.returnType, () => this.BindReturnType(this.DeclaringCompilation!));
+    public override TypeSymbol ReturnType => this.BindReturnTypeIfNeeded(this.DeclaringCompilation!);
     private TypeSymbol? returnType;
 
     public override Symbol ContainingSymbol { get; }
@@ -34,8 +31,7 @@ internal sealed class SourceFunctionSymbol : FunctionSymbol, ISourceSymbol
 
     public override FunctionDeclarationSyntax DeclaringSyntax { get; }
 
-    public BoundStatement Body =>
-        InterlockedUtils.InitializeNull(ref this.body, () => this.BindBody(this.DeclaringCompilation!));
+    public BoundStatement Body => this.BindBodyIfNeeded(this.DeclaringCompilation!);
     private BoundStatement? body;
 
     public override string Documentation => this.DeclaringSyntax.Documentation;
@@ -55,12 +51,12 @@ internal sealed class SourceFunctionSymbol : FunctionSymbol, ISourceSymbol
 
     public void Bind(IBinderProvider binderProvider)
     {
-        this.BindGenericParameters(binderProvider.DiagnosticBag);
-        this.BindParameters(binderProvider.DiagnosticBag);
+        this.BindGenericParametersIfNeeded(binderProvider);
+        this.BindParametersIfNeeded(binderProvider);
         // Force binding of parameters, as the type is lazy too
         foreach (var param in this.Parameters.Cast<SourceParameterSymbol>()) param.Bind(binderProvider);
-        this.BindReturnType(binderProvider);
-        var body = InterlockedUtils.InitializeNull(ref this.body, () => this.BindBody(binderProvider));
+        this.BindReturnTypeIfNeeded(binderProvider);
+        var body = this.BindBodyIfNeeded(binderProvider);
 
         // Check, if this function collides with any other overloads that are visible from here
         this.CheckForSameParameterOverloads(binderProvider);
@@ -71,7 +67,10 @@ internal sealed class SourceFunctionSymbol : FunctionSymbol, ISourceSymbol
         ValAssignment.Analyze(this, binderProvider.DiagnosticBag);
     }
 
-    private ImmutableArray<TypeParameterSymbol> BindGenericParameters(DiagnosticBag diagnostics)
+    private ImmutableArray<TypeParameterSymbol> BindGenericParametersIfNeeded(IBinderProvider binderProvider) =>
+        InterlockedUtils.InitializeDefault(ref this.genericParameters, () => this.BindGenericParameters(binderProvider));
+
+    private ImmutableArray<TypeParameterSymbol> BindGenericParameters(IBinderProvider binderProvider)
     {
         // Simplest case if the function is not generic
         if (this.DeclaringSyntax.Generics is null) return ImmutableArray<TypeParameterSymbol>.Empty;
@@ -87,7 +86,7 @@ internal sealed class SourceFunctionSymbol : FunctionSymbol, ISourceSymbol
             if (usedBefore)
             {
                 // NOTE: We only report later duplicates, no need to report the first instance
-                diagnostics.Add(Diagnostic.Create(
+                binderProvider.DiagnosticBag.Add(Diagnostic.Create(
                     template: SymbolResolutionErrors.IllegalShadowing,
                     location: genericParamSyntax.Location,
                     formatArgs: paramName));
@@ -131,7 +130,10 @@ internal sealed class SourceFunctionSymbol : FunctionSymbol, ISourceSymbol
         }
     }
 
-    private ImmutableArray<ParameterSymbol> BindParameters(DiagnosticBag diagnostics)
+    private ImmutableArray<ParameterSymbol> BindParametersIfNeeded(IBinderProvider binderProvider) =>
+        InterlockedUtils.InitializeDefault(ref this.parameters, () => this.BindParameters(binderProvider));
+
+    private ImmutableArray<ParameterSymbol> BindParameters(IBinderProvider binderProvider)
     {
         var parameterSyntaxes = this.DeclaringSyntax.ParameterList.Values.ToList();
         var parameters = ImmutableArray.CreateBuilder<ParameterSymbol>();
@@ -145,7 +147,7 @@ internal sealed class SourceFunctionSymbol : FunctionSymbol, ISourceSymbol
             if (usedBefore)
             {
                 // NOTE: We only report later duplicates, no need to report the first instance
-                diagnostics.Add(Diagnostic.Create(
+                binderProvider.DiagnosticBag.Add(Diagnostic.Create(
                     template: SymbolResolutionErrors.IllegalShadowing,
                     location: parameterSyntax.Location,
                     formatArgs: parameterName));
@@ -153,7 +155,7 @@ internal sealed class SourceFunctionSymbol : FunctionSymbol, ISourceSymbol
 
             if (parameterSyntax.Variadic is not null && i != parameterSyntaxes.Count - 1)
             {
-                diagnostics.Add(Diagnostic.Create(
+                binderProvider.DiagnosticBag.Add(Diagnostic.Create(
                     template: SymbolResolutionErrors.VariadicParameterNotLast,
                     location: parameterSyntax.Location,
                     formatArgs: parameterName));
@@ -166,6 +168,9 @@ internal sealed class SourceFunctionSymbol : FunctionSymbol, ISourceSymbol
         return parameters.ToImmutable();
     }
 
+    private TypeSymbol BindReturnTypeIfNeeded(IBinderProvider binderProvider) =>
+        InterlockedUtils.InitializeNull(ref this.returnType, () => this.BindReturnType(binderProvider));
+
     private TypeSymbol BindReturnType(IBinderProvider binderProvider)
     {
         // If the return type is unspecified, it's assumed to be unit
@@ -175,6 +180,9 @@ internal sealed class SourceFunctionSymbol : FunctionSymbol, ISourceSymbol
         var binder = binderProvider.GetBinder(this.DeclaringSyntax);
         return binder.BindTypeToTypeSymbol(this.DeclaringSyntax.ReturnType.Type, binderProvider.DiagnosticBag);
     }
+
+    private BoundStatement BindBodyIfNeeded(IBinderProvider binderProvider) =>
+        InterlockedUtils.InitializeNull(ref this.body, () => this.BindBody(binderProvider));
 
     private BoundStatement BindBody(IBinderProvider binderProvider)
     {

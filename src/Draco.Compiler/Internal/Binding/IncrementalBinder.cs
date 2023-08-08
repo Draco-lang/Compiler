dafb89d8cd012c2d87b81d7b44b19f50c7c0371a
LPeter1997 <lenkefi.peti@gmail.com>
2023-08-08 19:26:47 +0200

    Fix

--- src/Draco.Compiler/Internal/Binding/IncrementalBinder.cs
+++ src/Draco.Compiler/Internal/Binding/IncrementalBinder.cs
@@ -102,7 +102,7 @@ public sealed partial class SemanticModel
             if (untyped.Syntax is null) return binder();
 
             return (TBoundNode)this.semanticModel.boundNodeMap.GetOrAdd(
-                key: (untyped.Syntax, untyped.GetType()),
+                key: (untyped.Syntax, typeof(TBoundNode)),
                 valueFactory: _ =>
                 {
                     var node = binder();

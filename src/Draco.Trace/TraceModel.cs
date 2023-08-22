99e2a5b788b11fd9e74bc94f3e525518563d0d4a
LPeter1997 <lenkefi.peti@gmail.com>
2023-08-22 17:49:20 +0200

    Update TraceModel.cs

--- src/Draco.Trace/TraceModel.cs
+++ src/Draco.Trace/TraceModel.cs
@@ -7,18 +7,23 @@ using System.Threading.Tasks;
 
 namespace Draco.Trace;
 
-internal sealed record class TraceModel(IReadOnlyList<ThreadTraceModel> Threads);
+internal sealed record class TraceModel(IReadOnlyList<ThreadTraceModel> Threads)
+{
+    public DateTime StartTime => this.Threads.Min(t => t.Messages[0].TimeStamp);
+    public DateTime EndTime => this.Threads.Min(t => t.Messages[^1].TimeStamp);
+    public TimeSpan TimeSpan => this.EndTime - this.StartTime;
+}
 
 internal sealed class ThreadTraceModel
 {
     public Thread Thread { get; }
     public IReadOnlyList<MessageTraceModel> Messages { get; }
 
-    private readonly TraceModel traceModel;
+    internal TraceModel TraceModel { get; }
 
     public ThreadTraceModel(TraceModel traceModel, Thread thread, IReadOnlyList<MessageTraceModel> messages)
     {
-        this.traceModel = traceModel;
+        this.TraceModel = traceModel;
         this.Thread = thread;
         this.Messages = messages;
     }
@@ -26,18 +31,19 @@ internal sealed class ThreadTraceModel
 
 internal sealed class MessageTraceModel
 {
-    public bool IsBeginEvent => this.message.Kind == TraceKind.Begin;
-    public bool IsEndEvent => this.message.Kind == TraceKind.End;
-    public bool IsInstantEvent => this.message.Kind == TraceKind.Event;
+    public bool IsBeginEvent => this.UnderlyingMessage.Kind == TraceKind.Begin;
+    public bool IsEndEvent => this.UnderlyingMessage.Kind == TraceKind.End;
+    public bool IsInstantEvent => this.UnderlyingMessage.Kind == TraceKind.Event;
 
-    public string Message => this.message.Message;
+    public DateTime TimeStamp => this.UnderlyingMessage.TimeStamp;
+    public string Message => this.UnderlyingMessage.Message;
 
-    private readonly ThreadTraceModel threadTraceModel;
-    private readonly TraceMessage message;
+    internal ThreadTraceModel ThreadTraceModel { get; }
+    internal TraceMessage UnderlyingMessage { get; }
 
     public MessageTraceModel(ThreadTraceModel threadTraceModel, TraceMessage message)
     {
-        this.threadTraceModel = threadTraceModel;
-        this.message = message;
+        this.ThreadTraceModel = threadTraceModel;
+        this.UnderlyingMessage = message;
     }
 }

4a0592430fc83ebe12d967bccdffce264686b17c
LPeter1997 <lenkefi.peti@gmail.com>
2023-09-20 23:02:37 +0200

    Update TimelineGraph.tsx

--- src/Draco.Trace.Visualizer/src/TimelineGraph.tsx
+++ src/Draco.Trace.Visualizer/src/TimelineGraph.tsx
@@ -1,18 +1,18 @@
 import * as d3 from "d3";
 import * as d3scale from "d3-scale";
-import React, { Component, ReactNode, useEffect, useId, useRef, useState } from "react";
+import React from "react";
 
 const TimelineGraph = () => {
-    const domRef = useRef(null);
+    const domRef = React.useRef(null);
 
-    const [data, setData] = useState([
+    const [data, setData] = React.useState([
         { x: 10, y: 10, w: 50, h: 10 },
         { x: 10, y: 30, w: 70, h: 10 },
         { x: 10, y: 50, w: 20, h: 10 },
         { x: 10, y: 70, w: 30, h: 10 },
     ]);
 
-    useEffect(() => {
+    React.useEffect(() => {
         const svg = d3.select(domRef.current);
 
         svg

ef74155729ef1f4818dc70f9e80bc9ba3fa29269
LPeter1997 <lenkefi.peti@gmail.com>
2023-10-13 12:01:48 +0200

    Cleanup

--- src/Draco.Compiler/Internal/Symbols/Synthetized/IntrinsicFunctionSymbol.cs
+++ src/Draco.Compiler/Internal/Symbols/Synthetized/IntrinsicFunctionSymbol.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using Draco.Compiler.Api.Syntax;
 
 namespace Draco.Compiler.Internal.Symbols.Synthetized;
 

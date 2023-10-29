522bd6b17daf3d37e271bf117f818e482ade90dd
LPeter1997 <lenkefi.peti@gmail.com>
2023-10-29 11:55:31 +0100

    Cleanup

--- src/Draco.Compiler/Internal/Syntax/Formatting/Formatter.cs
+++ src/Draco.Compiler/Internal/Syntax/Formatting/Formatter.cs
@@ -2,9 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using Draco.Compiler.Api.Syntax;
 
 namespace Draco.Compiler.Internal.Syntax.Formatting;

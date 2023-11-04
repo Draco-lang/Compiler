3941742241d3dd784e0baa395f69b203e0d020d3
LPeter1997 <lenkefi.peti@gmail.com>
2023-11-04 13:30:07 +0100

    Update BindingTask.cs

--- src/Draco.Compiler/Internal/Binding/Tasks/BindingTask.cs
+++ src/Draco.Compiler/Internal/Binding/Tasks/BindingTask.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Runtime.CompilerServices;
 using Draco.Compiler.Internal.Solver;
+using Draco.Compiler.Internal.Solver.Tasks;
 
 namespace Draco.Compiler.Internal.Binding.Tasks;
 
@@ -15,7 +16,7 @@ internal static class BindingTask
         return task;
     }
 
-    public static async BindingTask<ImmutableArray<T>> WhenAll<T>(IEnumerable<BindingTask<T>> tasks)
+    public static async SolverTask<ImmutableArray<T>> WhenAll<T>(IEnumerable<BindingTask<T>> tasks)
     {
         var result = ImmutableArray.CreateBuilder<T>();
         foreach (var task in tasks) result.Add(await task);

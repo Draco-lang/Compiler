7dbd86318b4a4e18adadc60713f108582bc87560
LPeter1997 <lenkefi.peti@gmail.com>
2023-12-03 17:35:31 +0100

    Update DefaultConstructorSymbol.cs

--- src/Draco.Compiler/Internal/Symbols/Synthetized/DefaultConstructorSymbol.cs
+++ src/Draco.Compiler/Internal/Symbols/Synthetized/DefaultConstructorSymbol.cs
@@ -4,25 +4,23 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Draco.Compiler.Internal.BoundTree;
 
 namespace Draco.Compiler.Internal.Symbols.Synthetized;
 
 /// <summary>
 /// A default constructor for in-source types.
 /// </summary>
-internal sealed class DefaultConstructorSymbol : IrFunctionSymbol
+internal sealed class DefaultConstructorSymbol : SynthetizedFunctionSymbol
 {
     public override Symbol ContainingSymbol { get; }
 
-    public override CodegenDelegate Codegen => (codegen, target, args) =>
-    {
-        // TODO
-        throw new NotImplementedException();
-    };
-
+    public override string Name => ".ctor";
     public override ImmutableArray<ParameterSymbol> Parameters => ImmutableArray<ParameterSymbol>.Empty;
     public override TypeSymbol ReturnType => IntrinsicSymbols.Unit;
     public override bool IsStatic => false;
+
+    public override BoundStatement Body => throw new NotImplementedException();
 
     public DefaultConstructorSymbol(Symbol containingSymbol)
     {

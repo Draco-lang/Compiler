295e607a60e0059a25f8144f8e236724f03929a9
LPeter1997 <lenkefi.peti@gmail.com>
2023-12-03 13:17:47 +0100

    Create IClass.cs

---
+++ src/Draco.Compiler/Internal/OptimizingIr/Model/IClass.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Draco.Compiler.Api.Semantics;
+
+namespace Draco.Compiler.Internal.OptimizingIr.Model;
+
+/// <summary>
+/// Read-only interface of a class.
+/// </summary>
+internal interface IClass
+{
+    /// <summary>
+    /// The symbol that corresponds to this class.
+    /// </summary>
+    public TypeSymbol Symbol { get; }
+
+    /// <summary>
+    /// The name of this class.
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// The module this class is defined in.
+    /// </summary>
+    public IModule DeclaringModule { get; }
+
+    /// <summary>
+    /// The assembly this class is defined in.
+    /// </summary>
+    public IAssembly Assembly { get; }
+
+    /// <summary>
+    /// The generic parameters on this class.
+    /// </summary>
+    public IReadOnlyList<TypeParameterSymbol> Generics { get; }
+
+    // TODO: Base class
+    // TODO: Interfaces? (we might wanna keep them external)
+    // TODO: Nested classes
+
+    /// <summary>
+    /// The constructors defined on this class.
+    /// </summary>
+    public IReadOnlyDictionary<FunctionSymbol, IProcedure> Constructors { get; }
+
+    // TODO: fields
+    // TODO: properties
+    // TODO: methods
+}

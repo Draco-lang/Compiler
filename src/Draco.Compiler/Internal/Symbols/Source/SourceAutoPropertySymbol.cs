76d4e619d33920692809672230ff2706ffff123c
LPeter1997 <lenkefi.peti@gmail.com>
2023-12-21 10:18:01 +0100

    Create SourceAutoPropertySymbol.cs

---
+++ src/Draco.Compiler/Internal/Symbols/Source/SourceAutoPropertySymbol.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Draco.Compiler.Api.Syntax;
+using Draco.Compiler.Internal.Binding;
+
+namespace Draco.Compiler.Internal.Symbols.Source;
+
+/// <summary>
+/// An auto-prop coming from source.
+/// </summary>
+internal sealed class SourceAutoPropertySymbol : PropertySymbol, ISourceSymbol
+{
+    public override TypeSymbol ContainingSymbol { get; }
+
+    public override TypeSymbol Type => throw new NotImplementedException();
+
+    public override FunctionSymbol? Getter => throw new NotImplementedException();
+    public override FunctionSymbol? Setter => throw new NotImplementedException();
+
+    /// <summary>
+    /// The backing field of this auto-prop.
+    /// </summary>
+    public FieldSymbol BackingField => throw new NotImplementedException();
+
+    public override bool IsIndexer => false;
+    public override bool IsStatic => false;
+
+    // TODO: Not necessarily this type, only for primary constructors
+    public override PrimaryConstructorParameterSyntax DeclaringSyntax { get; }
+
+    public SourceAutoPropertySymbol(TypeSymbol containingSymbol, PrimaryConstructorParameterSyntax declaringSyntax)
+    {
+        this.ContainingSymbol = containingSymbol;
+        this.DeclaringSyntax = declaringSyntax;
+    }
+
+    public void Bind(IBinderProvider binderProvider) => throw new NotImplementedException();
+}

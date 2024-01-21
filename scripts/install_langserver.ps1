e0f7efc73120de1207e10f13a21ad8c6db9061bd
Kuinox <github@kuinox.io>
2024-01-21 17:32:59 +0100

    Fixed VS Code Extension install script, enabled stop on error in installation scripts. (#366)

--- scripts/install_langserver.ps1
+++ scripts/install_langserver.ps1
@@ -1,3 +1,4 @@
+$ErrorActionPreference = "Stop"
 dotnet pack ../src/Draco.LanguageServer --output .
 if ((dotnet tool list --global) -match "Draco.LanguageServer") {
     dotnet tool uninstall --global Draco.LanguageServer
--- scripts/install_vscext.ps1
+++ scripts/install_vscext.ps1
@@ -1,5 +1,9 @@
+$ErrorActionPreference = "Stop"
+# get scripts directory and push location
+Push-Location $PSScriptRoot
 cd ../src/Draco.Extension.VsCode
-echo 'y' | vsce package
+npm i
+vsce package --skip-license
 $vsixFile = Get-ChildItem -Filter "*.vsix" | Select-Object -First 1
 code --install-extension $vsixFile --force
-cd ../../scripts
+Pop-Location

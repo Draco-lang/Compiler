57505d30f87c79175da5c6fe6a9d6d3a518a4a3b
Kuinox <github@kuinox.io>
2024-01-21 19:58:00 +0100

    Same fix, but for the language server.

--- scripts/install_langserver.ps1
+++ scripts/install_langserver.ps1
@@ -1,6 +1,8 @@
 $ErrorActionPreference = "Stop"
+Push-Location $PSScriptRoot
 dotnet pack ../src/Draco.LanguageServer --output .
 if ((dotnet tool list --global) -match "Draco.LanguageServer") {
     dotnet tool uninstall --global Draco.LanguageServer
 }
 dotnet tool install --global --add-source . Draco.LanguageServer
+Pop-Location

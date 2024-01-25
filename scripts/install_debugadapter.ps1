afde6cbe40224427902d4cc7e6fdb1b8b7f740b0
Kuinox <github@kuinox.io>
2024-01-25 17:33:06 +0100

    Script fix for install debugger. (#379)

--- scripts/install_debugadapter.ps1
+++ scripts/install_debugadapter.ps1
@@ -1,6 +1,8 @@
 $ErrorActionPreference = "Stop"
+Push-Location $PSScriptRoot
 dotnet pack ../src/Draco.DebugAdapter --output .
 if ((dotnet tool list --global) -match "Draco.DebugAdapter") {
     dotnet tool uninstall --global Draco.DebugAdapter
 }
 dotnet tool install --global --add-source . Draco.DebugAdapter
+Pop-Location

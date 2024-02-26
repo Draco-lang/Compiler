33fac1667957bde8650e88d8377335a7155217b8
LPeter1997 <lenkefi.peti@gmail.com>
2024-02-26 17:55:46 +0100

    Update ConstraintStore.cs

--- src/Draco.Chr/Constraints/ConstraintStore.cs
+++ src/Draco.Chr/Constraints/ConstraintStore.cs
@@ -16,7 +16,7 @@ public sealed class ConstraintStore : ICollection<IConstraint>
     public void Add(IConstraint item) => this.constraints.Add(item);
     public void AddRange(IEnumerable<IConstraint> items) => this.constraints.UnionWith(items);
     public bool Remove(IConstraint item) => this.constraints.Remove(item);
-    public void Clear() => throw new System.NotImplementedException();
+    public void Clear() => this.constraints.Clear();
     public bool Contains(IConstraint item) => this.constraints.Contains(item);
     public void CopyTo(IConstraint[] array, int arrayIndex) => this.constraints.CopyTo(array, arrayIndex);
     public IEnumerator<IConstraint> GetEnumerator() => this.constraints.GetEnumerator();

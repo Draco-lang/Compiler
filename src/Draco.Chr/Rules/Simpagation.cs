827fb7530365155c6ea189162eb6602993fd2794
LPeter1997 <lenkefi.peti@gmail.com>
2024-02-29 10:09:27 +0100

    Added simpagation factories

--- src/Draco.Chr/Rules/Simpagation.cs
+++ src/Draco.Chr/Rules/Simpagation.cs
@@ -11,37 +11,36 @@ namespace Draco.Chr.Rules;
 /// </summary>
 public sealed class Simpagation : Rule
 {
-    private int HeadRemoveCount => this.HeadCount - this.headKeepCount;
+    public int HeadKeepCount { get; }
+    public int HeadRemoveCount => this.HeadCount - this.HeadKeepCount;
 
     private SimpagationGuardDelegate guard = (_, _) => true;
     private SimpagationBodyDelegate body = (_, _, _) => { };
 
-    private readonly int headKeepCount;
-
     public Simpagation(int headKeepCount, int headRemoveCount)
         : base(headKeepCount + headRemoveCount)
     {
-        this.headKeepCount = headKeepCount;
+        this.HeadKeepCount = headKeepCount;
         this.CheckHeadCounts(nameof(headKeepCount), nameof(headRemoveCount));
     }
 
     public Simpagation(int headKeepCount, ImmutableArray<Type> headTypes)
         : base(headTypes)
     {
-        this.headKeepCount = headKeepCount;
+        this.HeadKeepCount = headKeepCount;
         this.CheckHeadCounts(nameof(headKeepCount), nameof(headTypes));
     }
 
     public Simpagation(int headKeepCount, ImmutableArray<Head> headDefinitions)
         : base(headDefinitions)
     {
-        this.headKeepCount = headKeepCount;
+        this.HeadKeepCount = headKeepCount;
         this.CheckHeadCounts(nameof(headKeepCount), nameof(headDefinitions));
     }
 
     private void CheckHeadCounts(string arg1, string arg2)
     {
-        if (this.headKeepCount < 1) throw new ArgumentOutOfRangeException(arg1, "at least one head must be kept");
+        if (this.HeadKeepCount < 1) throw new ArgumentOutOfRangeException(arg1, "at least one head must be kept");
         if (this.HeadRemoveCount < 1) throw new ArgumentOutOfRangeException(arg2, "at least one head must be removed");
     }
 
@@ -49,16 +48,16 @@ public sealed class Simpagation : Rule
     {
         if (constraints.Count != this.HeadCount) return false;
 
-        var headKeep = constraints.Take(this.headKeepCount).ToList();
-        var headRemove = constraints.Skip(this.headKeepCount).ToList();
+        var headKeep = constraints.Take(this.HeadKeepCount).ToList();
+        var headRemove = constraints.Skip(this.HeadKeepCount).ToList();
 
         return this.guard(headKeep, headRemove);
     }
 
     public override IEnumerable<IConstraint> Apply(ImmutableArray<IConstraint> constraints)
     {
-        var headKeep = constraints.Take(this.headKeepCount).ToImmutableArray();
-        var headRemove = constraints.Skip(this.headKeepCount).ToImmutableArray();
+        var headKeep = constraints.Take(this.HeadKeepCount).ToImmutableArray();
+        var headRemove = constraints.Skip(this.HeadKeepCount).ToImmutableArray();
 
         var store = new ConstraintStore();
         this.body(headKeep, headRemove, store);

af3a6c8d4c7042b4c0a03577341b4d4ef5fb590a
LPeter1997 <lenkefi.peti@gmail.com>
2024-02-29 17:22:30 +0100

    Update RuleFactory.cs

--- src/Draco.Chr/Rules/RuleFactory.cs
+++ src/Draco.Chr/Rules/RuleFactory.cs
@@ -84,12 +84,172 @@ public static partial class RuleFactory
     public static Rule Named(this Rule rule, string name) => rule.WithName(name);
 
     // Guards
-    // TODO
+    public static Rule Guard(this Rule rule, Delegate guard)
+    {
+        if (rule is Propagation p)
+        {
+            p.WithGuard(CreateGuardDelegate(rule, guard));
+        }
+        else if (rule is Simplification s)
+        {
+            s.WithGuard(CreateGuardDelegate(rule, guard));
+        }
+        else if (rule is Simpagation sp)
+        {
+            sp.WithGuard(CreateSimpagationGuardDelegate(rule, guard));
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException(nameof(rule), "unknown rule type");
+        }
 
-    // Action
-    // TODO
+        return rule;
+    }
+
+    // Body
+    public static Rule Body(this Rule rule, Delegate body)
+    {
+        if (rule is Propagation p)
+        {
+            p.WithBody(CreateBodyDelegate(rule, body));
+        }
+        else if (rule is Simplification s)
+        {
+            s.WithBody(CreateBodyDelegate(rule, body));
+        }
+        else if (rule is Simpagation sp)
+        {
+            sp.WithBody(CreateSimpagationBodyDelegate(rule, body));
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException(nameof(rule), "unknown rule type");
+        }
+
+        return rule;
+    }
 
     // Internal
+
+    private static Rule.GuardDelegate CreateGuardDelegate(Rule rule, Delegate guard)
+    {
+        var parameters = guard.Method.GetParameters();
+        if (parameters.Length != rule.HeadCount)
+        {
+            throw new ArgumentException("guard must have the same number of parameters as the rule has heads");
+        }
+
+        if (guard.Method.ReturnType != typeof(bool))
+        {
+            throw new ArgumentException("guard must return a boolean");
+        }
+
+        var argExtractors = parameters
+            .Select(p => GetArgumentExtractor(p.ParameterType))
+            .ToList();
+
+        return heads =>
+        {
+            var args = heads
+                .Zip(argExtractors, (head, extractor) => extractor(head))
+                .ToArray();
+            return (bool)guard.Method.Invoke(guard.Target, args)!;
+        };
+    }
+
+    private static Rule.SimpagationGuardDelegate CreateSimpagationGuardDelegate(Rule rule, Delegate guard)
+    {
+        var parameters = guard.Method.GetParameters();
+        if (parameters.Length != rule.HeadCount)
+        {
+            throw new ArgumentException("guard must have the same number of parameters as the rule has heads");
+        }
+
+        if (guard.Method.ReturnType != typeof(bool))
+        {
+            throw new ArgumentException("guard must return a boolean");
+        }
+
+        var argExtractors = parameters
+            .Select(p => GetArgumentExtractor(p.ParameterType))
+            .ToList();
+
+        return (headsKeep, headsRemove) =>
+        {
+            var args = headsKeep
+                .Concat(headsRemove)
+                .Zip(argExtractors, (head, extractor) => extractor(head))
+                .ToArray();
+            return (bool)guard.Method.Invoke(guard.Target, args)!;
+        };
+    }
+
+    private static Rule.BodyDelegate CreateBodyDelegate(Rule rule, Delegate body)
+    {
+        var parameters = body.Method.GetParameters();
+        if (parameters.Length != rule.HeadCount + 1)
+        {
+            throw new ArgumentException("body must have the same number of parameters as the rule has heads plus one");
+        }
+
+        if (!parameters[0].ParameterType.IsAssignableTo(typeof(ConstraintStore)))
+        {
+            throw new ArgumentException("body must have the store as the first parameter");
+        }
+
+        if (body.Method.ReturnType != typeof(void))
+        {
+            throw new ArgumentException("body must return void");
+        }
+
+        var argExtractors = parameters
+            .Skip(1)
+            .Select(p => GetArgumentExtractor(p.ParameterType))
+            .ToList();
+
+        return (heads, store) =>
+        {
+            var args = heads
+                .Zip(argExtractors, (head, extractor) => extractor(head))
+                .Prepend(store)
+                .ToArray();
+            body.Method.Invoke(body.Target, args);
+        };
+    }
+
+    private static Rule.SimpagationBodyDelegate CreateSimpagationBodyDelegate(Rule rule, Delegate body)
+    {
+        var parameters = body.Method.GetParameters();
+        if (parameters.Length != rule.HeadCount + 2)
+        {
+            throw new ArgumentException("body must have the same number of parameters as the rule has heads plus two");
+        }
+
+        if (!parameters[0].ParameterType.IsAssignableTo(typeof(ConstraintStore)))
+        {
+            throw new ArgumentException("body must have the store as the first parameter");
+        }
+
+        if (body.Method.ReturnType != typeof(void))
+        {
+            throw new ArgumentException("body must return void");
+        }
+
+        var argExtractors = parameters
+            .Skip(1)
+            .Select(p => GetArgumentExtractor(p.ParameterType))
+            .ToList();
+
+        return (headsKeep, headsRemove, store) =>
+        {
+            var args = headsKeep
+                .Concat(headsRemove)
+                .Zip(argExtractors, (head, extractor) => extractor(head))
+                .Prepend(store)
+                .ToArray();
+            body.Method.Invoke(body.Target, args);
+        };
+    }
 
     private static IEnumerable<Type>? ExtractTypeList(object[] args) =>
         args.All(arg => arg is Type t && t.IsAssignableTo(typeof(Type)))
@@ -103,4 +263,10 @@ public static partial class RuleFactory
         Var var => Head.Any().Bind(var),
         _ => Head.OfValue(arg),
     };
+
+    private static Func<object, object> GetArgumentExtractor(Type type)
+    {
+        if (type.IsAssignableTo(typeof(IConstraint))) return x => x;
+        return x => ((IConstraint)x).Value;
+    }
 }

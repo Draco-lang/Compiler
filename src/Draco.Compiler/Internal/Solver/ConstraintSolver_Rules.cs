cf6cf5bc0d15edf801cd2e340b3b219dcfa1ed6b
LPeter1997 <lenkefi.peti@gmail.com>
2024-07-18 21:15:22 +0200

    Update ConstraintSolver_Rules.cs

--- src/Draco.Compiler/Internal/Solver/ConstraintSolver_Rules.cs
+++ src/Draco.Compiler/Internal/Solver/ConstraintSolver_Rules.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Immutable;
-using System.Diagnostics;
-using System.Linq;
-using Draco.Compiler.Internal.Binding;
+using System.Collections.Generic;
+using Draco.Chr.Rules;
 using Draco.Compiler.Internal.Diagnostics;
-using Draco.Compiler.Internal.Solver.OverloadResolution;
-using Draco.Compiler.Internal.Symbols;
-using Draco.Compiler.Internal.Symbols.Error;
-using Draco.Compiler.Internal.Symbols.Synthetized;
 
 namespace Draco.Compiler.Internal.Solver;
 
 internal sealed partial class ConstraintSolver
 {
-    // TODO
+    private static IEnumerable<Rule> ConstructRules(DiagnosticBag diagnostics) => [
+    ];
 }

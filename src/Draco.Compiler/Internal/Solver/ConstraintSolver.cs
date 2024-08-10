b53cea3ebe07dd5103235901e61251eff419eadb
LPeter1997 <lenkefi.peti@gmail.com>
2024-08-10 11:47:02 +0200

    Update ConstraintSolver.cs

--- src/Draco.Compiler/Internal/Solver/ConstraintSolver.cs
+++ src/Draco.Compiler/Internal/Solver/ConstraintSolver.cs
@@ -152,17 +152,6 @@ internal sealed partial class ConstraintSolver(SyntaxNode context, string contex
     }
 
     /// <summary>
-    /// Unwraps the potential type-variable until it is a non-type-variable type.
-    /// </summary>
-    /// <param name="type">The type to unwrap.</param>
-    /// <returns>The task that completes when <paramref name="type"/> is subsituted as a non-type-variable.</returns>
-    public static async SolverTask<TypeSymbol> Substituted(TypeSymbol type)
-    {
-        while (type is TypeVariable tv) type = await tv.Substituted;
-        return type;
-    }
-
-    /// <summary>
     /// Unifies two types, asserting their success.
     /// </summary>
     /// <param name="first">The first type to unify.</param>

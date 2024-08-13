7a933d6bc4196e0ee904a1f18814359ccb66c4e2
LPeter1997 <lenkefi.peti@gmail.com>
2024-08-13 11:43:23 +0200

    Script fixes

--- scripts/install_langserver.ps1
+++ scripts/install_langserver.ps1
@@ -6,7 +6,7 @@
 
 $ErrorActionPreference = "Stop"
 Push-Location $PSScriptRoot
-dotnet pack ../src/Draco.LanguageServer --output .
+dotnet pack ../src/Draco.LanguageServer --configuration Debug --output .
 if ((dotnet tool list --global) -match "Draco.LanguageServer") {
     dotnet tool uninstall --global Draco.LanguageServer
 }

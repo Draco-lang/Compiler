d9be30a90400c571468eea680ddca18e87ba12b3
LPeter1997 <lenkefi.peti@gmail.com>
2024-08-21 12:30:24 +0200

    Update TextEdit.cs

--- src/Draco.Compiler/Api/Syntax/TextEdit.cs
+++ src/Draco.Compiler/Api/Syntax/TextEdit.cs
@@ -6,7 +6,7 @@ namespace Draco.Compiler.Api.Syntax;
 /// <param name="Source">The source text that will be edited.</param>
 /// <param name="Span">The span of the text that will be replaced by <paramref name="Text"/>.</param>
 /// <param name="Text">The text that should be inserted into the source text.</param>
-public record class TextEdit(SourceText Source, SourceSpan Span, string Text)
+public sealed record class TextEdit(SourceText Source, SourceSpan Span, string Text)
 {
     /// <summary>
     /// The range of the text that will be replaced by <see cref="Text"/>.

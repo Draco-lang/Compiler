1784f1050a6ad05c70102cfe6ba4244fdfd6562d
LPeter1997 <lenkefi.peti@gmail.com>
2024-08-22 17:10:57 +0200

    Update Program.cs

--- src/Draco.Repl/Program.cs
+++ src/Draco.Repl/Program.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
-using Draco.Compiler.Api;
-using Draco.Compiler.Api.Scripting;
 using PrettyPrompt.Consoles;
-using static Basic.Reference.Assemblies.Net80;
 
 namespace Draco.Repl;
 

68fa7d90b4e0a147c85656855a4c549f447d9a2b
LPeter1997 <lenkefi.peti@gmail.com>
2024-08-25 13:30:36 +0200

    Update ScriptBinding.cs

--- src/Draco.Compiler/Internal/Binding/ScriptBinding.cs
+++ src/Draco.Compiler/Internal/Binding/ScriptBinding.cs
@@ -1,6 +1,7 @@
 using System.Collections.Immutable;
 using Draco.Compiler.Api.Syntax;
 using Draco.Compiler.Internal.BoundTree;
+using Draco.Compiler.Internal.Symbols;
 
 namespace Draco.Compiler.Internal.Binding;
 
@@ -9,6 +10,8 @@ namespace Draco.Compiler.Internal.Binding;
 /// </summary>
 /// <param name="GlobalBindings">The global variables defined in the script.</param>
 /// <param name="FunctionBodies">The function bodies bound in the script.</param>
+/// <param name="EvalType">The type the script evaluates to.</param>
 internal readonly record struct ScriptBinding(
     ImmutableDictionary<VariableDeclarationSyntax, GlobalBinding> GlobalBindings,
-    ImmutableDictionary<FunctionDeclarationSyntax, BoundStatement> FunctionBodies);
+    ImmutableDictionary<FunctionDeclarationSyntax, BoundStatement> FunctionBodies,
+    TypeSymbol EvalType);

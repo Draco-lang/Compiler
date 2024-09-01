3c12806a686b1329ede654eaf190dd3d474a2c99
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-01 23:03:17 +0200

    Create MinimalAssemblyCodegen.cs

---
+++ src/Draco.Compiler/Internal/OptimizingIr/Codegen/MinimalAssemblyCodegen.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Draco.Compiler.Api;
+using Draco.Compiler.Internal.BoundTree;
+using Draco.Compiler.Internal.Lowering;
+using Draco.Compiler.Internal.OptimizingIr.Model;
+using Draco.Compiler.Internal.Symbols;
+using Draco.Compiler.Internal.Symbols.Source;
+
+namespace Draco.Compiler.Internal.OptimizingIr.Codegen;
+
+/// <summary>
+/// A code generator that compiles only the minimal number of procedures in order to evaluate something.
+/// Used for compile-time execution.
+/// </summary>
+internal sealed class MinimalAssemblyCodegen(Compilation compilation)
+{
+    private readonly Dictionary<FunctionSymbol, Procedure> compiledProcedures = [];
+    private readonly HashSet<FunctionSymbol> functionsToCompile = [];
+
+    private void EmptyWorkList()
+    {
+        while (this.functionsToCompile.Count > 0)
+        {
+            var function = this.functionsToCompile.First();
+            this.functionsToCompile.Remove(function);
+            if (this.compiledProcedures.ContainsKey(function)) continue;
+
+            var procedure = this.CompileFunction(function);
+            if (procedure is not null)
+            {
+                this.compiledProcedures.Add(function, procedure);
+            }
+        }
+    }
+
+    private Procedure? CompileFunction(FunctionSymbol function)
+    {
+        if (function.Body is null) return null;
+
+        // TODO: Little copypasta from ModuleCodegen, maybe refactor
+        // Create the body
+        var body = this.RewriteBody(function.Body);
+        // Yank out potential local functions and closures
+        var (bodyWithoutLocalFunctions, localFunctions) = ClosureRewriter.Rewrite(body);
+        // Compile it
+        var bodyCodegen = new LocalCodegen(compilation, TODO_MODULE);
+        bodyWithoutLocalFunctions.Accept(bodyCodegen);
+
+        // Add the rest to the worklist
+        foreach (var localFunction in localFunctions)
+        {
+            this.functionsToCompile.Add(localFunction);
+        }
+
+        // TODO: Return the procedure
+    }
+
+    // NOTE: For now we don't inject sequence points
+    private BoundNode RewriteBody(BoundNode body) => body.Accept(new LocalRewriter(compilation));
+}

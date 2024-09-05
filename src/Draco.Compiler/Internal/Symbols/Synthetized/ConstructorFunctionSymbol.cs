9c0c3728397c909eaae3ad726ed2acfbd0a39473
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-05 10:08:10 +0200

    Update ConstructorFunctionSymbol.cs

--- src/Draco.Compiler/Internal/Symbols/Synthetized/ConstructorFunctionSymbol.cs
+++ src/Draco.Compiler/Internal/Symbols/Synthetized/ConstructorFunctionSymbol.cs
@@ -1,6 +1,7 @@
 using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
+using Draco.Compiler.Internal.Documentation;
 using Draco.Compiler.Internal.Symbols.Generic;
 using static Draco.Compiler.Internal.OptimizingIr.InstructionFactory;
 
@@ -15,6 +16,8 @@ internal sealed class ConstructorFunctionSymbol(FunctionSymbol ctorDefinition) :
     public override string Name => this.InstantiatedType.Name;
     public override bool IsSpecialName => true;
     public override Api.Semantics.Visibility Visibility => ctorDefinition.Visibility;
+    public override SymbolDocumentation Documentation => ctorDefinition.Documentation;
+    internal override string RawDocumentation => ctorDefinition.RawDocumentation;
 
     public override ImmutableArray<TypeParameterSymbol> GenericParameters =>
         InterlockedUtils.InitializeDefault(ref this.genericParameters, this.BuildGenericParameters);

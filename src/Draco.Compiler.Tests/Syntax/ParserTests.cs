b45df9ac388a8ef46ac3ab89793c91c1b84813f2
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-07 10:07:23 +0200

    Added syntax tests

--- src/Draco.Compiler.Tests/Syntax/ParserTests.cs
+++ src/Draco.Compiler.Tests/Syntax/ParserTests.cs
@@ -1901,4 +1901,93 @@ public sealed class ParserTests
             this.T(TokenKind.Semicolon);
         }
     }
+
+    [Fact]
+    public void TestFunctionAndParameterWithAttribute()
+    {
+        this.ParseDeclaration("""
+            @Attr1("this is a function")
+            func foo(@Attr2("this is a parameter") x: int32) {}
+            """);
+
+        this.N<FunctionDeclarationSyntax>();
+        {
+            this.N<SyntaxList<AttributeSyntax>>();
+            this.N<AttributeSyntax>();
+            {
+                this.T(TokenKind.AtSign);
+                this.N<NameTypeSyntax>();
+                {
+                    this.T(TokenKind.Identifier, "Attr1");
+                }
+                this.N<ArgumentListSyntax>();
+                {
+                    this.T(TokenKind.ParenOpen);
+                    this.N<SeparatedSyntaxList<ExpressionSyntax>>();
+                    {
+                        this.N<StringExpressionSyntax>();
+                        {
+                            this.T(TokenKind.LineStringStart, "\"");
+                            this.N<SyntaxList<StringPartSyntax>>();
+                            {
+                                this.N<TextStringPartSyntax>();
+                                this.T(TokenKind.StringContent, "this is a function");
+                            }
+                            this.T(TokenKind.LineStringEnd, "\"");
+                        }
+                    }
+                    this.T(TokenKind.ParenClose);
+                }
+            }
+            this.T(TokenKind.KeywordFunc);
+            this.T(TokenKind.Identifier, "foo");
+            this.T(TokenKind.ParenOpen);
+            this.N<SeparatedSyntaxList<ParameterSyntax>>();
+            {
+                this.N<ParameterSyntax>();
+                {
+                    this.N<SyntaxList<AttributeSyntax>>();
+                    this.N<AttributeSyntax>();
+                    {
+                        this.T(TokenKind.AtSign);
+                        this.N<NameTypeSyntax>();
+                        {
+                            this.T(TokenKind.Identifier, "Attr2");
+                        }
+                        this.N<ArgumentListSyntax>();
+                        {
+                            this.T(TokenKind.ParenOpen);
+                            this.N<SeparatedSyntaxList<ExpressionSyntax>>();
+                            {
+                                this.N<StringExpressionSyntax>();
+                                {
+                                    this.T(TokenKind.LineStringStart, "\"");
+                                    this.N<SyntaxList<StringPartSyntax>>();
+                                    {
+                                        this.N<TextStringPartSyntax>();
+                                        this.T(TokenKind.StringContent, "this is a parameter");
+                                    }
+                                    this.T(TokenKind.LineStringEnd, "\"");
+                                }
+                            }
+                            this.T(TokenKind.ParenClose);
+                        }
+                    }
+                    this.T(TokenKind.Identifier, "x");
+                    this.T(TokenKind.Colon);
+                    this.N<NameTypeSyntax>();
+                    {
+                        this.T(TokenKind.Identifier, "int32");
+                    }
+                }
+            }
+            this.T(TokenKind.ParenClose);
+            this.N<BlockFunctionBodySyntax>();
+            {
+                this.T(TokenKind.CurlyOpen);
+                this.N<SyntaxList<StatementSyntax>>();
+                this.T(TokenKind.CurlyClose);
+            }
+        }
+    }
 }

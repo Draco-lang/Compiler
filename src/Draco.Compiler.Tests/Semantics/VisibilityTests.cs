4987d2ff191204a485a88e6ef247ed12b9bb5ef7
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-11 13:48:59 +0200

    Update VisibilityTests.cs

--- src/Draco.Compiler.Tests/Semantics/VisibilityTests.cs
+++ src/Draco.Compiler.Tests/Semantics/VisibilityTests.cs
@@ -51,4 +51,54 @@ public sealed class VisibilityTests
         Assert.True(fooSymbol.IsVisibleFrom(barSymbol));
         Assert.True(barSymbol.IsVisibleFrom(fooSymbol));
     }
+
+    [Fact]
+    public void MutuallyExclusivePrivateMemberVisibility()
+    {
+        var compilation = CreateCompilation("""
+            module A {
+                func foo() {}
+            }
+
+            module B {
+                func bar() {}
+            }
+            """);
+
+        var fooSyntax = compilation.SyntaxTrees[0].FindInChildren<FunctionDeclarationSyntax>(0);
+        var barSyntax = compilation.SyntaxTrees[0].FindInChildren<FunctionDeclarationSyntax>(1);
+
+        var semanticModel = compilation.GetSemanticModel(compilation.SyntaxTrees[0]);
+
+        var fooSymbol = GetInternalSymbol<Symbol>(semanticModel.GetDeclaredSymbol(fooSyntax));
+        var barSymbol = GetInternalSymbol<Symbol>(semanticModel.GetDeclaredSymbol(barSyntax));
+
+        Assert.False(fooSymbol.IsVisibleFrom(barSymbol));
+        Assert.False(barSymbol.IsVisibleFrom(fooSymbol));
+    }
+
+    [Fact]
+    public void MutuallyExclusiveInternalMemberVisibility()
+    {
+        var compilation = CreateCompilation("""
+            module A {
+                internal func foo() {}
+            }
+
+            module B {
+                func bar() {}
+            }
+            """);
+
+        var fooSyntax = compilation.SyntaxTrees[0].FindInChildren<FunctionDeclarationSyntax>(0);
+        var barSyntax = compilation.SyntaxTrees[0].FindInChildren<FunctionDeclarationSyntax>(1);
+
+        var semanticModel = compilation.GetSemanticModel(compilation.SyntaxTrees[0]);
+
+        var fooSymbol = GetInternalSymbol<Symbol>(semanticModel.GetDeclaredSymbol(fooSyntax));
+        var barSymbol = GetInternalSymbol<Symbol>(semanticModel.GetDeclaredSymbol(barSyntax));
+
+        Assert.True(fooSymbol.IsVisibleFrom(barSymbol));
+        Assert.False(barSymbol.IsVisibleFrom(fooSymbol));
+    }
 }

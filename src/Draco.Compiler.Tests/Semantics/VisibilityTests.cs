551bf9b095c210118147fb38932b900a9b99c895
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-11 13:44:55 +0200

    Create VisibilityTests.cs

---
+++ src/Draco.Compiler.Tests/Semantics/VisibilityTests.cs
@@ -0,0 +1,54 @@
+using Draco.Compiler.Api.Syntax;
+using Draco.Compiler.Internal.Symbols;
+using static Draco.Compiler.Tests.TestUtilities;
+
+namespace Draco.Compiler.Tests.Semantics;
+
+public sealed class VisibilityTests
+{
+    [Fact]
+    public void NestedModulePrivateMemberVisibility()
+    {
+        var compilation = CreateCompilation("""
+            func foo() {}
+
+            module M {
+                func bar() {}
+            }
+            """);
+
+        var fooSyntax = compilation.SyntaxTrees[0].FindInChildren<FunctionDeclarationSyntax>(0);
+        var barSyntax = compilation.SyntaxTrees[0].FindInChildren<FunctionDeclarationSyntax>(1);
+
+        var semanticModel = compilation.GetSemanticModel(compilation.SyntaxTrees[0]);
+
+        var fooSymbol = GetInternalSymbol<Symbol>(semanticModel.GetDeclaredSymbol(fooSyntax));
+        var barSymbol = GetInternalSymbol<Symbol>(semanticModel.GetDeclaredSymbol(barSyntax));
+
+        Assert.True(fooSymbol.IsVisibleFrom(barSymbol));
+        Assert.False(barSymbol.IsVisibleFrom(fooSymbol));
+    }
+
+    [Fact]
+    public void NestedModuleInternalMemberVisibility()
+    {
+        var compilation = CreateCompilation("""
+            func foo() {}
+
+            module M {
+                internal func bar() {}
+            }
+            """);
+
+        var fooSyntax = compilation.SyntaxTrees[0].FindInChildren<FunctionDeclarationSyntax>(0);
+        var barSyntax = compilation.SyntaxTrees[0].FindInChildren<FunctionDeclarationSyntax>(1);
+
+        var semanticModel = compilation.GetSemanticModel(compilation.SyntaxTrees[0]);
+
+        var fooSymbol = GetInternalSymbol<Symbol>(semanticModel.GetDeclaredSymbol(fooSyntax));
+        var barSymbol = GetInternalSymbol<Symbol>(semanticModel.GetDeclaredSymbol(barSyntax));
+
+        Assert.True(fooSymbol.IsVisibleFrom(barSymbol));
+        Assert.True(barSymbol.IsVisibleFrom(fooSymbol));
+    }
+}

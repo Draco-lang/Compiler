828ff2082dbe3c0eeb0fa6fd4a1b2dd619a07969
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-12 12:58:00 +0200

    Update SymbolResolutionTests.cs

--- src/Draco.Compiler.Tests/Semantics/SymbolResolutionTests.cs
+++ src/Draco.Compiler.Tests/Semantics/SymbolResolutionTests.cs
@@ -1270,7 +1270,7 @@ public sealed class SymbolResolutionTests
 
         // Assert
         Assert.Single(diags);
-        AssertDiagnostics(diags, SymbolResolutionErrors.UndefinedReference);
+        AssertDiagnostics(diags, SymbolResolutionErrors.InaccessibleSymbol);
     }
 
     [Fact]
@@ -1305,7 +1305,7 @@ public sealed class SymbolResolutionTests
 
         // Assert
         Assert.Single(diags);
-        AssertDiagnostics(diags, SymbolResolutionErrors.UndefinedReference);
+        AssertDiagnostics(diags, SymbolResolutionErrors.InaccessibleSymbol);
     }
 
     [Fact]

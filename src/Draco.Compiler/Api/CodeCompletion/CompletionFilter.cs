da09f29b466ab7cd20a8aab2b98f10e9c09ab421
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-13 11:24:35 +0200

    Update CompletionFilter.cs

--- src/Draco.Compiler/Api/CodeCompletion/CompletionFilter.cs
+++ src/Draco.Compiler/Api/CodeCompletion/CompletionFilter.cs
@@ -1,3 +1,4 @@
+using System;
 using Draco.Compiler.Api.Syntax;
 
 namespace Draco.Compiler.Api.CodeCompletion;
@@ -7,6 +8,26 @@ namespace Draco.Compiler.Api.CodeCompletion;
 /// </summary>
 public static class CompletionFilter
 {
+    /// <summary>
+    /// Constructs a completion filter from a delegate.
+    /// </summary>
+    /// <param name="filter">The filter function.</param>
+    /// <returns>The created completion filter.</returns>
+    public static ICompletionFilter Create(Func<SyntaxToken?, CompletionItem, bool> filter) => new DelegateCompletionFilter(filter);
+
+    /// <summary>
+    /// Constructs a completion filter that accepts all completion items when the token under the cursor is null.
+    /// Otherwise, the provided filter is used.
+    /// </summary>
+    /// <param name="filter">The filter to use when the token is not null.</param>
+    /// <returns>The created completion filter.</returns>
+    public static ICompletionFilter AcceptNullToken(Func<SyntaxToken, CompletionItem, bool> filter) =>
+        Create((token, item) => token is null ? true : filter(token, item));
+
+    private sealed class DelegateCompletionFilter(Func<SyntaxToken?, CompletionItem, bool> filter) : ICompletionFilter
+    {
+        public bool ShouldKeep(SyntaxToken? underCursor, CompletionItem item) => filter(underCursor, item);
+    }
 }
 
 /// <summary>

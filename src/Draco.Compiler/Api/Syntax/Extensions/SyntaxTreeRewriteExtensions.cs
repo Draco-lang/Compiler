c757c17c18db57929510ece91ed7b6a2bc0429f5
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-14 22:02:37 +0200

    Update SyntaxTreeRewriteExtensions.cs

--- src/Draco.Compiler/Api/Syntax/Extensions/SyntaxTreeRewriteExtensions.cs
+++ src/Draco.Compiler/Api/Syntax/Extensions/SyntaxTreeRewriteExtensions.cs
@@ -9,29 +9,55 @@ namespace Draco.Compiler.Api.Syntax.Extensions;
 /// </summary>
 public static class SyntaxTreeRewriteExtensions
 {
-    // TODO: Doc, rewrite...
-    public static SyntaxTree Reorder(this SyntaxTree tree, SyntaxNode toReorder, int position) => new(
+    /// <summary>
+    /// Inserts a node before another node in the syntax tree.
+    /// </summary>
+    /// <param name="tree">The syntax tree to rewrite.</param>
+    /// <param name="toInsert">The node to insert.</param>
+    /// <param name="insertBefore">The node to insert before.</param>
+    /// <returns>The rewritten syntax tree.</returns>
+    public static SyntaxTree InsertBefore(this SyntaxTree tree, SyntaxNode toInsert, SyntaxNode insertBefore) => new(
         tree.SourceText,
-        tree.GreenRoot.Accept(new ReorderRewriter(toReorder.Green, position)),
+        InsertRewriter.InsertBefore(tree.GreenRoot, toInsert.Green, insertBefore.Green),
         new());
 
-    // TODO: Doc, rewrite...
+    /// <summary>
+    /// Inserts a node after another node in the syntax tree.
+    /// </summary>
+    /// <param name="tree">The syntax tree to rewrite.</param>
+    /// <param name="toInsert">The node to insert.</param>
+    /// <param name="insertBefore">The node to insert after.</param>
+    /// <returns>The rewritten syntax tree.</returns>
+    public static SyntaxTree InsertAfter(this SyntaxTree tree, SyntaxNode toInsert, SyntaxNode insertBefore) => new(
+        tree.SourceText,
+        InsertRewriter.InsertAfter(tree.GreenRoot, toInsert.Green, insertBefore.Green),
+        new());
+
+    /// <summary>
+    /// Removes a node from the syntax tree.
+    /// </summary>
+    /// <param name="tree">The syntax tree to rewrite.</param>
+    /// <param name="toRemove">The node to remove.</param>
+    /// <returns>The rewritten syntax tree.</returns>
     public static SyntaxTree Remove(this SyntaxTree tree, SyntaxNode toRemove) => new(
         tree.SourceText,
-        tree.GreenRoot.Accept(new RemoveRewriter(toRemove.Green)),
+        RemoveRewriter.Remove(tree.GreenRoot, toRemove.Green),
         new());
 
-    // TODO: Doc, rewrite...
-    public static SyntaxTree Insert(this SyntaxTree tree, SyntaxNode toInsert, SyntaxNode insertInto, int position) => new(
-        tree.SourceText,
-        tree.GreenRoot.Accept(new InsertRewriter(toInsert.Green, insertInto.Green, position)),
-        new());
-
-    // TODO: Doc, rewrite...
-    public static ImmutableArray<TextEdit> SyntaxTreeDiff(this SyntaxTree tree, SyntaxTree other) =>
+    /// <summary>
+    /// Calculates the edits needed to transform one syntax tree into another.
+    /// </summary>
+    /// <param name="tree">The original syntax tree.</param>
+    /// <param name="target">The target syntax tree.</param>
+    /// <returns>The edits needed to transform the original syntax tree into the target syntax tree.</returns>
+    public static ImmutableArray<TextEdit> CalculateEdits(this SyntaxTree tree, SyntaxTree target) =>
         // TODO: We can use a better diff algo
-        [new TextEdit(tree.Root.Span, other.ToString())];
+        [new TextEdit(tree.Root.Span, target.ToString())];
 
-    // TODO: Doc, rewrite...
+    /// <summary>
+    /// Formats the syntax tree.
+    /// </summary>
+    /// <param name="tree">The syntax tree to format.</param>
+    /// <returns>The formatted syntax tree.</returns>
     public static SyntaxTree Format(this SyntaxTree tree) => Formatter.Format(tree);
 }

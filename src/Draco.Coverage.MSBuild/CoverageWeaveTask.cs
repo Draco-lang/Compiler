c6ce47a6575f1108689b52c9c5bc159516d3f786
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-17 18:00:43 +0200

    Elaboration

--- src/Draco.Coverage.MSBuild/CoverageWeaveTask.cs
+++ src/Draco.Coverage.MSBuild/CoverageWeaveTask.cs
@@ -1,12 +1,25 @@
+using System.IO;
+using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
 namespace Draco.Coverage.MSBuild;
 
 public sealed class CoverageWeaveTask : Task
 {
+    [Required]
+    public string InputPath { get; set; }
+
+    [Required]
+    public string OutputPath { get; set; }
+
     public override bool Execute()
     {
-        throw new System.NotImplementedException("TODO");
+        using var readerStream = File.OpenRead(this.InputPath);
+        using var writerStream = File.OpenWrite(this.OutputPath);
+        InstrumentedAssembly.Weave(readerStream, writerStream);
+
+        readerStream.Close();
+        writerStream.Close();
 
         return true;
     }

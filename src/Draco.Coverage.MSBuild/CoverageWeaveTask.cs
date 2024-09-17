ff95df4feeab1350ce474c8ad64a0996bf1f6b76
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-17 17:40:59 +0200

    MSBuild skeleton

---
+++ src/Draco.Coverage.MSBuild/CoverageWeaveTask.cs
@@ -0,0 +1,13 @@
+using Microsoft.Build.Utilities;
+
+namespace Draco.Coverage.MSBuild;
+
+public sealed class CoverageWeaveTask : Task
+{
+    public override bool Execute()
+    {
+        throw new System.NotImplementedException("TODO");
+
+        return true;
+    }
+}

eeaf05b6c468ee560b2f4cc82ac3766ff2b34741
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-19 11:59:17 +0200

    Update CoverageWeaveTask.cs

--- src/Draco.Coverage.MSBuild/CoverageWeaveTask.cs
+++ src/Draco.Coverage.MSBuild/CoverageWeaveTask.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
@@ -14,21 +18,44 @@ public sealed class CoverageWeaveTask : Task
 
     public override bool Execute()
     {
-        if (this.InputPath != this.OutputPath)
+        var inputPaths = ProcessPath(this.InputPath);
+        var outputPaths = ProcessPath(this.OutputPath);
+        if (inputPaths.Length != outputPaths.Length)
         {
-            using var readerStream = File.OpenRead(this.InputPath);
-            using var writerStream = File.OpenWrite(this.OutputPath);
+            throw new ArgumentException("input and output paths must have the same number of elements");
+        }
+
+        for (var i = 0; i < inputPaths.Length; i++)
+        {
+            this.Weave(inputPaths[i], outputPaths[i]);
+        }
+
+        return true;
+    }
+
+    private void Weave(string inputPath, string outputPath)
+    {
+        if (inputPath != outputPath)
+        {
+            using var readerStream = File.OpenRead(inputPath);
+            using var writerStream = File.OpenWrite(outputPath);
             InstrumentedAssembly.Weave(readerStream, writerStream);
         }
         else
         {
             // We use memory streams to avoid locking the file
-            using var readerStream = new MemoryStream(File.ReadAllBytes(this.InputPath));
+            using var readerStream = new MemoryStream(File.ReadAllBytes(inputPath));
             using var writerStream = new MemoryStream();
             InstrumentedAssembly.Weave(readerStream, writerStream);
-            File.WriteAllBytes(this.OutputPath, writerStream.ToArray());
+            File.WriteAllBytes(outputPath, writerStream.ToArray());
         }
-
-        return true;
     }
+
+    // MSBuild passes in the path to all binaries copied, concatenated with a semocilon
+    // We need to split them and filter for .dll files
+    private static string[] ProcessPath(string path) => path
+        .Split(';')
+        .Where(p => !string.IsNullOrWhiteSpace(p))
+        .Where(p => Path.GetExtension(p) == ".dll")
+        .ToArray();
 }

23134e954e6d80fcc83340f861e1e8789ff34a87
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-19 09:57:33 +0200

    Update Program.cs

--- src/Sandbox/Program.cs
+++ src/Sandbox/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Sandbox;
+using System.Reflection;
+using Draco.Compiler.Api;
+using Draco.Coverage;
+
+namespace Sandbox;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var asm = InstrumentedAssembly.FromWeavedAssembly(typeof(Compilation).Assembly);
+        asm.ClearCoverageData();
+
+        var compilation = Compilation.Create(
+            syntaxTrees: []);
+
+        var result = asm.GetCoverageResult();
+        Console.WriteLine(result.Entires.Count(e => e.Hits > 0));
     }
 }

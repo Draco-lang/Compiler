1c042d4e4b1717e73218f8ca26d0d1c8c11c73bf
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-19 11:59:30 +0200

    Cleanup

--- src/Draco.Coverage/InstrumentationWeaver.cs
+++ src/Draco.Coverage/InstrumentationWeaver.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 using Mono.Cecil;
 using Mono.Cecil.Cil;

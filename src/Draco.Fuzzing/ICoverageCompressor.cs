84a921240c734783dec7406839657af8b242f68a
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-20 09:32:07 +0200

    More defaults

--- src/Draco.Fuzzing/ICoverageCompressor.cs
+++ src/Draco.Fuzzing/ICoverageCompressor.cs
@@ -1,3 +1,4 @@
+using System;
 using Draco.Coverage;
 
 namespace Draco.Fuzzing;
@@ -14,4 +15,36 @@ public interface ICoverageCompressor<TCoverage>
     /// <param name="result">The coverage result to compress.</param>
     /// <returns>The compressed coverage data.</returns>
     public TCoverage Compress(CoverageResult result);
+}
+
+/// <summary>
+/// Factory for common coverage compression logic.
+/// </summary>
+public static class CoverageCompressor
+{
+    /// <summary>
+    /// A hash-based coverage compressor.
+    /// </summary>
+    public static ICoverageCompressor<int> Hash => HashCoverageCompressor.Instance;
+
+    private sealed class HashCoverageCompressor : ICoverageCompressor<int>
+    {
+        public static HashCoverageCompressor Instance { get; } = new();
+
+        private HashCoverageCompressor()
+        {
+        }
+
+        public int Compress(CoverageResult result)
+        {
+            // NOTE: Naive, slow implementation, we might need to come back to vectorize this
+            // We create a bitarray of the 0 and nonzero hit positions then hash combine them
+            var hash = default(HashCode);
+            foreach (var entry in result.Entires)
+            {
+                hash.Add(entry.Hits != 0);
+            }
+            return hash.ToHashCode();
+        }
+    }
 }
--- src/Draco.Fuzzing/ITargetExecutor.cs
+++ src/Draco.Fuzzing/ITargetExecutor.cs
@@ -1,3 +1,4 @@
+using System;
 using Draco.Coverage;
 
 namespace Draco.Fuzzing;
@@ -14,4 +15,24 @@ public interface ITargetExecutor<TInput>
     /// <param name="input">The input to execute the target with.</param>
     /// <param name="assembly">The instrumented assembly.</param>
     public void Execute(TInput input, InstrumentedAssembly assembly);
+}
+
+/// <summary>
+/// Factory for common target execution logic.
+/// </summary>
+public static class TargetExecutor
+{
+    /// <summary>
+    /// Creates a target executor from the given action.
+    /// </summary>
+    /// <typeparam name="TInput">The type of the input data.</typeparam>
+    /// <param name="action">The action to execute.</param>
+    /// <returns>The target executor.</returns>
+    public static ITargetExecutor<TInput> Create<TInput>(Action<TInput> action) =>
+        new DelegateTargetExecutor<TInput>(action);
+
+    private sealed class DelegateTargetExecutor<TInput>(Action<TInput> action) : ITargetExecutor<TInput>
+    {
+        public void Execute(TInput input, InstrumentedAssembly assembly) => action(input);
+    }
 }

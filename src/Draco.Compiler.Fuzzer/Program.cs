9160e73821efb48cee1c5e6148c7782fb8dfbf89
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-20 09:38:24 +0200

    Coverage tool crashing ofc

---
+++ src/Draco.Compiler.Fuzzer/Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Draco.Compiler.Fuzzer;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}

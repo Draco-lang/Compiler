1a018218d9b6c1d7442de44e143847bc121a69ce
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-20 09:57:27 +0200

    Fixed one crash

--- src/Draco.Compiler.Fuzzer/Program.cs
+++ src/Draco.Compiler.Fuzzer/Program.cs
@@ -1,11 +1,8 @@
-using System;
-
 namespace Draco.Compiler.Fuzzer;
 
 internal static class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
     }
 }

51198388b3f36e1f3dc80e98a0c19b26b5c0fcf5
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-21 14:53:25 +0200

    Basic mutator util

--- src/Draco.Compiler.Fuzzer/SyntaxTreeInputMutator.cs
+++ src/Draco.Compiler.Fuzzer/SyntaxTreeInputMutator.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Draco.Compiler.Api.Syntax;
 using Draco.Fuzzing;
 
@@ -7,5 +8,15 @@ namespace Draco.Compiler.Fuzzer;
 
 internal sealed class SyntaxTreeInputMutator : IInputMutator<SyntaxTree>
 {
-    public IEnumerable<SyntaxTree> Mutate(Random random, SyntaxTree input) => throw new System.NotImplementedException();
+    public IEnumerable<SyntaxTree> Mutate(Random random, SyntaxTree input)
+    {
+        // For now we do something very simple and just swap around tokens
+        var swapper = InputMutator.Swap<SyntaxToken>();
+        var tokens = input.Root.Tokens.ToList();
+        foreach (var mutantList in swapper.Mutate(random, tokens).Take(30))
+        {
+            var source = string.Join(" ", mutantList.Select(t => t.Text));
+            yield return SyntaxTree.Parse(source);
+        }
+    }
 }
--- src/Draco.Fuzzing/IInputMutator.cs
+++ src/Draco.Fuzzing/IInputMutator.cs
@@ -32,6 +32,30 @@ public static class InputMutator
     public static IInputMutator<TInput> Create<TInput>(Func<Random, TInput, IEnumerable<TInput>> func) =>
         new DelegateMutator<TInput>(func);
 
+    /// <summary>
+    /// Creates a mutator that randomly swaps two elements in a sequence.
+    /// </summary>
+    /// <typeparam name="TElement">The type of the elements in the sequence.</typeparam>
+    /// <returns>The mutator.</returns>
+    public static IInputMutator<IList<TElement>> Swap<TElement>() => Create<IList<TElement>>(SwapDelegate);
+
+    private static IEnumerable<IList<TElement>> SwapDelegate<TElement>(Random random, IList<TElement> input)
+    {
+        // Can't mutate a sequence with less than 2 elements
+        if (input.Count < 2) yield return input;
+
+        while (true)
+        {
+            var index1 = random.Next(input.Count);
+            var index2 = random.Next(input.Count);
+
+            if (index1 == index2) continue;
+
+            (input[index1], input[index2]) = (input[index2], input[index1]);
+            yield return input;
+        }
+    }
+
     private sealed class DelegateMutator<TInput>(Func<Random, TInput, IEnumerable<TInput>> func) : IInputMutator<TInput>
     {
         public IEnumerable<TInput> Mutate(Random random, TInput input) => func(random, input);

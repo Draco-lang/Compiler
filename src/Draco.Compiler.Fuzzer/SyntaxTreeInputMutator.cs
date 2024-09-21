d558dc4bd292ced82537e7c5e06ed82ff7a9af5f
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-21 15:40:33 +0200

    Update SyntaxTreeInputMutator.cs

--- src/Draco.Compiler.Fuzzer/SyntaxTreeInputMutator.cs
+++ src/Draco.Compiler.Fuzzer/SyntaxTreeInputMutator.cs
@@ -13,6 +13,8 @@ internal sealed class SyntaxTreeInputMutator : IInputMutator<SyntaxTree>
         // For now we do something very simple and just swap around tokens
         var swapper = InputMutator.Swap<SyntaxToken>();
         var tokens = input.Root.Tokens.ToList();
+        // The last token is the EOF token, we don't want to swap that
+        tokens.RemoveAt(tokens.Count - 1);
         foreach (var mutantList in swapper.Mutate(random, tokens).Take(30))
         {
             var source = string.Join(" ", mutantList.Select(t => t.Text));

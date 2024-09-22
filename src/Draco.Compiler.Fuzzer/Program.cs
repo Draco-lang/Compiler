aa50bf525c12fd8c71540593864d72a4ae6b0b51
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-22 21:11:40 +0200

    Update Program.cs

--- src/Draco.Compiler.Fuzzer/Program.cs
+++ src/Draco.Compiler.Fuzzer/Program.cs
@@ -46,6 +46,27 @@ internal static class Program
             func baz() {}
             func qux() {}
             """));
+        fuzzer.Enqueue(SyntaxTree.Parse("""
+            import System.Console;
+
+            func main() {
+                WriteLine("Hello, world!");
+            }
+            """));
+        fuzzer.Enqueue(SyntaxTree.Parse("""
+            import System.Console;
+            import System.Linq.Enumerable;
+
+            func fib(n: int32): int32 =
+                if (n < 2) 1
+                else fib(n - 1) + fib(n - 2);
+
+            func main() {
+                for (i in Range(0, 10)) {
+                    WriteLine("fib(\{i}) = \{fib(i)}");
+                }
+            }
+            """));
 
         var fuzzerTask = Task.Run(() => fuzzer.Fuzz(CancellationToken.None));
 

fdd037d5ce1e4b3a1cdbbc813cb3278bdbe7872b
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-22 18:52:06 +0200

    Update Program.cs

--- src/Draco.Compiler.Fuzzer/Program.cs
+++ src/Draco.Compiler.Fuzzer/Program.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
-using System.IO.Pipes;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;

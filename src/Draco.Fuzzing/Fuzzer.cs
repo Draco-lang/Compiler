2151dada21ab6c72e8c36d8fd9f05aeae39686e7
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-24 15:23:32 +0200

    Update Fuzzer.cs

--- src/Draco.Fuzzing/Fuzzer.cs
+++ src/Draco.Fuzzing/Fuzzer.cs
@@ -17,6 +17,17 @@ namespace Draco.Fuzzing;
 /// <param name="seed">The seed to use for the random number generator.</param>
 public sealed class Fuzzer<TInput, TCoverage>(int? seed = null)
 {
+    // Minimal result info of an execution
+    private readonly record struct ExecutionResult(TCoverage Coverage, FaultResult FaultResult);
+
+    // Initial inputs have no coverage data, so the entry needs to handle the case where coverage is not yet present
+    // and we fill it out later
+    private sealed class QueueEntry(TInput input, ExecutionResult? executionResult = null)
+    {
+        public TInput Input { get; } = input;
+        public ExecutionResult? ExecutionResult { get; set; } = executionResult;
+    }
+
     /// <summary>
     /// A shared random number generator.
     /// </summary>
@@ -57,18 +68,26 @@ public sealed class Fuzzer<TInput, TCoverage>(int? seed = null)
     /// </summary>
     public required ITracer<TInput> Tracer { get; init; }
 
+    private readonly Queue<QueueEntry> inputQueue = new();
+
     /// <summary>
     /// Enqueues the given input into the fuzzer.
     /// </summary>
     /// <param name="input">The input to enqueue.</param>
     public void Enqueue(TInput input)
     {
-        // TODO
+        this.inputQueue.Enqueue(new QueueEntry(input));
+        this.Tracer.InputsEnqueued([input]);
     }
 
+    /// <summary>
+    /// Enqueues a range of inputs into the fuzzer.
+    /// </summary>
+    /// <param name="inputs">The inputs to enqueue.</param>
     public void EnqueueRange(IEnumerable<TInput> inputs)
     {
-        // TODO
+        foreach (var input in inputs) this.inputQueue.Enqueue(new QueueEntry(input));
+        this.Tracer.InputsEnqueued(inputs);
     }
 
     /// <summary>
@@ -76,6 +95,31 @@ public sealed class Fuzzer<TInput, TCoverage>(int? seed = null)
     /// </summary>
     /// <param name="cancellationToken">The cancellation token to stop the loop.</param>
     public void Run(CancellationToken cancellationToken)
+    {
+        // First off, make sure the executor is set up
+        // For example, in-process execution will need to run all type constructors here
+        // The reason is to not poison the coverage data with all the setup code
+        this.TargetExecutor.GlobalInitializer();
+        while (true)
+        {
+            if (cancellationToken.IsCancellationRequested) break;
+            if (!this.inputQueue.TryDequeue(out var entry)) break;
+
+            // We want to minimize the input first
+            entry = this.Minimize(entry);
+
+            // And we want to mutate the minimized input
+            this.Mutate(entry);
+        }
+        this.Tracer.FuzzerFinished();
+    }
+
+    private QueueEntry Minimize(QueueEntry entry)
+    {
+        // TODO
+    }
+
+    private void Mutate(QueueEntry entry)
     {
         // TODO
     }

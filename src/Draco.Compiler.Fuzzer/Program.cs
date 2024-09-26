cbdc9bbc0ac8159669add78220898d76ae1dc531
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-26 15:55:56 +0200

    Update Program.cs

--- src/Draco.Compiler.Fuzzer/Program.cs
+++ src/Draco.Compiler.Fuzzer/Program.cs
@@ -2,30 +2,41 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Draco.Compiler.Api.Syntax;
+using Draco.Fuzzing;
 using Terminal.Gui;
 
 namespace Draco.Compiler.Fuzzer;
 
 internal static class Program
 {
-    private static async Task Main(string[] args)
+    private static void Main(string[] args)
     {
         Application.Init();
-        var debuggerWindow = new TuiTracer();
+        Application.MainLoop.Invoke(async () =>
+        {
+            var choice = MessageBox.Query("Fuzzer Mode", "Run the fuzzer in-process or out-of-process?", "in-process", "out-of-process");
+            if (choice == -1)
+            {
+                Application.Shutdown();
+                return;
+            }
 
-        var fuzzer = FuzzerFactory.CreateOutOfProcess(debuggerWindow);
-        debuggerWindow.Fuzzer = fuzzer;
+            var debuggerWindow = new TuiTracer();
+            var fuzzer = choice == 0
+                ? FuzzerFactory.CreateInProcess(debuggerWindow)
+                : FuzzerFactory.CreateOutOfProcess(debuggerWindow);
+            debuggerWindow.Fuzzer = fuzzer;
 
-        var fuzzerTask = Task.Run(() => fuzzer.Run(CancellationToken.None));
+            var fuzzerTask = Task.Run(() => fuzzer.Run(CancellationToken.None));
 
+            await fuzzerTask;
+            Application.Shutdown();
+        });
         Application.MainLoop.AddTimeout(TimeSpan.FromMilliseconds(500), loop =>
         {
             Application.Refresh();
             return true;
         });
-
         Application.Run(Application.Top);
-        await fuzzerTask;
-        Application.Shutdown();
     }
 }

905766480c15bbbf476f47de088761f2ff74f4bf
LPeter1997 <lenkefi.peti@gmail.com>
2024-09-27 19:23:17 +0200

    Update LimitingThreadPool.cs

--- src/Draco.Fuzzing/LimitingThreadPool.cs
+++ src/Draco.Fuzzing/LimitingThreadPool.cs
@@ -66,7 +66,7 @@ internal sealed class LimitingThreadPool
     /// </summary>
     /// <param name="action">The work to be done.</param>
     /// <param name="cancellationToken">The cancellation token to cancel waiting for thread availability.</param>
-    public void QueueWork(Action action, CancellationToken? cancellationToken)
+    public void QueueWork(Action action, CancellationToken? cancellationToken = null)
     {
         this.semaphore.Wait(cancellationToken ?? CancellationToken.None);
         if (this.threads.TryDequeue(out var thread))

0c69b6d5b5d51d622b6a368e2678691853d2d86e
LPeter1997 <lenkefi.peti@gmail.com>
2024-10-01 22:06:59 +0200

    Create IFuzzerAddon.cs

---
+++ src/Draco.Fuzzing.Tui/IFuzzerAddon.cs
@@ -0,0 +1,34 @@
+using Draco.Fuzzing.Tracing;
+using Terminal.Gui;
+
+namespace Draco.Fuzzing.Tui;
+
+/// <summary>
+/// Addon for a fuzzer.
+/// </summary>
+public interface IFuzzerAddon
+{
+    /// <summary>
+    /// Registers the addon onto a tracer.
+    /// </summary>
+    /// <param name="tracer">The tracer to register onto.</param>
+    public void Register(EventTracer<object?> tracer);
+
+    /// <summary>
+    /// Creates a view for the addon, if applicable.
+    /// </summary>
+    /// <returns>The view, if applicable.</returns>
+    public View? CreateView();
+
+    /// <summary>
+    /// Creates a menu bar item for the addon, if applicable.
+    /// </summary>
+    /// <returns>The menu bar item, if applicable.</returns>
+    public MenuBarItem? CreateMenuBarItem();
+
+    /// <summary>
+    /// Creates a status item for the addon, if applicable.
+    /// </summary>
+    /// <returns>The status item, if applicable.</returns>
+    public StatusItem? CreateStatusItem();
+}

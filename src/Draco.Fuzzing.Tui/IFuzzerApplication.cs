35bf7555af9ff06aff1814ca9db48a26fb8127eb
LPeter1997 <lenkefi.peti@gmail.com>
2024-10-03 16:34:49 +0200

    Update IFuzzerApplication.cs

--- src/Draco.Fuzzing.Tui/IFuzzerApplication.cs
+++ src/Draco.Fuzzing.Tui/IFuzzerApplication.cs
@@ -22,7 +22,7 @@ public interface IFuzzerApplication
     /// </summary>
     /// <typeparam name="TAddon">The type of the addon.</typeparam>
     /// <param name="name">The name of the addon.</param>
-    /// <param name="by">The name of the addon requiring the other addon.</param>
-    /// <returns>The addon.</returns>
+    /// <param name="by">The name of the component requiring the addon for identification.</param>
+    /// <returns>The retrieved addon.</returns>
     public TAddon RequireAddon<TAddon>(string name, string by);
 }

bdd675ab78c982a2809776903143cd201da18bc0
LPeter1997 <lenkefi.peti@gmail.com>
2024-10-13 10:28:37 +0200

    More elaborate interfaces

--- src/Draco.Compiler/Internal/FlowAnalysis/BasicBlock.cs
+++ src/Draco.Compiler/Internal/FlowAnalysis/BasicBlock.cs
@@ -9,9 +9,30 @@ namespace Draco.Compiler.Internal.FlowAnalysis;
 /// <summary>
 /// A single basic block in a control flow graph.
 /// </summary>
-/// <param name="nodes">The bound nodes in the basic block.</param>
-internal sealed class BasicBlock(ImmutableArray<BoundNode> nodes) : IReadOnlyList<BoundNode>
+internal interface IBasicBlock : IReadOnlyList<BoundNode>
 {
+    /// <summary>
+    /// The predecessors of the basic block.
+    /// </summary>
+    public IEnumerable<BasicBlock> Predecessors { get; }
+
+    /// <summary>
+    /// The successors of the basic block.
+    /// </summary>
+    public IEnumerable<BasicBlock> Successors { get; }
+}
+
+/// <summary>
+/// A mutable implementation of <see cref="IBasicBlock"/>.
+/// </summary>
+internal sealed class BasicBlock(ImmutableArray<BoundNode> nodes) : IBasicBlock
+{
+    public ISet<BasicBlock> Predecessors { get; } = new HashSet<BasicBlock>();
+    public ISet<BasicBlock> Successors { get; } = new HashSet<BasicBlock>();
+
+    IEnumerable<BasicBlock> IBasicBlock.Predecessors => this.Predecessors;
+    IEnumerable<BasicBlock> IBasicBlock.Successors => this.Successors;
+
     public int Count => nodes.Length;
     public BoundNode this[int index] => nodes[index];
 
--- src/Draco.Compiler/Internal/FlowAnalysis/ControlFlowGraph.cs
+++ src/Draco.Compiler/Internal/FlowAnalysis/ControlFlowGraph.cs
@@ -1,8 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Draco.Compiler.Internal.Utilities;
+
 namespace Draco.Compiler.Internal.FlowAnalysis;
 
 /// <summary>
-/// A control flow graph consisting of <see cref="BasicBlock"/>s.
+/// A control flow graph consisting of <see cref="IBasicBlock"/>s.
 /// </summary>
-internal sealed class ControlFlowGraph
+internal interface IControlFlowGraph
 {
+    /// <summary>
+    /// The entry point of the control flow graph.
+    /// </summary>
+    public IBasicBlock Entry { get; }
+
+    /// <summary>
+    /// All basic blocks in the control flow graph.
+    /// </summary>
+    public IEnumerable<IBasicBlock> AllBlocks { get; }
+}
+
+/// <summary>
+/// A mutable implementation of <see cref="IControlFlowGraph"/>.
+/// </summary>
+internal sealed class ControlFlowGraph : IControlFlowGraph
+{
+    public BasicBlock? Entry { get; set; }
+    IBasicBlock IControlFlowGraph.Entry => this.Entry
+                                        ?? throw new InvalidOperationException("the control flow graph has no entry point");
+
+    public IEnumerable<IBasicBlock> AllBlocks => GraphTraversal.DepthFirst(
+        start: (this as IControlFlowGraph).Entry,
+        getNeighbors: n => n.Successors);
 }

04fa77f21b51bcdc160ec841c94e1b3e82504729
LPeter1997 <lenkefi.peti@gmail.com>
2024-10-16 22:22:07 +0200

    Update FlowDomain.cs

--- src/Draco.Compiler/Internal/FlowAnalysis/FlowDomain.cs
+++ src/Draco.Compiler/Internal/FlowAnalysis/FlowDomain.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using Draco.Compiler.Internal.BoundTree;
 
 namespace Draco.Compiler.Internal.FlowAnalysis;
@@ -67,4 +69,82 @@ internal abstract class FlowDomain<TState>
         for (var i = basicBlock.Count - 1; i >= 0; --i) changed |= this.Transfer(ref state, basicBlock[i]);
         return changed;
     }
+}
+
+// TODO: Replace BitArray, it's horrible
+/// <summary>
+/// A gen-kill flow domain that uses a bit array to represent the state.
+/// </summary>
+/// <typeparam name="TElement">The type of elements the bits represent.</typeparam>
+internal abstract class GenKillFlowDomain<TElement>(IEnumerable<TElement> elements) : FlowDomain<BitArray>
+{
+    /// <summary>
+    /// The elements that the bits represent.
+    /// </summary>
+    public ImmutableArray<TElement> Elements { get; } = elements.ToImmutableArray();
+
+    private readonly Dictionary<BoundNode, BitArray> genSets = [];
+    private readonly Dictionary<BoundNode, BitArray> notKillSets = [];
+
+    public override BitArray Clone(in BitArray state) => new(state);
+
+    public override bool Transfer(ref BitArray state, BoundNode node)
+    {
+        var gen = this.Gen(node);
+        var notKill = this.NotKill(node);
+
+        // I hate you BitArray for needing to clone instead of reporting number of bits changed
+        var oldState = this.Clone(in state);
+        state = state.And(notKill).Or(gen);
+        // And I hate you for needing to compare each bit
+        for (var i = 0; i < state.Length; ++i)
+        {
+            if (state[i] != oldState[i]) return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Retrieves the gen set for the given node.
+    /// </summary>
+    /// <param name="node">The node to retrieve the gen set for.</param>
+    /// <returns>The gen set for the node.</returns>
+    private BitArray Gen(BoundNode node)
+    {
+        if (!this.genSets.TryGetValue(node, out var gen))
+        {
+            gen = this.ComputeGen(node);
+            this.genSets.Add(node, gen);
+        }
+        return gen;
+    }
+
+    /// <summary>
+    /// Retrieves the kill set for the given node.
+    /// </summary>
+    /// <param name="node">The node to retrieve the kill set for.</param>
+    /// <returns>The kill set for the node.</returns>
+    private BitArray NotKill(BoundNode node)
+    {
+        if (!this.notKillSets.TryGetValue(node, out var notKill))
+        {
+            notKill = this.ComputeKill(node).Not();
+            this.notKillSets.Add(node, notKill);
+        }
+        return notKill;
+    }
+
+    /// <summary>
+    /// Constructs the gen set for the given node.
+    /// </summary>
+    /// <param name="node">The node to construct the gen set for.</param>
+    /// <returns>The gen set for the node.</returns>
+    protected abstract BitArray ComputeGen(BoundNode node);
+
+    /// <summary>
+    /// Constructsthe kill set for the given node.
+    /// </summary>
+    /// <param name="node">The node to construct the kill set for.</param>
+    /// <returns>The kill set for the node.</returns>
+    protected abstract BitArray ComputeKill(BoundNode node);
 }

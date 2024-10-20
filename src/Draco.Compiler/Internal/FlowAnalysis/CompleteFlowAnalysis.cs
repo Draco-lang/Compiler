bc60ab6a45e6c26c276874926cd1b4fdd0bf9ef2
LPeter1997 <lenkefi.peti@gmail.com>
2024-10-20 19:14:32 +0200

    Update CompleteFlowAnalysis.cs

--- src/Draco.Compiler/Internal/FlowAnalysis/CompleteFlowAnalysis.cs
+++ src/Draco.Compiler/Internal/FlowAnalysis/CompleteFlowAnalysis.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
 using Draco.Compiler.Internal.BoundTree;
+using Draco.Compiler.Internal.Diagnostics;
+using Draco.Compiler.Internal.FlowAnalysis.Domains;
+using Draco.Compiler.Internal.Symbols;
 using Draco.Compiler.Internal.Symbols.Source;
 
 namespace Draco.Compiler.Internal.FlowAnalysis;
@@ -12,19 +16,51 @@ internal sealed class CompleteFlowAnalysis : BoundTreeVisitor
     /// Analyzes a function body.
     /// </summary>
     /// <param name="symbol">The symbol to analyze.</param>
-    public static void AnalyzeFunction(SourceFunctionSymbol symbol)
+    /// <param name="diagnostics">The diagnostics to report errors to.</param>
+    public static void AnalyzeFunction(SourceFunctionSymbol symbol, DiagnosticBag diagnostics)
     {
+        var analysis = CreateAnalysis(symbol.Body);
+        var flowAnalysis = new CompleteFlowAnalysis(diagnostics, analysis);
+        symbol.Body.Accept(flowAnalysis);
     }
 
     /// <summary>
     /// Analyzes a global value.
     /// </summary>
     /// <param name="symbol">The symbol to analyze.</param>
-    public static void AnalyzeValue(SourceGlobalSymbol symbol)
+    /// <param name="diagnostics">The diagnostics to report errors to.</param>
+    public static void AnalyzeValue(SourceGlobalSymbol symbol, DiagnosticBag diagnostics)
     {
+        if (symbol.Value is null) return;
+
+        var analysis = CreateAnalysis(symbol.Value);
+        var flowAnalysis = new CompleteFlowAnalysis(diagnostics, analysis);
+        symbol.Value.Accept(flowAnalysis);
     }
 
-    private CompleteFlowAnalysis()
+    private static DataFlowAnalysis<(ReturnState, Dictionary<LocalSymbol, AssignementState>)> CreateAnalysis(BoundNode node)
     {
+        var cfg = ControlFlowGraphBuilder.Build(node);
+        var domain = BuildForwardDomain(node);
+        return DataFlowAnalysis.Create(cfg, domain);
+    }
+
+    private static TupleDomain<ReturnState, Dictionary<LocalSymbol, AssignementState>> BuildForwardDomain(BoundNode node)
+    {
+        var locals = BoundTreeCollector.CollectLocals(node);
+        var returnAnalysisDomain = new ReturnsOnAllPathsDomain();
+        var assignmentAnalysisDomain = new DefiniteAssignmentDomain(locals);
+        return new(returnAnalysisDomain, assignmentAnalysisDomain);
+    }
+
+    private readonly DiagnosticBag diagnostics;
+    private readonly DataFlowAnalysis<(ReturnState, Dictionary<LocalSymbol, AssignementState>)> analysis;
+
+    private CompleteFlowAnalysis(
+        DiagnosticBag diagnostics,
+        DataFlowAnalysis<(ReturnState, Dictionary<LocalSymbol, AssignementState>)> analysis)
+    {
+        this.diagnostics = diagnostics;
+        this.analysis = analysis;
     }
 }

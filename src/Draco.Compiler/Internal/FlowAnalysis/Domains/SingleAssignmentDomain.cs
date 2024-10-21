ea45fc778035152df4e34dd81fa253ea11411a16
LPeter1997 <lenkefi.peti@gmail.com>
2024-10-21 20:17:34 +0200

    New domains

--- src/Draco.Compiler/Internal/FlowAnalysis/Domains/SingleAssignmentDomain.cs
+++ src/Draco.Compiler/Internal/FlowAnalysis/Domains/SingleAssignmentDomain.cs
@@ -1,22 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Text;
 using Draco.Compiler.Internal.BoundTree;
 using Draco.Compiler.Internal.Symbols;
 
 namespace Draco.Compiler.Internal.FlowAnalysis.Domains;
 
 /// <summary>
-/// A domain for performing definite assignment analysis.
-/// Essentially checks, if a local variable is definitely assigned at a given point in the program.
+/// A domain for checking if a variable is assigned only once or not.
 /// </summary>
-internal sealed class DefiniteAssignmentDomain(IEnumerable<LocalSymbol> locals)
+internal sealed class SingleAssignmentDomain(IEnumerable<LocalSymbol> locals)
     : GenKillFlowDomain<LocalSymbol>(locals)
 {
     public override FlowDirection Direction => FlowDirection.Forward;
-    public override BitArray Top => new(this.Elements.Length, true);
+    public override BitArray Top => new(this.Elements.Length, false);
 
     public override void Join(ref BitArray target, IEnumerable<BitArray> sources)
     {
@@ -24,16 +20,16 @@ internal sealed class DefiniteAssignmentDomain(IEnumerable<LocalSymbol> locals)
         foreach (var source in sources) target.Or(source);
     }
 
-    protected override BitArray Gen(BoundNode node) => new(this.Elements.Length, false);
-
-    protected override BitArray Kill(BoundNode node) => node switch
+    protected override BitArray Gen(BoundNode node) => node switch
     {
         BoundAssignmentExpression assign when assign.Left is BoundLocalLvalue local =>
             this.CreateWithBitsSet([local.Local]),
         _ => new(this.Elements.Length, false),
     };
 
+    protected override BitArray Kill(BoundNode node) => new(this.Elements.Length, false);
+
     protected override string? ElementToString(BitArray state, int elementIndex) => state[elementIndex]
-        ? null
-        : this.Elements[elementIndex].Name;
+        ? this.Elements[elementIndex].Name
+        : null;
 }

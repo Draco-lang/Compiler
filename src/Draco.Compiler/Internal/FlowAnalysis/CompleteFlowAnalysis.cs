e2d302a7738146c754826ee09de0e22e5271f57c
LPeter1997 <lenkefi.peti@gmail.com>
2024-10-22 19:11:41 +0200

    Update CompleteFlowAnalysis.cs

--- src/Draco.Compiler/Internal/FlowAnalysis/CompleteFlowAnalysis.cs
+++ src/Draco.Compiler/Internal/FlowAnalysis/CompleteFlowAnalysis.cs
@@ -115,8 +115,35 @@ internal sealed class CompleteFlowAnalysis : BoundTreeVisitor
     public override void VisitAssignmentExpression(BoundAssignmentExpression node)
     {
         base.VisitAssignmentExpression(node);
-        if (node.Left is not BoundLocalLvalue localLvalue) return;
+        if (node.Left is BoundGlobalLvalue globalLvalue) this.VisitGlobalAssignmentExpression(node, globalLvalue);
+        if (node.Left is BoundLocalLvalue localLvalue) this.VisitLocalAssignmentExpression(node, localLvalue);
+        if (node.Left is BoundFieldLvalue fieldLvalue) this.VisitFieldAssignmentExpression(node, fieldLvalue);
+    }
 
+    private void VisitGlobalAssignmentExpression(BoundAssignmentExpression node, BoundGlobalLvalue globalLvalue)
+    {
+        // We never allow assignment to global immutables, we assume they are always initialized
+        if (globalLvalue.Global.IsMutable) return;
+
+        this.diagnostics.Add(Diagnostic.Create(
+            template: FlowAnalysisErrors.ImmutableVariableCanNotBeAssignedTo,
+            location: node.Syntax?.Location,
+            formatArgs: globalLvalue.Global.Name));
+    }
+
+    private void VisitFieldAssignmentExpression(BoundAssignmentExpression node, BoundFieldLvalue fieldLvalue)
+    {
+        // We never allow assignment to field immutables, we assume they are always initialized
+        if (fieldLvalue.Field.IsMutable) return;
+
+        this.diagnostics.Add(Diagnostic.Create(
+            template: FlowAnalysisErrors.ImmutableVariableCanNotBeAssignedTo,
+            location: node.Syntax?.Location,
+            formatArgs: fieldLvalue.Field.Name));
+    }
+
+    private void VisitLocalAssignmentExpression(BoundAssignmentExpression node, BoundLocalLvalue localLvalue)
+    {
         var (_, localAssignment, singleAssignment) = this.analysis.GetEntry(node);
 
         // First we check, if we have a compound assignment

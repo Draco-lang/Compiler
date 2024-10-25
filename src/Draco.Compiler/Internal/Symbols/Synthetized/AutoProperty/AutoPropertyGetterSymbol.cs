da784488c1203362aa07f801df57b5780a9f87bb
LPeter1997 <lenkefi.peti@gmail.com>
2024-10-25 21:17:26 +0200

    Added getter and setter

---
+++ src/Draco.Compiler/Internal/Symbols/Synthetized/AutoProperty/AutoPropertyGetterSymbol.cs
@@ -0,0 +1,40 @@
+using Draco.Compiler.Internal.BoundTree;
+using Draco.Compiler.Internal.Symbols.Source;
+using System;
+using System.Collections.Immutable;
+using System.Threading;
+using static Draco.Compiler.Internal.BoundTree.BoundTreeFactory;
+
+namespace Draco.Compiler.Internal.Symbols.Synthetized.AutoProperty;
+
+/// <summary>
+/// An auto-generated getter for an auto-property.
+/// </summary>
+internal sealed class AutoPropertyGetterSymbol(
+    Symbol containingSymbol,
+    SourceAutoPropertySymbol property) : FunctionSymbol, IPropertyAccessorSymbol
+{
+    public override Symbol ContainingSymbol { get; } = containingSymbol;
+
+    public override string Name => $"{this.Property.Name}_Getter";
+    public override bool IsStatic => this.Property.IsStatic;
+    public override Api.Semantics.Visibility Visibility => this.Property.Visibility;
+
+    public override ImmutableArray<ParameterSymbol> Parameters => ImmutableArray<ParameterSymbol>.Empty;
+    public override TypeSymbol ReturnType => this.Property.Type;
+
+    public override BoundStatement Body => LazyInitializer.EnsureInitialized(ref this.body, this.BuildBody);
+    private BoundStatement? body;
+
+    PropertySymbol IPropertyAccessorSymbol.Property => this.Property;
+    public SourceAutoPropertySymbol Property { get; } = property;
+
+    private BoundStatement BuildBody() => ExpressionStatement(BlockExpression(
+        locals: [],
+        statements: [ExpressionStatement(ReturnExpression(FieldExpression(
+            receiver: this.IsStatic
+                ? null
+                : throw new NotImplementedException("TODO: for classes"),
+            field: this.Property.BackingField)))],
+        value: BoundUnitExpression.Default));
+}
---
+++ src/Draco.Compiler/Internal/Symbols/Synthetized/AutoProperty/AutoPropertySetterSymbol.cs
@@ -0,0 +1,51 @@
+using Draco.Compiler.Internal.BoundTree;
+using Draco.Compiler.Internal.Symbols.Source;
+using Draco.Compiler.Internal.Utilities;
+using System;
+using System.Collections.Immutable;
+using System.Threading;
+using static Draco.Compiler.Internal.BoundTree.BoundTreeFactory;
+
+namespace Draco.Compiler.Internal.Symbols.Synthetized.AutoProperty;
+
+/// <summary>
+/// An auto-generated setter for an auto-property.
+/// </summary>
+internal sealed class AutoPropertySetterSymbol(
+    Symbol containingSymbol,
+    SourceAutoPropertySymbol property) : FunctionSymbol, IPropertyAccessorSymbol
+{
+    public override Symbol ContainingSymbol { get; } = containingSymbol;
+
+    public override string Name => $"{this.Property.Name}_Setter";
+    public override bool IsStatic => this.Property.IsStatic;
+    public override Api.Semantics.Visibility Visibility => this.Property.Visibility;
+
+    public override ImmutableArray<ParameterSymbol> Parameters => InterlockedUtils.InitializeDefault(ref this.parameters, this.BuildParameters);
+    private ImmutableArray<ParameterSymbol> parameters;
+
+    public override TypeSymbol ReturnType => WellKnownTypes.Unit;
+
+    public override BoundStatement Body => LazyInitializer.EnsureInitialized(ref this.body, this.BuildBody);
+    private BoundStatement? body;
+
+    PropertySymbol IPropertyAccessorSymbol.Property => this.Property;
+    public SourceAutoPropertySymbol Property { get; } = property;
+
+    private ImmutableArray<ParameterSymbol> BuildParameters() =>
+        [new SynthetizedParameterSymbol(this, "value", this.Property.Type)];
+
+    private BoundStatement BuildBody() => ExpressionStatement(BlockExpression(
+        locals: [],
+        statements: [
+            ExpressionStatement(AssignmentExpression(
+                compoundOperator: null,
+                left: FieldLvalue(
+                    receiver: this.IsStatic
+                        ? null
+                        : throw new NotImplementedException("TODO: classes"),
+                    field: this.Property.BackingField),
+                right: ParameterExpression(this.Parameters[^1]))),
+            ExpressionStatement(ReturnExpression(BoundUnitExpression.Default))],
+        value: BoundUnitExpression.Default));
+}

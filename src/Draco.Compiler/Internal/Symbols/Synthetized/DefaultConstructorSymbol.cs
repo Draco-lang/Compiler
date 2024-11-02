fbbffa936e788d9c532236a6d180ff2eab202069
LPeter1997 <lenkefi.peti@gmail.com>
2024-11-02 13:56:15 +0100

    Update DefaultConstructorSymbol.cs

--- src/Draco.Compiler/Internal/Symbols/Synthetized/DefaultConstructorSymbol.cs
+++ src/Draco.Compiler/Internal/Symbols/Synthetized/DefaultConstructorSymbol.cs
@@ -2,12 +2,13 @@ using System.Collections.Immutable;
 using Draco.Compiler.Internal.BoundTree;
 using static Draco.Compiler.Internal.BoundTree.BoundTreeFactory;
 
-
 namespace Draco.Compiler.Internal.Symbols.Synthetized;
 
+/// <summary>
+/// The default constructor for types without a user-defined constructor.
+/// </summary>
 internal sealed class DefaultConstructorSymbol(TypeSymbol containingSymbol) : FunctionSymbol
 {
-
     public override ImmutableArray<ParameterSymbol> Parameters => [];
 
     public override TypeSymbol ReturnType { get; } = WellKnownTypes.Unit;
@@ -17,8 +18,8 @@ internal sealed class DefaultConstructorSymbol(TypeSymbol containingSymbol) : Fu
 
     public override string Name => ".ctor";
 
-    public override Symbol? ContainingSymbol { get; } = containingSymbol;
+    public override TypeSymbol ContainingSymbol { get; } = containingSymbol;
 
     public override Api.Semantics.Visibility Visibility => Api.Semantics.Visibility.Public;
-    public override BoundStatement? Body { get; } = ExpressionStatement(null, ReturnExpression(null, BoundUnitExpression.Default));
+    public override BoundStatement Body { get; } = ExpressionStatement(ReturnExpression(BoundUnitExpression.Default));
 }

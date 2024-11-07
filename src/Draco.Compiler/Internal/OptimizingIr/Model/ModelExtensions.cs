f46bb86416f4dccd78887ffd8b1009a0f662cb7f
LPeter1997 <lenkefi.peti@gmail.com>
2024-11-07 14:11:30 +0100

    Update ModelExtensions.cs

--- src/Draco.Compiler/Internal/OptimizingIr/Model/ModelExtensions.cs
+++ src/Draco.Compiler/Internal/OptimizingIr/Model/ModelExtensions.cs
@@ -10,23 +10,6 @@ namespace Draco.Compiler.Internal.OptimizingIr.Model;
 internal static class ModelExtensions
 {
     /// <summary>
-    /// Looks up the module in an assembly based on the module symbol.
-    /// </summary>
-    /// <param name="assembly">The assembly to lookup in.</param>
-    /// <param name="module">The module to look up.</param>
-    /// <returns>The looked up module within <paramref name="assembly"/>.</returns>
-    public static IModule Lookup(this IAssembly assembly, ModuleSymbol module)
-    {
-        if (ReferenceEquals(assembly.RootModule.Symbol, module)) return assembly.RootModule;
-        if (module.ContainingSymbol is not ModuleSymbol containingModule)
-        {
-            throw new KeyNotFoundException("the module could not be resolved based on the symbol");
-        }
-        var parentModule = Lookup(assembly, containingModule);
-        return parentModule.Submodules[module];
-    }
-
-    /// <summary>
     /// Retrieves all functions that are statically referenced by a given procedure.
     /// </summary>
     /// <param name="procedure">The procedure to retrieve the referenced functions from.</param>

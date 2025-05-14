f2d7a8b3d20556da19719d8dc2aa5f9a552c1263
LPeter1997 <lenkefi.peti@gmail.com>
2025-05-14 20:38:51 +0200

    Create ConstraintSolver.cs

---
+++ src/Draco.Compiler/Internal/Solver/ConstraintSolver.cs
@@ -0,0 +1,9 @@
+namespace Draco.Compiler.Internal.Solver;
+
+/// <summary>
+/// Solves constraints for the type--system so the binding process can continue.
+/// </summary>
+internal sealed class ConstraintSolver
+{
+    // TODO
+}
